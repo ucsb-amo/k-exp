2dae5d706773babba7b3193d8a2be9d84f60ff5e
jpagett <pagett.jared@gmail.com>
2023-02-02 11:08:59 -0800

    more batch for quickstarts

---
+++ bat/start_artiq_cmd.bat
@@ -0,0 +1,1 @@
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\Users\jar jar binks\miniconda3\shell\condabin\conda-hook.ps1' ; conda activate Kartiq ; cd C:\Code\_GitClones\k-exp"
