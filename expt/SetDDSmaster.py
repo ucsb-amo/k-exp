a690196ab7fe22b4a1cb7e2756e514315411e72d
unknown <pagett.jared@gmail.com>
2023-02-06 09:26:37 -0800

    an s

--- expt/SetDDSmaster.py
+++ expt/SetDDSmaster.py
@@ -5,14 +5,15 @@ class SetDDSmaster(EnvExperiment):
 
     def specify_dds_settings(self, DDS_list):
         '''For now, DDS frequencies must be set in Hz (weird float error)'''
+
         DDS_list[0][0] = DDS(urukul_idx=0, ch=0, freq_Hz=0, att_dB=0)
-        # self.DDS_param_list[0][1] = DDS(urukul_idx=0, ch=1, freq_Hz=98000000, att_dB=0)
         DDS_list[1][3] = DDS(1,3,100000000,0)
 
         return DDS_list
 
-    def prep_default_DDS_lists(self):
+    def prep_default_DDS_list(self):
         ''' Preps a list of DDS states, defaulting to off. '''
+
         DDS_list = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
         for urukul_idx in range(3):
             for ch in range(4):
@@ -21,6 +22,7 @@ class SetDDSmaster(EnvExperiment):
 
     def get_dds(self,dds):
         '''Fetch a DDS device from its name in device-db.py'''
+
         self.setattr_device(dds.name())
         dds.dds_device = self.get_device(dds.name())
         return dds
@@ -44,10 +46,8 @@ class SetDDSmaster(EnvExperiment):
         '''Prep lists, set parameters manually, get the device drivers.'''
 
         self.setattr_device("core")
-
-        self.DDS_list = self.prep_default_DDS_lists()
+        self.DDS_list = self.prep_default_DDS_list()
         self.DDS_list = self.specify_dds_settings(self.DDS_list)
-
         self.DDS_list = [[self.get_dds(dds) for dds in dds_on_this_uru] for dds_on_this_uru in self.DDS_list]
     
     @kernel
