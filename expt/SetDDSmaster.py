e557ce37866a30f89abc9860fda190a3a5114990
unknown <pagett.jared@gmail.com>
2023-02-06 08:48:27 -0800

    move manual dds settings to top of file

--- expt/SetDDSmaster.py
+++ expt/SetDDSmaster.py
@@ -3,6 +3,14 @@ from DDS import DDS
 
 class SetDDSmaster(EnvExperiment):
 
+    def specify_dds_settings(self, DDS_param_list):
+        '''For now, DDS frequencies must be set in Hz (weird float error)'''
+        DDS_param_list[0][0] = DDS(urukul_idx=0, ch=0, freq_Hz=0, att_dB=0)
+        # self.DDS_param_list[0][1] = DDS(urukul_idx=0, ch=1, freq_Hz=98000000, att_dB=0)
+        DDS_param_list[1][3] = DDS(1,3,0,0)
+
+        return DDS_param_list
+
     def prep_default_DDS_lists(self):
         ''' Preps a list of DDS states, defaulting to off. Also get a same-size list to fill with DDS devices. '''
         DDS_param_list = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
@@ -53,18 +61,15 @@ class SetDDSmaster(EnvExperiment):
                 self.set_dds(this_dds,this_dds_params)
 
     def build(self):
-        '''Prep lists, get the devices, and manually set parameters you want.
-        For now, DDS frequencies must be set in Hz (weird float error)'''
+        '''Prep lists, set parameters manually, get the devices, and set each dds.'''
 
         self.setattr_device("core")
 
         self.DDS_param_list, DDS_list = self.prep_default_DDS_lists()
 
-        self.DDS_list = self.get_dds_list(self.DDS_param_list, DDS_list)
+        self.DDS_param_list = self.specify_dds_settings(self.DDS_param_list)
 
-        self.DDS_param_list[0][0] = DDS(urukul_idx=0, ch=0, freq_Hz=0, att_dB=0)
-        # self.DDS_param_list[0][1] = DDS(urukul_idx=0, ch=1, freq_Hz=98000000, att_dB=0)
-        self.DDS_param_list[1][3] = DDS(1,3,0,0)
+        self.DDS_list = self.get_dds_list(self.DDS_param_list, DDS_list)
     
     @kernel
     def run(self):
