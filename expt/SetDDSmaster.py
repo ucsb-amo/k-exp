1a22acaa729576faea9e485d678b9221de38bcce
unknown <pagett.jared@gmail.com>
2023-02-07 12:37:08 -0800

    adapt to use set, init DDS from DDS class

--- expt/SetDDSmaster.py
+++ expt/SetDDSmaster.py
@@ -35,24 +35,9 @@ class SetDDSmaster(EnvExperiment):
 
     def get_dds(self,dds):
         '''Fetch a DDS device from its name in device-db.py'''
-        
+
         dds.dds_device = self.get_device(dds.name())
         return dds
-
-    @kernel
-    def set_dds(self,dds):
-        '''Set a dds device. If freq_MHz = 0, turn it off'''
-
-        dds.dds_device.cpld.init()
-        dds.dds_device.init()
-
-        if dds.freq_MHz != 0.:
-            dds.dds_device.set(dds.freq_MHz * MHz, amplitude = 1.)
-            dds.dds_device.set_att(dds.att_dB * dB)
-            dds.dds_device.sw.on()
-        else:
-            dds.dds_device.sw.off()
-            dds.dds_device.power_down()
 
     def build(self):
         '''Prep lists, set parameters manually, get the device drivers.'''
@@ -61,12 +46,13 @@ class SetDDSmaster(EnvExperiment):
         self.DDS_list = self.prep_default_DDS_list()
         self.DDS_list = self.specify_dds_settings(self.DDS_list)
         self.DDS_list = [[self.get_dds(dds) for dds in dds_on_this_uru] for dds_on_this_uru in self.DDS_list]
-    
+
     @kernel
     def run(self):
-        '''Execute on the core device, set the DDS devices to the corresponding parameters'''
+        '''Execute on the core device, init then set the DDS devices to the corresponding parameters'''
 
         self.core.reset()
-        [[self.set_dds(dds) for dds in dds_on_this_uru] for dds_on_this_uru in self.DDS_list]
+        [[dds.init_dds() for dds in dds_on_this_uru] for dds_on_this_uru in self.DDS_list]
+        [[dds.set_dds() for dds in dds_on_this_uru] for dds_on_this_uru in self.DDS_list]
 
         
