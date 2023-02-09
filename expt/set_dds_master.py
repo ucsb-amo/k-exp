42e9357ad07d9638a8f4130b00e57f4ede05df72
unknown <pagett.jared@gmail.com>
2023-02-09 11:48:57 -0800

    changed syntax, fcn structure to avoid index mismatch

--- expt/set_dds_master.py
+++ expt/set_dds_master.py
@@ -12,34 +12,35 @@ class set_dds_master(EnvExperiment):
     artiq_run --device-db %db% %code%/k-exp/expt/set_dds_master.py
     '''
 
-    def specify_dds_settings(self, DDS_list):
+    def dds(self,urukul_idx,ch,freq_MHz,att_dB):
+        
+        self.DDS_list[urukul_idx][ch] = DDS(urukul_idx,ch,freq_MHz,att_dB)
+        
+
+    def specify_dds_settings(self):
         '''
         Manually specify channel parameters. All numbers must be floats (end in a decimal)
-        To add a DDS, add a line: 
-        DDS[urukul_index][channel_idx] = DDS(urukul_idx,ch,freq_MHz,att_dB)
+        To specify the parameters for a dds channel, add a line: 
+        dds(urukul_idx,ch,freq_MHz,att_dB)
         The DDS puts out +10dBm with no attenuation. Reduce this with att_dB > 0
         '''
 
         # DDS_list[0][0] = DDS(urukul_idx=0, ch=0, freq_MHz=0., att_dB=0.)
-        DDS_list[0][0] = DDS(0,0,98.,14.5)
-        DDS_list[0][1] = DDS(0,1,98.,14.5)
-        DDS_list[0][2] = DDS(0,2,125.4,13.7)
-        DDS_list[0][3] = DDS(0,3,98.,14.5)
-        DDS_list[1][0] = DDS(1,0,125.4,13.7)
-        DDS_list[1][3] = DDS(1,3,20.,13.7)
-
-        return DDS_list
+        self.dds(0,0,98.,14.5)
+        self.dds(0,1,98.,14.5)
+        self.dds(0,2,125.4,13.7)
+        self.dds(0,3,98.,14.5)
+        self.dds(1,0,125.4,13.7)
+        self.dds(1,3,20.,13.7)
 
     def prep_default_DDS_list(self):
         ''' Preps a list of DDS states, defaulting to off. '''
 
-        DDS_list = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        self.DDS_list = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
-        for urukul_idx in range(len(DDS_list)):
-            for ch in range(len(DDS_list[urukul_idx])):
-                DDS_list[urukul_idx][ch] = DDS(urukul_idx,ch,freq_MHz=0.,att_dB=0.)
-                
-        return DDS_list
+        for urukul_idx in range(len(self.DDS_list)):
+            for ch in range(len(self.DDS_list[urukul_idx])):
+                self.DDS_list[urukul_idx][ch] = DDS(urukul_idx,ch,freq_MHz=0.,att_dB=0.)
 
     def get_dds(self,dds):
         '''Fetch a DDS device from its name in device-db.py'''
@@ -51,8 +52,8 @@ class set_dds_master(EnvExperiment):
         '''Prep lists, set parameters manually, get the device drivers.'''
 
         self.setattr_device("core")
-        self.DDS_list = self.prep_default_DDS_list()
-        self.DDS_list = self.specify_dds_settings(self.DDS_list)
+        self.prep_default_DDS_list()
+        self.specify_dds_settings()
         self.DDS_list = [[self.get_dds(dds) for dds in dds_on_this_uru] for dds_on_this_uru in self.DDS_list]
 
     @kernel
