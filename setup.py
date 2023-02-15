7cc45d89c9146edc76b53573d3f2a0bc63e6ef6b
jpagett <pagett.jared@gmail.com>
2023-02-15 12:34:53 -0800

    move files to make module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kexp',
+    version='0.0.1',
+    url='https://github.com/ucsb-amo/kexp',
+    author='Jared Pagett',
+    author_email='pagett.jared@gmail.com',
+    description='Weld Lab K Experiment Repo',
+    packages=find_packages(),    
+    install_requires=[],
+)
