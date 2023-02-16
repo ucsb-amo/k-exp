28c245b672a1bee9eb10a30e58742e92cd40adaa
jpagett <pagett.jared@gmail.com>
2023-02-15 18:22:08 -0800

    fix init

--- kexp/control/basler/BaslerUSB.py
+++ kexp/control/basler/BaslerUSB.py
@@ -1,12 +1,9 @@
 from pypylon import pylon
 
-class BaslerUSB(pylon.InstantCamera()):
+class BaslerUSB(pylon.InstantCamera):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
 
         tl_factory = pylon.TlFactory.GetInstance()
         self.Attach(tl_factory.CreateFirstDevice())
-        
-
-
         
