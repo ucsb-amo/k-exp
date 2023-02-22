3b91f7c443cbcc65ea3f5ed6a386362a2a315169
jpagett <pagett.jared@gmail.com>
2023-02-22 12:37:43 -0800

    new idle kernel

---
+++ kexp/expt/idle.py
@@ -0,0 +1,12 @@
+from artiq.experiment import *
+
+class IdleKernel(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+
+    @kernel
+    def run(self):
+        start_time = now_mu() + self.core.seconds_to_mu(500*ms)
+        while self.core.get_rtio_counter_mu() < start_time:
+            pass
+        self.core.reset()
