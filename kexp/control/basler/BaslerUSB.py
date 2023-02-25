9a875e07f8179d2762cd3f4f71900ca571bf3cc1
jpagett <pagett.jared@gmail.com>
2023-02-25 11:04:42 -0800

    update settings init, remove broken grab script

--- kexp/control/basler/BaslerUSB.py
+++ kexp/control/basler/BaslerUSB.py
@@ -1,4 +1,5 @@
 from pypylon import pylon
+from kexp.control.basler.TriggeredImage import TriggeredImage
 
 class BaslerUSB(pylon.InstantCamera):
     '''
@@ -11,40 +12,53 @@ class BaslerUSB(pylon.InstantCamera):
         TriggerMode (str): picks whether or not the camera waits for a trigger to capture frames. (default: 'On')
     '''
     def __init__(self,ExposureTime=0.,TriggerSource='Line1',TriggerMode='On'):
+
         super().__init__()
 
         tl_factory = pylon.TlFactory.GetInstance()
         self.Attach(tl_factory.CreateFirstDevice())
 
         self.Open()
-        self.TriggerMode.SetValue(TriggerMode)
-        self.TriggerSource.SetValue(TriggerSource)
+
+        self.UserSetSelector = "Default"
+        self.UserSetLoad.Execute()
+
+        self.LineSelector = TriggerSource
+        self.LineMode = "Input"
+
+        self.TriggerSelector = "FrameStart"
+        self.TriggerMode = TriggerMode
+        self.TriggerSource = TriggerSource
         if ExposureTime < self.ExposureTime.GetMin():
             ExposureTime = self.ExposureTime.GetMin()
         self.ExposureTime.SetValue(ExposureTime)
-        self.Close()
 
-    def grab_N_images(self,N=1,timeout_us=5000):
-        '''Grabs N images from the camera, then closes the camera connection.
+    # def grab_N_images(self,N=1,timeout_us=5000):
+    #     '''Grabs N images from the camera, then closes the camera connection.
         
-        Args:
-            N (int): the number of frames to be grabbed from the buffer. (default: 1)
-            timeout_us (int): The time in microseconds to wait before throwing a timeout exception. (default: 5000)
-        '''
-        images = []
-        self.StartGrabbing(pylon.GrabStrategy_OneByOne)
-        count = 0
-        while self.IsGrabbing():
-            grab = self.RetrieveResult(timeout_us,pylon.TimeoutHandling_ThrowException)
-            if grab.GrabSucceeded():
-                img = grab.GetArray()
-                images.append(img)
-                count += 1
-            grab.Release()
-            if count >= N:
-                break
-        self.Close()
-        return images
-        
+    #     Args:
+    #         N (int): the number of frames to be grabbed from the buffer. (default: 1)
+    #         timeout_us (int): The time in microseconds to wait before throwing a timeout exception. (default: 5000)
+    #     '''
+    #     images = []
+    #     self.StartGrabbing(pylon.GrabStrategy_OneByOne)
+    #     count = 0
+    #     while self.IsGrabbing():
+    #         grab = self.RetrieveResult(timeout_us,pylon.TimeoutHandling_ThrowException)
+    #         if grab.GrabSucceeded():
+    #             img = grab.GetArray()
+    #             images.append(img)
+    #             count += 1
+    #         grab.Release()
+    #         if count >= N:
+    #             break
+    #     self.Close()
+    #     return images
+    
+    # def clear_buffer(self,timeout=5000):
+    #     self.Open()
+    #     while self.NumReadyBuffers.GetValue() > 0:
+    #         self.RetrieveResult(timeout, pylon.TimeoutHandling_Return)
+    #     self.Close()
         
         
