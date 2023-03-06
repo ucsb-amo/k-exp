518fb0f078a8cb1deb700a17c5bf8738ad8f9836
jpagett <pagett.jared@gmail.com>
2023-03-06 15:26:24 -0800

    startup kernel docstrings

--- kexp/expt/startup/startup.py
+++ kexp/expt/startup/startup.py
@@ -1,10 +1,12 @@
 from artiq.experiment import *
-from artiq.experiment import delay, parallel, sequential
 from wax.config.config_dds import defaults as default_dds
 
 class Startup(EnvExperiment):
 
     def read_dds_from_config(self):
+        '''
+        Generate list of dds objects from defaults file, get device drivers for each
+        '''
         self.dds = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
         for dds0 in default_dds:
             dds0.dds_device = self.get_device(dds0.name())
@@ -12,29 +14,37 @@ class Startup(EnvExperiment):
 
     @kernel
     def set_and_turn_on_dds(self):
+        '''
+        Loops over list of dds objects, sets dds for each.
+        '''
         for dds_sublist in self.dds:
             for dds in dds_sublist:
                 dds.set_dds()
-                delay(10*us)
-
     @kernel
     def init_all_dds(self):
+        '''
+        Loops over list of dds objects, init dds (and cpld) for each.
+        '''
         for dds_sublist in self.dds:
             for dds in dds_sublist:
                 dds.init_dds()
 
     def build(self):
+        '''
+        Get core device, dds, zotino drivers.
+        '''
         self.setattr_device("core")
         self.read_dds_from_config()
-
         self.setattr_device("zotino0")
 
     @kernel
     def run(self):
+        '''
+        Init all devices, set dds to default values and turn on
+        '''
         self.core.reset()
+        self.zotino0.init()
         self.init_all_dds()
         self.set_and_turn_on_dds()
-        self.zotino0.init()
         
-
     
