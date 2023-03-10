a222a4eccc12fbe008d5330e7650793ae1b264a2
jpagett <pagett.jared@gmail.com>
2023-03-09 16:45:13 -0800

    Update basler_usb.py

--- kexp/control/cameras/basler_usb.py
+++ kexp/control/cameras/basler_usb.py
@@ -7,13 +7,15 @@ class BaslerUSB(pylon.InstantCamera):
     Excercise caution if multiple cameras are connected.
 
     Args:
-        ExposureTime (float): the exposure time in us. If below the minimum for the connected camera, sets to minimum value. (default: 0.)
+        ExposureTime (float): the exposure time in s. If below the minimum for the connected camera, sets to minimum value. (default: 0.)
         TriggerSource (str): picks the line that the camera triggers on. (default: 'Line1')
         TriggerMode (str): picks whether or not the camera waits for a trigger to capture frames. (default: 'On')
     '''
     def __init__(self,ExposureTime=0.,TriggerSource='Line1',TriggerMode='On'):
 
         super().__init__()
+
+        ExposureTime_us = ExposureTime * 1.e6
 
         tl_factory = pylon.TlFactory.GetInstance()
         self.Attach(tl_factory.CreateFirstDevice())
