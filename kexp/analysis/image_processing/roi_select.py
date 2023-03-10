29f628dc09400e2f88430c6db167a95a8916664d
jpagett <pagett.jared@gmail.com>
2023-03-09 16:01:17 -0800

    defaults

--- kexp/analysis/image_processing/roi_select.py
+++ kexp/analysis/image_processing/roi_select.py
@@ -1,13 +1,14 @@
-def crop_OD(OD,roi_type='mot'):
-    if roi_type == 'full':
+def crop_OD(OD,crop_type='mot'):
+
+    if crop_type == 'mot':
+        roix = [850,1450]
+        roiy = [250,1200]
+    elif crop_type == 'gm':
+        roix = [850,1450]
+        roiy = [250,1200]
+    else:
         roix = [0,np.shape(OD)[1]-1]
         roiy = [0,np.shape(OD)[0]-1]
-    if roi_type == 'mot':
-        roix = [850,1450]
-        roiy = [250,1200]
-    elif roi_type == 'gm':
-        roix = [850,1450]
-        roiy = [250,1200]
 
     cropOD = OD[ roiy[0]:roiy[1], roix[0]:roix[1] ]
     return cropOD
