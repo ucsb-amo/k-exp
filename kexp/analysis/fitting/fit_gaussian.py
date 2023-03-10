ce07d5d5204c6f067e2b596df5dd8675af6897e1
jpagett <pagett.jared@gmail.com>
2023-03-10 11:26:55 -0800

    make fit class better, guesses

--- kexp/analysis/fitting/fit_gaussian.py
+++ kexp/analysis/fitting/fit_gaussian.py
@@ -2,60 +2,67 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 class GaussianFit():
-    def __init__(self,amplitude,sigma,center,offset,xaxis,):
+    def __init__(self,xdata,ydata):
+        self.xdata = xdata
+        self.ydata = ydata
+
+        amplitude, sigma, x_center, y_offset = self.fit(xdata,ydata)
         self.amplitude = amplitude
         self.sigma = sigma
-        self.center = center
-        self.offset = offset
-        self.xaxis = xaxis
+        self.x_center = x_center
+        self.y_offset = y_offset
 
-    # def _fit_func()
+        self.y_fitdata = self._fit_func(xdata,amplitude,sigma,x_center,y_offset)
 
-def _gauss(x, amplitude, sigma, x0, offset):
-    return offset + amplitude * np.exp( -(x-x0)**2 / (2 * sigma**2) )
+    def _fit_func(self, x, amplitude, sigma, x_center, y_offset):
+        return y_offset + amplitude * np.exp( -(x-x_center)**2 / (2 * sigma**2) )
 
-def _gauss_fit(x, y):
-    '''
-    Returns the gaussian fit parameters for y(x).
+    def _fit(self, x, y):
+        '''
+        Returns the gaussian fit parameters for y(x).
 
-    Fit equation: offset + amplitude * np.exp( -(x-x0)**2 / (2 * sigma**2) )
+        Fit equation: offset + amplitude * np.exp( -(x-x0)**2 / (2 * sigma**2) )
 
-    Parameters
-    ----------
-    x: ArrayLike
-    y: ArrayLike
+        Parameters
+        ----------
+        x: ArrayLike
+        y: ArrayLike
 
-    Returns
-    -------
-    amplitude: float
-    sigma: float
-    x0: float
-    offset: float
-    '''
-    mean = sum(x * y) / sum(y)
-    sigma = np.sqrt(sum(y * (x - mean) ** 2) / sum(y))
-    popt, pcov = curve_fit(_gauss, x, y, p0=[min(y), max(y), mean, sigma])
-    return popt
+        Returns
+        -------
+        amplitude: float
+        sigma: float
+        x0: float
+        offset: float
+        '''
+        amplitude_guess = np.max(y) - np.min(y)
+        x_center_guess = x[np.argmax(y)]
+        sigma_guess = np.abs(x_center_guess - x[np.argmax(np.abs(y - 0.65*np.max(y)))])
+        y_offset_guess = np.min(y)
+        popt, pcov = curve_fit(self._fit_func, x, y,
+                                p0=[amplitude_guess, sigma_guess, x_center_guess, y_offset_guess],
+                                bounds=((0,0,-np.inf,0),(np.inf,np.inf,np.inf,np.inf)))
+        return popt
 
-def gauss_fit(x,y):
-    '''
-    Returns a GaussianFit object containing the gaussian fit parameters for
-    y(x).
+    def fit(self,x,y):
+        '''
+        Returns the gaussian fit parameters for y(x).
 
-    Fit equation: offset + amplitude * np.exp( -(x-x0)**2 / (2 * sigma**2) )
+        Compared to `fit`, explicitly unpacks the output.
 
-    Parameters
-    ----------
-    x: ArrayLike
-    y: ArrayLike
+        Fit equation: offset + amplitude * np.exp( -(x-x0)**2 / (2 * sigma**2) )
 
-    Returns
-    -------
-    amplitude: float
-    sigma: float
-    x0: float
-    offset: float
-    '''
-    amplitude, sigma, x0, offset = _gauss_fit(x,y)
-    fit = GaussianFit(amplitude, sigma, x0, offset, xaxis=x)
-    return fit
+        Parameters
+        ----------
+        x: ArrayLike
+        y: ArrayLike
+
+        Returns
+        -------
+        amplitude: float
+        sigma: float
+        x0: float
+        offset: float
+        '''
+        amplitude, sigma, x_center, y_offset = self._fit(x,y)
+        return amplitude, sigma, x_center, y_offset
