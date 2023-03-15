5a9b94f16e5b1c4a41fe1f8ce79e9fa1b92abd60
jpagett <pagett.jared@gmail.com>
2023-03-14 19:47:37 -0700

    dict of lists to list of dicts

--- kexp/config/dds_id.py
+++ kexp/config/dds_id.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 def dds_id(uru_idx,ch_idx):
     ids = define_dds_id()
-    name = ids["name"][uru_idx][ch_idx]
-    aom_order = ids["aom_order"][uru_idx][ch_idx]
+    name = ids[uru_idx][ch_idx]["name"]
+    aom_order = ids[uru_idx][ch_idx]["aom_order"]
     return name, aom_order
     
 def define_dds_id():
@@ -10,28 +12,19 @@ def define_dds_id():
     aom order that is used for each AOM here.
     '''
 
-    dds_ids = dict()
-    names = []
-    orders = []
+    N_uru = 2
+    N_ch = 4
 
-    names.append([
-        "push",
-        "d2_2d_r",
-        "d2_2d_c",
-        "d2_3d_r"
-        ])
-    orders.append([1,1,-1,1])
+    dds_ids = [[{} for _ in range(N_ch)] for _ in range(N_uru)]
 
-    names.append([
-        "d2_3d_c",
-        "imaging",
-        "d1_3d_r",
-        "d1_3d_c",
-        ])
-    orders.append([-1,1,1,-1])
-
-    dds_ids["name"] = names
-    dds_ids["aom_order"] = orders
+    dds_ids[0][0] = {"names": "push", "aom_order": 1}
+    dds_ids[0][1] = {"names": "d2_2d_r", "aom_order": 1}
+    dds_ids[0][2] = {"names": "d2_2d_c", "aom_order": -1}
+    dds_ids[0][3] = {"names": "d2_3d_r", "aom_order": 1}
+    dds_ids[1][0] = {"names": "d2_3d_c", "aom_order": -1}
+    dds_ids[1][1] = {"names": "imaging", "aom_order": 1}
+    dds_ids[1][2] = {"names": "d1_3d_r", "aom_order": 1}
+    dds_ids[1][3] = {"names": "d1_3d_c", "aom_order": -1}
 
     return dds_ids
 
