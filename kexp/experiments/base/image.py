3598c0c57a3644bed224c16732a1d67be5b2e403
jpagett <pagett.jared@gmail.com>
2023-03-15 10:04:54 -0700

    image to class

--- kexp/experiments/base/image.py
+++ kexp/experiments/base/image.py
@@ -1,21 +1,25 @@
 from artiq.experiment import *
 from artiq.experiment import delay, parallel, sequential
 
-@kernel
-def pulse_imaging_light(expt,t):
-    expt.dds.get("imaging").on()
-    delay(t)
-    expt.dds.get("imaging").off()
+class image():
+    def __init__():
+        pass
 
-@kernel
-def trigger_camera(expt, params):
-    '''
-    Written to pretrigger camera such that the camera exposure begins at the
-    timeline cursor position where this is called. Returns the timeline
-    cursor to this position after pretrigger.
-    '''
-    delay(-params.t_pretrigger * s)
-    expt.ttl_camera.pulse(params.t_camera_trigger * s)
-    t_adv = params.t_pretrigger - params.t_camera_trigger
-    delay(t_adv * s)
+    @kernel
+    def pulse_imaging_light(self,t):
+        self.dds.get("imaging").on()
+        delay(t)
+        self.dds.get("imaging").off()
+
+    @kernel
+    def trigger_camera(self, params):
+        '''
+        Written to pretrigger camera such that the camera exposure begins at the
+        timeline cursor position where this is called. Returns the timeline
+        cursor to this position after pretrigger.
+        '''
+        delay(-params.t_pretrigger * s)
+        self.ttl_camera.pulse(params.t_camera_trigger * s)
+        t_adv = params.t_pretrigger - params.t_camera_trigger
+        delay(t_adv * s)
 
