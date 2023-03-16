aa8e8684ba6b9eaf1f530e5d28bf439dea6d5bdb
jpagett <pagett.jared@gmail.com>
2023-03-16 12:46:58 -0700

    reorg

--- kexp/base/sub/devices.py
+++ kexp/base/sub/devices.py
@@ -1,22 +1,13 @@
 from artiq.experiment import *
-from artiq.experiment import delay
+from artiq.experiment import delay, sequential, parallel
 
 from kexp.config.dds_id import dds_frame
 from kexp.control.artiq.DDS import DDS
+from kexp.config.expt_params import ExptParams
 
 class devices():
     def __init__(self):
         pass
-
-    @kernel
-    def set_all_dds(self, state=0):
-        for dds in self.dds_list:
-            dds.set_dds()
-            if state == 0:
-                dds.off()
-            elif state == 1:
-                dds.on()
-            delay(10*us)
 
     def prepare_devices(self):
 
@@ -25,10 +16,19 @@ class devices():
 
         self.dds = dds_frame()
         self.dds.get_dds_devices(self)
+        self.dds_list = self.dds.dds_list()
 
         self.dac_ch_3Dmot_current_control = 0
 
         self.ttl_camera = self.get_device("ttl4")
 
-        self.dds_list = self.dds.dds_list()
+    @kernel
+    def set_all_dds(self, state):
+        for dds in self.dds_list:
+            dds.set_dds()
+            if state == 0:
+                dds.off()
+            elif state == 1:
+                dds.on()
+            delay(10*us)
     
