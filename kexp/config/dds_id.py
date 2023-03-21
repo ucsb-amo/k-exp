0ee6d5684f1e4d6cd2392fb33a63246da9a66af9
jpagett <pagett.jared@gmail.com>
2023-03-21 11:01:47 -0700

    r and c aom order relabeled

--- kexp/config/dds_id.py
+++ kexp/config/dds_id.py
@@ -30,8 +30,8 @@ class dds_frame():
         self._aom_order[0][3] = 1
         self._aom_order[1][0] = -1
         self._aom_order[1][1] = 1
-        self._aom_order[1][2] = 1
-        self._aom_order[1][3] = -1
+        self._aom_order[1][2] = -1
+        self._aom_order[1][3] = 1
 
         # self.aom_name = self.dds_assign(urukul_idx,ch_idx)
         self.push = self.dds_assign(0,0)
