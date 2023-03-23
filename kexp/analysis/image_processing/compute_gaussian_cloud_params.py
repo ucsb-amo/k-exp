8170160342df0d8ec0933cb966e892db5a4ed155
jpagett <pagett.jared@gmail.com>
2023-03-22 17:11:50 -0700

    Revert "type suppression"
    This reverts commit 9e7d0145c10862e355ca650253d6a965f4c4fa34.

--- kexp/analysis/image_processing/compute_gaussian_cloud_params.py
+++ kexp/analysis/image_processing/compute_gaussian_cloud_params.py
@@ -2,7 +2,7 @@ import numpy as np
 import kexp.config.camera_params as cam
 import kexp.analysis.fitting.gaussian as gfit
 
-def fit_gaussian_sum_OD(sum_od):
+def fit_gaussian_sum_OD(sum_od) -> gfit.GaussianFit:
     '''
     Performs a guassian fit on each summedOD in the input list.
 
