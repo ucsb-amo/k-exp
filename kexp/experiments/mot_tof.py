848882c99629e664590de21ac1400cf2050c1efe
jpagett <pagett.jared@gmail.com>
2023-03-24 14:30:49 -0700

    Update mot_tof.py

--- kexp/experiments/mot_tof.py
+++ kexp/experiments/mot_tof.py
@@ -16,7 +16,7 @@ class mot_tof(EnvExperiment, Base):
 
         self.p.t_mot_kill = 1
         self.p.t_mot_load = 0.25
-        self.p.t_tof = np.linspace(0,1000,3) * 1.e-6
+        self.p.t_tof = np.linspace(0,1000,15) * 1.e-6
         self.p.N_img = 3 * len(self.p.t_tof)
 
     @kernel
