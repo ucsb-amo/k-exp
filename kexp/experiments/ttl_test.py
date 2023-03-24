a1e1a482da7617866e01db79183c418a9bb20bd3
jpagett <pagett.jared@gmail.com>
2023-03-24 14:30:46 -0700

    Update ttl_test.py

--- kexp/experiments/ttl_test.py
+++ kexp/experiments/ttl_test.py
@@ -1,21 +1,15 @@
 from artiq.experiment import *
-
 class TestTTL(EnvExperiment):
-
     def build(self):
         self.core = self.get_device("core")
-        self.ttl = self.get_device("ttl5")
-
-        self.T = 10.e-6
-
+        self.ttl = self.get_device("ttl13")
+        self.t = 1
     @kernel
     def run(self):
-
         self.core.reset()
         self.core.break_realtime()
-
         for _ in range(5):
             self.ttl.on()
-            delay(self.T*s)
+            delay(self.t * s)
             self.ttl.off()
-            delay(self.T*s)
+            delay(self.t * s)
