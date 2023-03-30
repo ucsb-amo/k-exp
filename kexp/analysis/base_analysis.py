36d3c6d84d4b8c47bf6b52e367d68e33f394f24d
jpagett <pagett.jared@gmail.com>
2023-03-30 12:25:37 -0700

    remove image args

--- kexp/analysis/base_analysis.py
+++ kexp/analysis/base_analysis.py
@@ -1,6 +1,9 @@
 from kexp.analysis.image_processing.compute_ODs import *
 from kexp.analysis.image_processing.compute_gaussian_cloud_params import fit_gaussian_sum_OD
 
+import os
+data_dir = os.getenv("data")
+
 class atomdata():
     '''
     Use to store and do basic analysis on data for every experiment.
@@ -9,19 +12,12 @@ class atomdata():
 
     This class also handles saving parameters from expt.params to the dataset.
     '''
-    def __init__(self, expt=[], crop_type='mot', img_atoms=[], img_light=[], img_dark=[]):
+    def __init__(self, expt=[], crop_type='mot'):
         
-        if expt != []:
-            self._expt = expt
-            self._images = expt.images
-            self._img_timestamps = expt.image_timestamps
-            self._split_images()
-        elif expt == []:
-            self.img_atoms = img_atoms
-            self.img_light = img_light
-            self.img_dark = img_dark
-        else:
-            raise ValueError("No experiment object or image lists provided to initialize atomdata.")
+        self._expt = expt
+        self._images = expt.images
+        self._img_timestamps = expt.image_timestamps
+        self._split_images()
 
         self.od_raw = []
         self.od = []
@@ -70,18 +66,7 @@ class atomdata():
         This function also handles saving parameters from expt.params to the dataset.
         '''
         print("Saving data...")
-        try:
-            param_keys = list(vars(self))
-            important_param_keys = [p for p in param_keys if not p.startswith("_")]
-            for key in important_param_keys:
-                value = vars(self)[key]
-                self._expt.set_dataset(key, value)
-        except Exception as e: 
-            print(e)
-        print("Done saving data!")
 
-        print("Saving parameters...")
-        self._expt.params.params_to_dataset(self._expt)
         print("Done saving parameters!")
 
     def _split_images(self):
@@ -97,3 +82,5 @@ class atomdata():
         self.img_atoms_tstamp = self._img_timestamps[atom_img_idx::3]
         self.img_light_tstamp = self._img_timestamps[light_img_idx::3]
         self.img_dark_tstamp = self._img_timestamps[dark_img_idx::3]
+
+# class DataVault():
