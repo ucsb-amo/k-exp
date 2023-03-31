9b1f084000af84686d891b1375f271471396ed37
bowjun <66513176+bowjun@users.noreply.github.com>
2023-03-31 15:45:46 -0700

    update to work with nd ods

--- kexp/analysis/image_processing/roi_select.py
+++ kexp/analysis/image_processing/roi_select.py
@@ -1,13 +1,23 @@
 import numpy as np
 
-def crop_OD(OD,crop_type=''):
+def crop_OD(OD,crop_type='',Nvars=1):
+
+    if len(np.shape(OD)) <= 2:
+        OD = [OD]
 
     if crop_type == 'mot':
         roix = [500, 1600]
         roiy = [50, 1050]
+    elif crop_type == 'test':
+        roix = [3,6]
+        roiy = [5,9]
     else:
         roix = [0,np.shape(OD)[1]-1]
         roiy = [0,np.shape(OD)[0]-1]
 
-    cropOD = OD[ roiy[0]:roiy[1], roix[0]:roix[1] ]
+    if Nvars == 1:
+        cropOD = OD[:, roiy[0]:roiy[1], roix[0]:roix[1] ]
+    if Nvars == 2:
+        cropOD = OD[:, :, roiy[0]:roiy[1], roix[0]:roix[1] ]
+
     return cropOD
