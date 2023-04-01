ae2761b37d2c5258227d91e55cc5aa813c65740f
jpagett <pagett.jared@gmail.com>
2023-03-31 17:11:09 -0700

    Revert "Revert "Jp n dim scan""

--- kexp/analysis/image_processing/roi_select.py
+++ kexp/analysis/image_processing/roi_select.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-def crop_OD(OD,crop_type=''):
+def crop_OD(OD,crop_type='',Nvars=1):
+
+    if len(np.shape(OD)) <= 2:
+        OD = [OD]
 
     if crop_type == 'mot':
         roix = [500, 1600]
@@ -9,5 +12,9 @@ def crop_OD(OD,crop_type=''):
         roix = [0,np.shape(OD)[1]-1]
         roiy = [0,np.shape(OD)[0]-1]
 
-    cropOD = OD[ roiy[0]:roiy[1], roix[0]:roix[1] ]
+    if Nvars == 1:
+        cropOD = OD[:, roiy[0]:roiy[1], roix[0]:roix[1] ]
+    if Nvars == 2:
+        cropOD = OD[:, :, roiy[0]:roiy[1], roix[0]:roix[1] ]
+
     return cropOD
