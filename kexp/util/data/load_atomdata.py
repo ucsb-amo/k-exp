49fa1cdfc4581968597fbbe40f7ca35b61309b50
jpagett <pagett.jared@gmail.com>
2023-04-03 11:33:32 -0700

    load atomdata by idx

--- kexp/util/data/load_atomdata.py
+++ kexp/util/data/load_atomdata.py
@@ -6,11 +6,28 @@ from kexp.analysis.base_analysis import atomdata
 
 data_dir = os.getenv("data")
 
-def load_atomdata(path = []) -> atomdata:
+def load_atomdata(idx=0, path = []) -> atomdata:
+    '''
+    Returns the atomdata stored in the `idx`th newest pickle file at `path`.
+
+    Parameters
+    ----------
+    idx: int
+        The index of the pickle file to be loaded, relative to the latest file
+        (idx=0). (default: idx = 0)
+    path: str
+        The full path to the file to be loaded. If not specified, loads the file
+        as dictated by `idx`.
+
+    Returns
+    -------
+    ad: atomdata
+    '''
     if path == []:
         folderpath=os.path.join(data_dir,'*','*.pickle')
         list_of_files = glob.glob(folderpath)
-        file = max(list_of_files, key=os.path.getmtime)
+        list_of_files.sort(key=lambda x: os.path.getmtime(x))
+        file = list_of_files[-(idx+1)]
     else:
         if path.endswith('.pickle'):
             file = path
