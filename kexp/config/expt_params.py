19349f464b910ce8b11c3a41fd29790e763a8a7b
jpagett <pagett.jared@gmail.com>
2023-04-04 11:15:01 -0700

    rm unused method

--- kexp/config/expt_params.py
+++ kexp/config/expt_params.py
@@ -20,13 +20,4 @@ class ExptParams():
 
         self.N_shots = 2
         self.N_repeats = 1
-
-    def params_to_dataset(self,expt):
-        try:
-            param_keys = list(vars(self))
-            for key in param_keys:
-                value = vars(self)[key]
-                expt.set_dataset(key, value)
-        except Exception as e: 
-            print(e)
             
