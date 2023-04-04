d519445c9509b55219f59ecad8e49a71bb05ae5b
jpagett <pagett.jared@gmail.com>
2023-04-04 12:46:23 -0700

    expand run info contents

--- kexp/util/data/run_info.py
+++ kexp/util/data/run_info.py
@@ -6,7 +6,17 @@ class RunInfo():
         _ds = DataSaver()
         self.run_id = _ds._get_rid()
         self.run_datetime = time.localtime(time.time())
+
+        self._run_description = ""
+
+        date = self.run_datetime
+        
+        self.run_date_str = time.strftime("%Y-%m-%d", date)
+        self.run_datetime_str = time.strftime("%Y-%m-%d_%H-%M-%S", date)
+
         self.filepath = []
+        self.xvarnames = []
+
         if expt_obj is not None:
             self.expt_class = expt_obj.__class__.__name__
         else:
