9f5afb6333bd6d112d0acd8ada4e93b62d7725c0
jpagett <pagett.jared@gmail.com>
2023-04-04 08:36:00 -0700

    option to get experiment class

--- kexp/util/data/run_info.py
+++ kexp/util/data/run_info.py
@@ -2,8 +2,13 @@ from kexp.util.data.data_vault import DataSaver
 import time
 
 class RunInfo():
-    def __init__(self):
+    def __init__(self,expt_obj=None):
         _ds = DataSaver()
         self.run_id = _ds._get_rid()
         self.run_datetime = time.localtime(time.time())
         self.filepath = []
+        if expt_obj not None:
+            expt_class = expt_obj.__class__.__name__
+        else:
+            expt_class = "expt"
+            
