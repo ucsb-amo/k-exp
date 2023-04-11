ee51ab176cbea4062e1876e28e529548e4bd246a
jpagett <pagett.jared@gmail.com>
2023-04-11 14:56:42 -0700

    more inits

--- kexp/__init__.py
+++ kexp/__init__.py
@@ -1,1 +1,4 @@
-from kexp.base.base import Base
+from .base import Base
+from .config import dds_frame
+from .analysis import atomdata
+from .util import load_atomdata, device_db
--- kexp/util/__init__.py
+++ kexp/util/__init__.py
@@ -4,3 +4,4 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
 from .data import load_atomdata
+from .db import device_db
