c69d5b37c1cd863d93e9aac6db6f644d63473f59
bowjun <66513176+bowjun@users.noreply.github.com>
2023-04-12 13:43:21 -0700

    Update cooling.py

--- kexp/base/sub/cooling.py
+++ kexp/base/sub/cooling.py
@@ -2,6 +2,7 @@ from artiq.experiment import *
 from artiq.experiment import delay, delay_mu, parallel, sequential
 from kexp.config.dds_id import dds_frame
 from kexp.config.expt_params import ExptParams
+import numpy as np
 
 class Cooling():
     def __init__(self):
@@ -93,6 +94,32 @@ class Cooling():
             self.switch_d1_3d(1)
             self.switch_d2_3d(0)
         delay(t)
+
+    #GM with only D1, turning B field off
+    @kernel
+    def gm_ramp(self,t,att_c_i,att_c_f,att_r_i,att_r_f):
+        delay(-10*us)
+        self.dds.d1_3d_r.set_dds_gamma(delta=self.p.detune_d1_r_gm, 
+                                       att_dB=self.p.att_d1_r_gm)
+        delay_mu(self.p.t_rtio_mu)
+        self.dds.d1_3d_c.set_dds_gamma(delta=self.p.detune_d1_c_gm, 
+                                       att_dB=self.p.att_d1_c_gm)
+        delay(10*us)
+        with parallel:
+            self.switch_mot_magnet(0)
+            self.switch_d1_3d(1)
+            self.switch_d2_3d(0)
+
+        dt = 8.e-9 # 8 ns
+        N = np.floor(t/dt)
+        att_list_c = np.linspace(att_c_i,att_c_f,N)
+        att_list_r = np.linspace(att_r_i,att_r_f,N)
+        for i in range(N):
+            with parallel:
+                self.dds.d1_3d_c.dds_device.set_att(att_list_c[i])
+                self.dds.d1_3d_r.dds_device.set_att(att_list_r[i])
+            delay(dt)
+        # delay(t)
 
     @kernel
     def release(self):
