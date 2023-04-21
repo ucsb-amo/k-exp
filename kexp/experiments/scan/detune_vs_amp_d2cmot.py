d2bdf08d4b755baad472a67b3b019e66c7775f92
jpagett <pagett.jared@gmail.com>
2023-04-21 10:27:59 -0700

    scans and new optimal parameterse

--- kexp/experiments/scan/detune_vs_amp_d2cmot.py
+++ kexp/experiments/scan/detune_vs_amp_d2cmot.py
@@ -3,12 +3,12 @@ from artiq.experiment import delay, parallel, sequential, delay_mu
 from kexp import Base
 import numpy as np
 
-class detune_scan_gm(EnvExperiment, Base):
+class detune_vs_amp_d2cmot(EnvExperiment, Base):
 
     def build(self):
         Base.__init__(self)
 
-        self.run_info._run_description = "detune vs amp scan gm, two-photon detuning = 0"
+        self.run_info._run_description = "d2 cmot detune vs field gradient"
 
         ## Parameters
 
@@ -17,42 +17,35 @@ class detune_scan_gm(EnvExperiment, Base):
         self.p.t_mot_kill = 1
         self.p.t_mot_load = 3
 
-        self.t_d2cmot = 5.e-3
-        self.t_d1cmot = 7.e-3
-        self.p.t_gm = 2.e-3
+        # self.p.N_shots = 4
+        self.p.N_repeats = 1
 
-        self.p.t_tof = 2.e-3
+        self.p.t_tof = 750.e-6
 
-        #MOT detunings
+        #MOT detuning
 
-        # scan_center = 4.8
+        self.p.detune_d2_c_d2cmot = np.linspace(-0.0,-1.75,8)
+        self.p.detune_d2_r_d2cmot = self.p.detune_d2_c_d2cmot + -2.
+        self.p.V_d2cmot_current = np.linspace(0.7,2.5,10)
 
-        self.p.detune_d1_gm = np.linspace(4.5,6.5,7)
-        self.p.detune_d1_c_gm = self.p.detune_d1_gm
-        self.p.detune_d1_r_gm = self.p.detune_d1_gm
-
-        self.p.amp_d1_gm = np.linspace(0.09,0.12,7)
-        self.p.amp_d1_c_gm = self.p.amp_d1_gm
-        self.p.amp_d1_r_gm = self.p.amp_d1_gm
-
-        self.xvarnames = ['detune_d1_gm','amp_d1_gm']
+        self.xvarnames = ['detune_d2_c_d2cmot','V_d2cmot_current']
 
         self.get_N_img()
 
-    #GM with only D1, turning B field off
     @kernel
-    def gm(self,t,delta,amp):
+    def cmot_d2(self,t,delta,V):
         delay(-10*us)
-        self.dds.d1_3d_c.set_dds_gamma(delta=delta, 
-                                       amplitude=amp)
-        delay_mu(self.params.t_rtio_mu)
-        self.dds.d1_3d_r.set_dds_gamma(delta=delta, 
-                                       amplitude=amp)
+        self.dds.d2_3d_c.set_dds_gamma(delta=delta,
+                                    amplitude=self.params.amp_d2_c_d2cmot)
+        self.dds.d2_3d_r.set_dds_gamma(delta=delta-2.,
+                                    amplitude=self.params.amp_d2_r_d2cmot)
         delay(10*us)
         with parallel:
-            self.switch_mot_magnet(0)
-            self.switch_d1_3d(1)
-            self.switch_d2_3d(0)
+            self.switch_d2_3d(1)
+            with sequential:
+                self.zotino.write_dac(self.dac_ch_3Dmot_current_control,
+                                    V)
+                self.zotino.load()
         delay(t)
 
     @kernel
@@ -65,8 +58,8 @@ class detune_scan_gm(EnvExperiment, Base):
         
         self.kill_mot(self.p.t_mot_kill * s)
 
-        for delta in self.p.detune_d1_gm:
-            for amp in self.p.amp_d1_gm:
+        for delta in self.p.detune_d2_c_d2cmot:
+            for V in self.p.V_d2cmot_current:
                 self.load_2D_mot(self.p.t_2D_mot_load_delay * s)
 
                 self.mot(self.p.t_mot_load * s)
@@ -74,10 +67,9 @@ class detune_scan_gm(EnvExperiment, Base):
                 self.dds.push.off()
                 self.switch_d2_2d(0)
 
-                self.cmot_d2(self.p.t_d2cmot)
-                self.cmot_d1(self.p.t_d1cmot)
+                self.cmot_d2(self.p.t_d2cmot, delta, V)
 
-                self.gm(self.p.t_gm, delta, amp)
+                # self.cmot_d1(self.p.t_d1cmot)
                 
                 self.release()
                 
