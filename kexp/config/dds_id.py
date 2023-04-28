96d070a18e2160e5dd05c76e5bce456acea02ac8
jpagett <pagett.jared@gmail.com>
2023-04-28 15:10:25 -0700

    add profile management to dds frame

--- kexp/config/dds_id.py
+++ kexp/config/dds_id.py
@@ -1,9 +1,18 @@
+import numpy as np
+
+from artiq.coredevice import ad9910
+
 from kexp.config.dds_state import dds_state
 from kexp.control.artiq.DDS import DDS
+from kexp.config.dds_calibration import DDS_Calibration
+
+from jax import AD9910Manager, RAMProfile, RAMType
 
 N_uru = 2
 N_ch = 4
 shape = (N_uru,N_ch)
+
+RAMP_STEP_TIME = 100 * 4.e-9
 
 def dds_empty_frame():
     return [[None for _ in range(N_ch)] for _ in range(N_uru)]
@@ -15,6 +24,9 @@ class dds_frame():
     determined from detunings.
     '''
     def __init__(self, dds_state = dds_state):
+
+        self.dds_manager = AD9910Manager
+        self.dds_calibration = DDS_Calibration()
 
         self._N_uru = N_uru
         self._N_ch = N_ch
@@ -72,5 +84,42 @@ class dds_frame():
         Returns a list of all dds objects in 
         '''
         return [self.__dict__[key] for key in self.__dict__.keys() if isinstance(self.__dict__[key],DDS)]
+        
+    def set_amplitude_profile(self, dds:DDS, t_ramp:float, amp=-1., p_i=-1., p_f=-1., dwell_end=1):
+
+        _power_specified = p_i < 0. and p_f < 0.
+        _amp_specified = amp < 0.
+        if (_power_specified and _amp_specified) or not (_power_specified or _amp_specified):
+            raise ValueError("Either initial and final power, or constant amplitude should be specified. \
+                              Either both or none were specified.")
+        
+        if _amp_specified and not _power_specified:
+            amp_list = [amp]
+        if _power_specified and not _amp_specified:
+            amp_list = self.get_amplitude_ramp_list(t_ramp,p_i,p_f)
+        
+        this_profile = RAMProfile(
+            dds, amp_list, RAMP_STEP_TIME, RAMType.AMP, ad9910.RAM_MODE_RAMPUP, dwell_end=dwell_end)
+
+        self.dds_manager.append(dds.dds_device, frequency_src=dds.frequency, amplitude_src=this_profile)
+        
+    def get_amplitude_ramp_list(self, t_ramp, power_i, power_f):
+      dt = RAMP_STEP_TIME
+      N = round(t_ramp / dt)
+      p_list = np.linspace(power_i,power_f,N)
+      amp_list = self.dds_calibration.power_fraction_to_dds_amplitude(p_list).tolist()
+      return amp_list
+    
+    @kernel
+    def enable_profile(self):
+        self.dds_manager.enable()
+        self.dds_manager.commit_enable()
+
+    @kernel
+    def disable_profile(self):
+        self.dds_manager.disable()
+        self.dds_manager.commit_disable()
+        
+
         
 
