34585f56c1c554b382c530e320dc9aa560e9ba2d
jpagett <pagett.jared@gmail.com>
2023-05-03 15:12:27 -0700

    add unshuffle arg to load ad

--- kexp/util/data/load_atomdata.py
+++ kexp/util/data/load_atomdata.py
@@ -8,7 +8,7 @@ from kexp.util.data.run_info import RunInfo
 
 data_dir = os.getenv("data")
 
-def load_atomdata(idx=0,path = [],crop_type='mot') -> atomdata:
+def load_atomdata(idx=0,path = [],unshuffle_xvars=True,crop_type='mot') -> atomdata:
     '''
     Returns the atomdata stored in the `idx`th newest pickle file at `path`.
 
@@ -55,7 +55,8 @@ def load_atomdata(idx=0,path = [],crop_type='mot') -> atomdata:
     image_timestamps = f['data']['image_timestamps'][()]
     xvarnames = f.attrs['xvarnames'][()]
 
-    ad = atomdata(xvarnames,images,image_timestamps,params,run_info,crop_type=crop_type)
+    ad = atomdata(xvarnames,images,image_timestamps,params,run_info,
+                  unshuffle_xvars=unshuffle_xvars,crop_type=crop_type)
 
     return ad
 
