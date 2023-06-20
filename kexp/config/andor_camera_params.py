85dbe5b4da4b11bd42939b572cb081dac733a0e7
jpagett <pagett.jared@gmail.com>
2023-06-20 14:41:19 -0700

    rm old files

--- kexp/config/andor_camera_params.py
+++
@@ -1,3 +0,0 @@
-pixel_size_m = 16.e-6
-magnification = 1. # needs to be figured out and updated
-exposure_delay = 0. # needs to be updated from docs
--- kexp/config/basler_camera_params.py
+++
@@ -1,6 +0,0 @@
-absorption_sn = '40316451'
-fluorescence_sn = '40320384'
-
-pixel_size_m = 3.45 * 1.e-6
-magnification = 0.5
-exposure_delay = 17 * 1.e-6
