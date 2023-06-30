01850fadab205f3bc0fb307004d48bf917a09e8f
jpagett <pagett.jared@gmail.com>
2023-06-29 17:55:47 -0700

    Update andor.py

--- kexp/control/cameras/andor.py
+++ kexp/control/cameras/andor.py
@@ -1,4 +1,7 @@
 from pylablib.devices import Andor
 
 class AndorEMCCD(Andor.AndorSDK2Camera):
-    
+    def __init__(self, ExposureTime=0.):
+        super().__init__()
+        self.set_exposure(ExposureTime)
+        self.set_trigger_mode("ext_start")
