67aab9f4748fcb85af9560cc374a0f0f6896d0d2
jpagett <pagett.jared@gmail.com>
2023-08-07 10:14:10 -0700

    add imaging power adjust for imaging schemes

--- kexp/config/expt_params.py
+++ kexp/config/expt_params.py
@@ -25,6 +25,8 @@ class ExptParams():
         
         #Imaging
         self.frequency_detuned_imaging = 25.78e6
+        self.amp_imaging_fluor = 0.15
+        self.amp_imaging_abs = 0.08
 
         #Cooling timing
         self.t_2D_mot_load_delay = 1.
