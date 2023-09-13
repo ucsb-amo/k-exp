14333534c7d51a155f52d0f4422b8137045c8b85
jpagett <pagett.jared@gmail.com>
2023-09-12 17:10:24 -0700

    create batch files for od watch

--- kexp/_bat/watch_ods.bat
+++
@@ -1,3 +0,0 @@
-%kpy%
-python "C:\Users\jarjarbinks\code\k-exp\kexp\analysis\preview\watch_for_ods.py"
-artiq_run "C:\Users\jarjarbinks\code\k-exp\kexp\analysis\preview\preview_experiment.py"
---
+++ kexp/_bat/watcher/watch_ods.bat
@@ -0,0 +1,2 @@
+start cmd /k %code%/k-exp/kexp/_bat/watcher/run_watcher_gui.bat
+start cmd /k %code%/k-exp/kexp/_bat/watcher/run_watcher_expt.bat
