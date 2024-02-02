4bbb20ebcb66f3dd8baeaea6599dd99254d33e06
jpagett <pagett.jared@gmail.com>
2024-02-02 14:15:47 -0800

    Update keysight_monitor_gui.py

--- kexp/util/guis/keysight_monitor/keysight_monitor_gui.py
+++ kexp/util/guis/keysight_monitor/keysight_monitor_gui.py
@@ -15,81 +15,99 @@ import vxi11
 import time
 
 T_UPDATE_MS = 250
+FONTSIZE_PT = 18
+
+# one of these per current supply
+class current_supply_widget(QWidget):
+    def __init__(self,ip:str,max_current:int):
+        super().__init__()
+        self.ip = ip
+        self.max_current = max_current
+        self.supply = vxi11.Instrument(ip)
+
+        self.init_UI()
+        
+    def read_current(self,supply):
+        # send the supply the query to measure the current
+        supply.write(":MEASure:CURRent:DC?")
+        # wait a bit for the reply to be available (probably don't need this)
+        time.sleep(0.05)
+        # read out the value that the supply sends back -- convert it to a
+        # number since it's a nasty string
+        return float(supply.read())
+    
+    def update_current_UI(self):
+        current = self.read_current(self.supply)
+        # set the value text of our box (see "init_UI") to the new current
+        # the "1.4f" formats the number to a string as with 4 decimal places (f for "float")
+        self.value_label.setText(f"{current:1.4f}") 
+    
+    def init_UI(self):
+
+        # this one gets "self" (is an attribute) since I'll need to update it
+        # later when I check the current value
+        self.value_label = QLabel("")
+        self.value_label.setStyleSheet("font-weight: bold; font-size: {FONTSIZE_PT}pt")
+
+        # these ones will remain the same forever so I just name them here and
+        # don't bother to save them as an attribute (since I don't need to
+        # reassign them later)
+        text_label = QLabel(f"{self.max_current} A supply current = ")
+        text_label.setStyleSheet("font-size: {FONTSIZE_PT}pt") # formatting
+
+        unit_label = QLabel("A")
+        unit_label.setStyleSheet("font-weight: bold; font-size: {FONTSIZE_PT}pt") # formatting
+
+        # the overall layout of this part will have widgets in a horizontal line
+        self.layout = QHBoxLayout() 
+        # now I just stack the parts of this part of the GUI into the layout in order
+        self.layout.addWidget(text_label)
+        self.layout.addWidget(self.value_label)
+        self.layout.addWidget(unit_label)
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
+
         self.init_instruments()
-        self.init_UI()
+        self.setup_timer_loop()
         self.set_layout()
 
-    def init_instruments(self):
-        self.supply_500A = vxi11.Instrument("192.168.1.78")
-        self.supply_170A = vxi11.Instrument("192.168.1.77")
-
-    def init_UI(self):
-
+    def setup_timer_loop(self):
+        # the window gets a timer object, this runs the "connected" function
+        # each time that the timer times out (exceeds T_UPDATE_MS), then runs
+        # again. effect is to run the "connected" function repeatedly every
+        # T_UPDATE_MS (defined way at top of file)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_current)
         self.timer.start(T_UPDATE_MS)
 
-        self.current_500A = QLabel("")
-        self.current_500A.setStyleSheet("font-weight: bold; font-size: 18pt")
-        self.current_500A_label = QLabel("500 A supply current = ")
-        self.current_500A_label.setStyleSheet("font-size: 18pt")
-        self.unit_500 = QLabel("A")
-        self.unit_500.setStyleSheet("font-weight: bold; font-size: 18pt")
-
-        self.current_170A = QLabel("")
-        self.current_170A.setStyleSheet("font-weight: bold; font-size: 18pt")
-        self.current_170A_label = QLabel("170 A supply current = ")
-        self.current_170A_label.setStyleSheet("font-size: 18pt")
-        self.unit_170 = QLabel("A")
-        self.unit_170.setStyleSheet("font-weight: bold; font-size: 18pt")
+    def init_instruments(self):
+        current_supplies = [(170,"192.168.1.77"),
+                            (500,"192.168.1.78")]
+        self.supply_UIs = []
+        for current, ip in current_supplies:
+            self.supply_UIs.append(current_supply_widget(ip,current))
 
     def set_layout(self):
-        layout = QVBoxLayout()
-
-        # title = QLabel("Keysight PSU Monitor")
-
-        layout_500 = QHBoxLayout()
-        layout_500.addWidget(self.current_500A_label)
-        layout_500.addWidget(self.current_500A)
-        layout_500.addWidget(self.unit_500)
-
-        layout_170 = QHBoxLayout()
-        layout_170.addWidget(self.current_170A_label)
-        layout_170.addWidget(self.current_170A)
-        layout_170.addWidget(self.unit_170)
-
-        # self.layout.addWidget(title)
-        layout.addLayout(layout_500)
-        layout.addLayout(layout_170)
-
-        self.layout = layout
+        self.layout = QVBoxLayout()
+        # stack the layout for each of the supply UIs into the main window layout
+        for supply_UI in self.supply_UIs:
+            self.layout.addLayout(supply_UI.layout)
 
     def update_current(self):
-        I_500A = self.read_current(self.supply_500A)
-        self.current_500A.setText(f"{I_500A:1.4f}")
-        I_170A = self.read_current(self.supply_170A)
-        self.current_170A.setText(f"{I_170A:1.4f}")
-
-    def read_current(self,supply):
-        supply.write(":MEASure:CURRent:DC?")
-        time.sleep(0.05)
-        return float(supply.read())
+        for supply_UI in self.supply_UIs:
+            supply_UI.update_current_UI()
     
 def main():
     app = QApplication(sys.argv)
-    window = QWidget()
 
-    app.setStyle("Windows")
+    app.setStyle("Windows") # fun formatting
 
-    grid = Window()
-    window.setLayout(grid.layout)
+    window = Window()
+    window.setLayout(window.layout)
     window.setWindowTitle("Keysight PSU Monitor")
     window.setWindowIcon(QIcon('banana-icon.png'))
-
     window.setFixedSize(400, 100)
 
     window.show()
