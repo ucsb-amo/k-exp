b0993061f5ba7b3d082bbdc00ee6621dc85ce8d3
jpagett <pagett.jared@gmail.com>
2024-02-26 09:32:00 -0800

    still broken

--- kexp/util/guis/ion_pump_controllers_SIP/SIP_ion_pump_controller_gui.py
+++ kexp/util/guis/ion_pump_controllers_SIP/SIP_ion_pump_controller_gui.py
@@ -1,16 +1,8 @@
 import socket
 import sys
-import os
-from PyQt6.QtWidgets import (
-    QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout,
-    QLineEdit, QPushButton, QMainWindow, QFileDialog, QFrame, QSpacerItem,
-    QSizePolicy, QMessageBox
-)
-import numpy as np
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit
 from PyQt6.QtCore import QTimer
-from PyQt6.QtGui import QIcon, QCloseEvent
-
-import asyncio
+from PyQt6.QtGui import QIcon
 
 DEFAULT_HOST_IP = '192.168.1.76'
 IP_SOURCE = '192.168.1.81'
@@ -39,19 +31,26 @@ class ion_pump_data_dealer():
         offset_bytes = initial_offset_bytes + start_bit // 8
         val = data_bytes[offset_bytes:(payload_length_bytes+offset_bytes)]
         return int.from_bytes(val,"big")
-    
-DEALER = ion_pump_data_dealer()
+
+class ion_pump_client(ion_pump_data_dealer):
+    def __init__(self,ip,socket:socket.socket):
+        super().__init__()
+        self.ip = ip
+        self.socket = socket
+        self.read_all_msg = bytes.fromhex("01 05")
+
+    def get_pressure(self):
+        self.socket.sendto(self.read_all_msg,(self.ip,UDP_PORT))
+        data = self.socket.recvfrom(302)
+        pressure_torr = self.decode_pressure(data)
+        return pressure_torr
 
 class ion_pump_panel(QWidget):
     def __init__(self,name,controller_ip,socket:socket.socket):
         super().__init__()
-        self.ip = controller_ip
-        self.sock = socket
         self.name = name
-
         self.pressure = 0.
-
-        self.read_all_msg = bytes.fromhex("01 05")
+        self.client = ion_pump_client(ip=controller_ip,socket=socket)
         
         self.setup_gui_elems()
         self.setup_layout()
@@ -68,31 +67,27 @@ class ion_pump_panel(QWidget):
         self.layout.addWidget(self.pressure_box_unit)
 
     def update_pressure(self):
-        self.pressure = self.get_pressure()
+        self.pressure = self.client.get_pressure()
         if self.pressure == 0.:
             self.pressure_box.setText("<1.5e-11")
         else:
             self.pressure_box.setText(f"{self.pressure:1.3g}")
-    
-    def get_pressure(self):
-        self.sock.sendto(self.read_all_msg,(self.ip,UDP_PORT))
-        data = self.sock.recvfrom(302)
-        pressure_torr = DEALER.decode_pressure(data)
-        return pressure_torr
 
-class app(QWidget):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setup_socket()
         self.setup_gui_elems()
         self.setup_layout()
+        # self.setup_timer()
 
     def setup_gui_elems(self):
         self.ip_box_label = QLabel("Host IP")
         self.ip_edit = QLineEdit(DEFAULT_HOST_IP)
 
-        self.ip_box_cell = ion_pump_panel(name="Cell",ip=IP_CELL)
-        self.ip_box_source = ion_pump_panel(name="Source",ip=IP_SOURCE)
+        self.ip_boxes = []
+        self.ip_boxes.append(ion_pump_panel(name="Cell",ip=IP_CELL,socket=self.sock))
+        self.ip_boxes.append(ion_pump_panel(name="Source",ip=IP_SOURCE,scoket=self.sock))
 
     def setup_layout(self):
 
@@ -102,8 +97,8 @@ class app(QWidget):
 
         self.layout = QVBoxLayout()
         self.layout.addLayout(self.ip_select_layout)
-        self.layout.addWidget(self.ip_box_cell)
-        self.layout.addWidget(self.ip_box_source)
+        for ip_box in self.ip_boxes:
+            self.layout.addWidget(ip_box)
 
     def setup_timer(self):
         self.timer = QTimer(self)
@@ -111,13 +106,27 @@ class app(QWidget):
         self.timer.start(T_UPDATE_MS)
 
     def update_gui(self):
-        self.ip_box_cell.update_pressure()
-        self.ip_box_source.update_pressure()
+        for ip_box in self.ip_boxes:
+            ip_box.update_pressure()
         
     def setup_socket(self):
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        print(self.ip_edit.text())
         self.sock.bind((self.ip_edit.text(),UDP_PORT))
 
     def closeEvent(self):
         self.sock.close()
 
+def main():
+    app = QApplication(sys.argv)
+    app.setStyle("Windows")
+    window = QWidget()
+    main_window = MainWindow()
+    window.setLayout(main_window.layout)
+    window.setWindowTitle("SIP Ion Pump Monitor")
+    window.setWindowIcon(QIcon('banana-icon.png'))
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "main":
+    main()
