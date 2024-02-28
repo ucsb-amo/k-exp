5da74ae597f7712efa0549c2a1a5d8543ebee050
jpagett <pagett.jared@gmail.com>
2024-02-28 09:07:31 -0800

    Update painted_lightsheet.py

--- kexp/control/misc/painted_lightsheet.py
+++ kexp/control/misc/painted_lightsheet.py
@@ -8,21 +8,23 @@ from artiq.experiment import kernel, delay
 
 import numpy as np
 
-dv = -1.
+dv = -100.
 dv_list = np.linspace(0.,1.,5)
+
+DAC_PAINT_FULLSCALE = 9.99
 
 class lightsheet():
     def __init__(self, vva_dac:DAC_CH, paint_amp_dac:DAC_CH, sw_ttl:TTL, expt_params:ExptParams):
         """Controls the light sheet beam.
 
         Args:
-            dac_ch (DAC_CH): A DAC channel that controls a VVA to attenuate the
+            vva_dac (DAC_CH): A DAC channel that controls a VVA to attenuate the
             overall RF that reaches the amp (and thus the AO.) 
-            paint_dds (DDS):
-            DDS channel, amplitude controls the painting amplitude, frequency
-            controls the modulation freuqency of the RF. 
-            sw_ttl (TTL): TTL
-            channel, controls an RF switch between AWG and amplifier.
+            paint_amp_dac (DAC_CH): DAC_CH, voltage controls the painting amplitude via
+            controlling the modulation depth. -9.99 V is minimal painting, 9.99
+            V is maximal painting. 
+            sw_ttl (TTL): TTL channel, controls an RF
+            switch between AWG and amplifier.
         """        
         self.vva_dac = vva_dac
         self.paint_amp_dac = paint_amp_dac
@@ -30,10 +32,11 @@ class lightsheet():
         self.params = expt_params
 
     @kernel
-    def set_paint_amp(self,v_paint_amp=dv,load_dac=True):
-        if v_paint_amp == dv:
-            v_paint_amp = 0.0
-        self.vva_dac.set(v=v_paint_amp,load_dac=load_dac)
+    def set_paint_amp(self,paint_fraction=dv,load_dac=True):
+        if paint_fraction == dv:
+            paint_fraction = 0.
+        v_dac = DAC_PAINT_FULLSCALE * (2 * paint_fraction - 1)
+        self.vva_dac.set(v=v_dac,load_dac=load_dac)
 
     @kernel
     def set_power(self,v_lightsheet_vva=dv,load_dac=True):
