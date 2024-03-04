12e84d72241ca41fa45517632b6de6f995653386
jpagett <pagett.jared@gmail.com>
2024-03-03 18:15:51 -0800

    Update test.py

--- kexp/experiments/_test/test.py
+++ kexp/experiments/_test/test.py
@@ -9,33 +9,28 @@ import numpy as np
 class tof(EnvExperiment, Base):
 
     def build(self):
-        Base.__init__(self,setup_camera=True,camera_select="xy_basler")
+        Base.__init__(self,setup_camera=False,camera_select="xy_basler")
 
-        self.xvar('t_tof',np.linspace(1.,3.,3)*1.e-3)
+        self.xvar('dummy',np.linspace(1.,1.,1)*1.e-3)
 
         self.finish_build()
 
     @kernel
     def scan_kernel(self):
-        print(self.params.t_tof)
+        self.ttl.xy_basler.pulse(self.camera_params.t_camera_trigger)
+        delay(30*ms)
+        self.ttl.xy_basler.pulse(self.camera_params.t_camera_trigger)
+        delay(30*ms)
+        self.ttl.xy_basler.pulse(self.camera_params.t_camera_trigger)
+
         delay(1*s)
-        self.abs_image()
 
     @kernel
     def run(self):
         
         self.init_kernel()
-        self.StartTriggeredGrab()
-        print(self.camera_params.connection_delay)
-        # delay(self.camera_params.connection_delay)
         self.scan()
 
     def analyze(self):
-
-        self.camera.Close()
-
-        import os
-        expt_filepath = os.path.abspath(__file__)
-        self.ds.save_data(self, expt_filepath)
 
         print("Done!")
