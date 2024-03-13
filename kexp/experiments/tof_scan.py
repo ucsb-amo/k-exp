7db0fb82c40b2b35ee80e70bc1cf8de33b3e1d9d
jpagett <pagett.jared@gmail.com>
2024-03-13 11:38:15 -0700

    Update tof_scan.py

--- kexp/experiments/tof_scan.py
+++ kexp/experiments/tof_scan.py
@@ -28,13 +28,22 @@ class tof(EnvExperiment, Base):
         # self.xvar('t_gm',np.linspace(1.,8.,5)*1.e-3)
         # self.xvar('t_gmramp',np.linspace(3.,9.,5)*1.e-3)
 
-        self.xvar('t_tof',np.linspace(10000,18000,6)*1.e-6)
+        # self.xvar('t_tweezer_1064_ramp',np.linspace(3.,7.,3)*1.e-3)
+        
+        # self.xvar('t_tof',np.linspace(3000,18000,8)*1.e-6)
+        self.xvar('t_tweezer_hold',np.linspace(.100,20.,2)*1.e-3)
+        # self.xvar('t_lightsheet_hold',np.linspace(5000,40000,5)*1.e-6)
 
         self.p.t_mot_load = 1.
-        self.p.t_tof = 14000.e-6
-        self.p.N_repeats = 3
-        # self.p.t_gm = 3.e-3
-        # self.p.t_gmramp = 4.e-3
+        self.p.t_tof = 20.e-6
+        # self.p.N_repeats = 3
+
+        # self.camera_params.em_gain = 100
+        # self.camera_params.exposure_time = 5.e-6
+        # self.p.t_imaging_pulse = 5.e-6
+        # self.p.t_dark_image_delay = 50.e-3
+
+        # self.p.amp_imaging_abs = 0.5
 
         self.finish_build()
 
@@ -47,19 +56,31 @@ class tof(EnvExperiment, Base):
         self.set_shims(v_zshim_current=self.p.v_zshim_current_gm, v_yshim_current=self.p.v_yshim_current_gm, v_xshim_current=self.p.v_xshim_current_gm)
         self.gm(self.p.t_gm * s)
         self.gm_ramp(self.p.t_gmramp)
-        self.release()
-        
-        # self.tweezer.ramp(t=self.p.t_tweezer_1064_ramp)
-        # delay(self.p.t_tweezer_hold)
-        # self.tweezer.off()
 
+        self.release()
+
+        # self.tweezer.on()
+        
         # self.lightsheet.ramp(t=self.p.t_lightsheet_rampup)
         # delay(self.p.t_lightsheet_hold)
         # self.lightsheet.off()
 
+        self.tweezer.ramp(t=self.p.t_tweezer_1064_ramp)
+        delay(self.p.t_tweezer_hold)
+        self.tweezer.off()
+
+        # self.lightsheet.ramp(t=self.p.t_lightsheet_rampup)
+        # delay(20.e-3)
+        # self.tweezer.ramp(t=self.p.t_tweezer_1064_ramp)
+        # self.lightsheet.off()
+        # delay(self.p.t_tweezer_hold)
+        # self.tweezer.off()
+
         delay(self.p.t_tof)
         self.flash_repump()
         self.abs_image()
+
+        # self.tweezer.off()
 
     @kernel
     def run(self):
