59255796b3a990ab0c87ad822e64b8876e96ad70
jpagett <pagett.jared@gmail.com>
2024-04-16 12:15:29 -0700

    Update expt_params.py

--- kexp/config/expt_params.py
+++ kexp/config/expt_params.py
@@ -24,9 +24,9 @@ class ExptParams():
 
         self.frequency_ao_imaging = 350.00e6
         self.frequency_detuned_imaging = 27.e6
-        self.frequency_detuned_imaging_F1 = 4.58e08 - 2.86e6
+        self.frequency_detuned_imaging_F1 = 431.54e6 + 17.e6
         
-        self.t_repump_flash_imaging = 2.e-6
+        self.t_repump_flash_imaging = 10.e-6
         self.detune_d2_r_imaging = 0.
         self.amp_d2_r_imaging = 0.065
 
@@ -44,7 +44,7 @@ class ExptParams():
         self.t_magnet_off_pretrigger = 0.e-3
         self.t_gm = 3.e-3
         self.t_gmramp = 5.e-3
-        self.t_optical_pumping = 100.e-6
+        self.t_optical_pumping = 200.e-6
         self.t_optical_pumping_bias_rampup = 2.e-3
         self.t_lightsheet_rampup = 10.e-3
         self.t_lightsheet_load = 10.e-3
@@ -55,6 +55,7 @@ class ExptParams():
         self.t_mot_reload = 2.
         self.t_bias_off_wait = 20.e-3
         self.t_recover = 40.e-3
+        self.t_magtrap_ramp = 10.e-3
 
         # DAC controlled AO amplitudes
         self.amp_d1_3d_c = 0.3
@@ -117,9 +118,9 @@ class ExptParams():
         self.detune_gm = 9.
         # self.amp_gm = 0.09
 
-        self.v_zshim_current_gm = .9
-        self.v_xshim_current_gm = 1.
-        self.v_yshim_current_gm = 1.
+        self.v_zshim_current_gm = 0.9
+        self.v_xshim_current_gm = 0.17
+        self.v_yshim_current_gm = 2.0
 
         self.detune_d1_c_gm = self.detune_gm
         self.pfrac_d1_c_gm = .78 # there is an ND on this photodiode -- much higher power/volt than the repump
@@ -136,13 +137,13 @@ class ExptParams():
 
         #Optical Pumping
         self.detune_optical_pumping_op = 0.0
-        self.amp_optical_pumping_op = 0.12
-        self.v_anti_zshim_current_op = 1.5
-        self.v_zshim_current_op = 3.0
+        self.amp_optical_pumping_op = 0.22
+        self.v_anti_zshim_current_op = 0.
+        self.v_zshim_current_op = 0.
         self.v_yshim_current_op = 2.0
         self.v_xshim_current_op = 0.17
         self.detune_optical_pumping_r_op = 0.0
-        self.amp_optical_pumping_r_op = 0.23
+        self.amp_optical_pumping_r_op = 0.25
 
         #ODT
         # self.amp_lightsheet = 0.6
@@ -168,35 +169,42 @@ class ExptParams():
         self.n_tweezer_1064_ramp_steps = 100
 
         # RF
+        self.t_rf_sweep_state_prep = 100.e-3
+        self.frequency_rf_sweep_state_prep_center = 459.3543e6
+        self.frequency_rf_sweep_state_prep_fullwidth = 30.e3
+        self.n_rf_sweep_state_prep_steps = 1000
+
+        # RF
         self.t_rf_state_xfer_sweep = 60.e-3
         self.amp_rf_source = 0.99
         self.frequency_rf_state_xfer_sweep_center = 461.7e6
         self.frequency_rf_state_xfer_sweep_fullwidth = 2.e6
-
         self.n_rf_state_xfer_sweep_steps = 1000
 
-        # self.v_vco_rf_state_xfer_sweep_center = 7.15
-        # self.v_vco_rf_state_xfer_sweep_fullwidth = 0.1
+        # mag trap
+        self.i_magtrap_ramp_start = 30.
+        self.i_magtrap_ramp_end = 0.0
+        self.n_magtrap_ramp_steps = 1000
 
         self.compute_derived()
 
     def compute_rf_sweep_params(self):
         self.dt_rf_state_xfer_sweep = self.t_rf_state_xfer_sweep / self.n_rf_state_xfer_sweep_steps
-
         self._frequency_rf_state_xfer_sweep_start = self.frequency_rf_state_xfer_sweep_center - self.frequency_rf_state_xfer_sweep_fullwidth
         self._frequency_rf_state_xfer_sweep_end = self.frequency_rf_state_xfer_sweep_center + self.frequency_rf_state_xfer_sweep_fullwidth
-        
         self.frequency_rf_state_xfer_sweep_list = np.linspace(
             self._frequency_rf_state_xfer_sweep_start,
             self._frequency_rf_state_xfer_sweep_end,
             self.n_rf_state_xfer_sweep_steps)
         
-        # self.__v_vco_rf_state_xfer_sweep_start = self.v_vco_rf_state_xfer_sweep_center - self.v_vco_rf_state_xfer_sweep_fullwidth/2
-        # self.__v_vco_rf_state_xfer_sweep_end = self.v_vco_rf_state_xfer_sweep_center + self.v_vco_rf_state_xfer_sweep_fullwidth/2
+        self.dt_rf_sweep_state_prep = self.t_rf_sweep_state_prep / self.n_rf_sweep_state_prep_steps
+        self._frequency_rf_sweep_state_prep_start = self.frequency_rf_sweep_state_prep_center - self.frequency_rf_sweep_state_prep_fullwidth
+        self._frequency_rf_sweep_state_prep_end = self.frequency_rf_sweep_state_prep_center + self.frequency_rf_sweep_state_prep_fullwidth
+        self.frequency_rf_sweep_state_prep_list = np.linspace(
+            self._frequency_rf_sweep_state_prep_start,
+            self._frequency_rf_sweep_state_prep_end,
+            self.n_rf_sweep_state_prep_steps)
         
-        # self.v_vco_rf_state_xfer_sweep_list = np.linspace(self.__v_vco_rf_state_xfer_sweep_start,
-        #                                                     self.__v_vco_rf_state_xfer_sweep_end,
-        #                                                     self.n_rf_state_xfer_sweep_steps)
         
     def compute_lightsheet_ramp_params(self):
         self.v_pd_lightsheet_ramp_list = np.linspace(
@@ -247,6 +255,10 @@ class ExptParams():
     def compute_tweezer_1064_ramp_params(self):
         self.v_pd_tweezer_1064_ramp_list = np.linspace(self.v_pd_tweezer_1064_ramp_start,self.v_pd_tweezer_1064_ramp_end, self.n_tweezer_1064_ramp_steps).transpose()
         self.dt_tweezer_1064_ramp = self.t_tweezer_1064_ramp / self.n_tweezer_1064_ramp_steps
+
+    def compute_magtrap_ramp_params(self):
+        self.magtrap_ramp_list = np.linspace(self.i_magtrap_ramp_start,self.i_magtrap_ramp_end, self.n_magtrap_ramp_steps).transpose()
+        self.dt_magtrap_ramp = self.t_magtrap_ramp / self.n_magtrap_ramp_steps
 
     def compute_derived(self):
         '''loop through methods (except built in ones) and compute all derived quantities'''
