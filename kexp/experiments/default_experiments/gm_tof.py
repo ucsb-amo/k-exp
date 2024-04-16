88f726f6ad68158b1254ff6f89409959bee81c91
jpagett <pagett.jared@gmail.com>
2024-04-16 12:15:11 -0700

    Create gm_tof.py

---
+++ kexp/experiments/default_experiments/gm_tof.py
@@ -0,0 +1,53 @@
+from artiq.experiment import *
+from artiq.experiment import delay
+from kexp import Base
+import numpy as np
+
+class tof(EnvExperiment, Base):
+
+    def build(self):
+        Base.__init__(self,setup_camera=True,camera_select='xy_basler',save_data=True)
+
+        self.p.imaging_state = 2.
+
+        self.xvar('t_tof',np.linspace(10.,17.,10)*1.e-3)
+
+        self.p.t_mot_load = .5
+
+        self.finish_build(shuffle=True)
+
+    @kernel
+    def scan_kernel(self):
+
+        self.dds.init_cooling()
+        
+        self.set_imaging_detuning(detuning=self.p.frequency_detuned_imaging)
+
+        self.switch_d2_2d(1)
+        self.mot(self.p.t_mot_load)
+        self.dds.push.off()
+        self.cmot_d1(self.p.t_d1cmot)
+        self.set_shims(v_zshim_current=self.p.v_zshim_current_gm,
+                        v_yshim_current=self.p.v_yshim_current_gm,
+                          v_xshim_current=self.p.v_xshim_current_gm)
+
+        self.gm(self.p.t_gm * s)
+        self.gm_ramp(self.p.t_gmramp)
+
+        self.release()
+
+        delay(self.p.t_tof)
+        self.flash_repump()
+        self.abs_image()
+       
+    @kernel
+    def run(self):
+        self.init_kernel()
+        self.load_2D_mot(self.p.t_2D_mot_load_delay)
+        self.scan()
+        self.mot_observe()
+
+    def analyze(self):
+        import os
+        expt_filepath = os.path.abspath(__file__)
+        self.end(expt_filepath)
