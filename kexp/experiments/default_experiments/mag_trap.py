ef76961b0392827b9e385a7c39dec92ebb21a23c
jpagett <pagett.jared@gmail.com>
2024-07-02 12:47:23 -0700

    Update mag_trap.py

--- kexp/experiments/default_experiments/mag_trap.py
+++ kexp/experiments/default_experiments/mag_trap.py
@@ -10,12 +10,13 @@ class mag_trap(EnvExperiment, Base):
 
         self.p.imaging_state = 2.
         # self.xvar('imaging_state',[2,1])
+        self.xvar('frequency_detuned_imaging',np.linspace(440.,500.,40)*1.e6)
 
         self.p.t_magtrap_hold = 100.e-3
-        # self.xvar('t_tof',np.linspace(7.,12.,15)*1.e-3)
+        # self.xvar('t_tof',np.linspace(4.,12.,15)*1.e-3)
         # self.xvar('i_magtrap_init',np.linspace(20.,34.,8))
-        self.xvar('i_magtrap_ramp_start',np.linspace(28.,95.,8))
-        self.xvar('t_magtrap',np.linspace(0.,20.,8)*1.e-3)
+        # self.xvar('i_magtrap_ramp_start',np.linspace(28.,95.,8))
+        # self.xvar('t_magtrap',np.linspace(0.,20.,8)*1.e-3)
         # self.xvar('v_zshim_current_magtrap',np.linspace(0.,8.,20))
 
         self.p.t_tof = 5.e-3
@@ -24,14 +25,6 @@ class mag_trap(EnvExperiment, Base):
 
     @kernel
     def scan_kernel(self):
-        self.dds.init_cooling()
-
-        self.core.break_realtime()
-
-        if self.p.imaging_state == 1.:
-            self.set_imaging_detuning(detuning=self.p.frequency_detuned_imaging_F1)
-        else:
-            self.set_imaging_detuning()
 
         self.switch_d2_2d(1)
         self.mot(self.p.t_mot_load)
@@ -39,7 +32,6 @@ class mag_trap(EnvExperiment, Base):
         self.cmot_d1(self.p.t_d1cmot * s)
         
         self.inner_coil.set_current(i_supply=self.p.i_magtrap_init)
-        self.inner_coil.set_voltage(v_supply=9.)
 
         self.set_shims(v_zshim_current=self.p.v_zshim_current_gm,
                         v_yshim_current=self.p.v_yshim_current_gm,
@@ -57,24 +49,26 @@ class mag_trap(EnvExperiment, Base):
 
         self.dds.power_down_cooling()
 
-        self.set_shims(v_zshim_current=0.,
-                        v_yshim_current=self.p.v_yshim_current_gm,
-                          v_xshim_current=self.p.v_xshim_current_gm)
+        self.set_shims(v_zshim_current=self.p.v_zshim_current_magtrap,
+                        v_yshim_current=self.p.v_yshim_current_magtrap,
+                          v_xshim_current=self.p.v_xshim_current_magtrap)
         
-        self.inner_coil.igbt_ttl.on()
+        self.inner_coil.on()
 
-        delay(self.p.t_magtrap)
+        delay(self.p.t_lightsheet_rampup)
 
-        self.inner_coil.set_current(i_supply=self.p.i_magtrap_ramp_start)
-        delay(20.e-3)
-        delay(self.p.t_magtrap_hold)        
+        for i in self.p.magtrap_ramp_list:
+            self.inner_coil.set_current(i_supply=i)
+            delay(self.p.dt_magtrap_ramp)
+
+        delay(20.e-3)        
 
         self.inner_coil.off()
 
         self.ttl.pd_scope_trig.off()
     
         delay(self.p.t_tof)
-        self.flash_repump()
+        # self.flash_repump()
         self.abs_image()
 
     @kernel
