f80d2e8044d95f30e9943b767c4f51a448c9cad3
jpagett <pagett.jared@gmail.com>
2024-08-07 13:19:03 -0700

    Update gm_tof.py

--- kexp/experiments/default_experiments/gm_tof.py
+++ kexp/experiments/default_experiments/gm_tof.py
@@ -8,18 +8,11 @@ class tof(EnvExperiment, Base):
     def build(self):
         Base.__init__(self,setup_camera=True,camera_select='xy_basler',save_data=True)
 
-        self.p.imaging_state = 1.
+        self.xvar('t_tof',np.linspace(2.,10.,10)*1.e-3)
 
-        # self.xvar('t_tof',np.linspace(13.,19.,10)*1.e-3)
-
-        self.p.t_tof = 13.e-3
-
-        self.xvar('pfrac_c_gmramp_end',np.linspace(.05,.4,5))
-        self.xvar('pfrac_r_gmramp_end',np.linspace(.03,.4,4))
-
-        self.p.N_repeats = [2,3]
-
-        self.p.t_mot_load = .4
+        self.p.t_tof = 10.e-3
+        self.p.t_mot_load = .2
+        self.camera_params.amp_imaging = .5
 
         self.finish_build(shuffle=True)
 
@@ -35,14 +28,13 @@ class tof(EnvExperiment, Base):
                           v_xshim_current=self.p.v_xshim_current_gm)
 
         self.gm(self.p.t_gm * s)
-        self.ttl.pd_scope_trig.on()
+        self.ttl.pd_scope_trig.pulse(1.e-6)
         self.gm_ramp(self.p.t_gmramp)
-        self.ttl.pd_scope_trig.off()
 
         self.release()
 
         delay(self.p.t_tof)
-        # self.flash_repump()
+        self.flash_repump()
         self.abs_image()
        
     @kernel
