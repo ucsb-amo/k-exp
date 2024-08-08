6c0311977895341e28e9b1490bc9cb968d16f4de
jpagett <pagett.jared@gmail.com>
2024-08-07 17:13:33 -0700

    Update cooling.py

--- kexp/base/sub/cooling.py
+++ kexp/base/sub/cooling.py
@@ -598,6 +598,33 @@ class Cooling():
             self.dds.op_r.off()
 
     @kernel
+    def start_magtrap(self,
+                        v_zshim_current=dv,
+                        v_yshim_current=dv,
+                        v_xshim_current=dv):
+        if v_zshim_current == dv:
+            v_zshim_current = self.params.v_zshim_current_magtrap
+        if v_yshim_current == dv:
+            v_yshim_current = self.params.v_yshim_current_magtrap
+        if v_xshim_current == dv:
+            v_xshim_current = self.params.v_xshim_current_magtrap
+        
+        self.switch_d2_3d(0)
+        self.switch_d1_3d(0)
+
+        self.flash_cooler()
+
+        self.dds.power_down_cooling()
+        
+
+        self.set_shims(v_zshim_current=v_zshim_current,
+                        v_yshim_current=v_yshim_current,
+                        v_xshim_current=v_xshim_current)
+
+        # magtrap start
+        self.inner_coil.on()
+
+    @kernel
     def magtrap_and_load_lightsheet(self,
                                 t_lightsheet_ramp=dv,
                                 t_magtrap_ramp=dv,
@@ -630,36 +657,55 @@ class Cooling():
         if v_xshim_current == dv:
             v_xshim_current = self.params.v_xshim_current_magtrap
 
-        self.switch_d2_3d(0)
-        self.switch_d1_3d(0)
-
-        self.flash_cooler()
-
-        self.dds.power_down_cooling()
-
-        self.set_shims(v_zshim_current=v_zshim_current,
-                        v_yshim_current=v_yshim_current,
-                        v_xshim_current=v_xshim_current)
-
-        # magtrap start
-        self.ttl.pd_scope_trig.pulse(t=1.e-6)
-        self.inner_coil.on()
+        self.start_magtrap(v_zshim_current=v_zshim_current,
+                           v_yshim_current=v_yshim_current,
+                           v_xshim_current=v_xshim_current)
 
         # ramp up lightsheet over magtrap
         
+
         self.lightsheet.ramp(t_lightsheet_ramp,
-                             v_pd_lightsheet_ramp_start,
-                             v_pd_lightsheet_ramp_end)
+                            v_pd_lightsheet_ramp_start,
+                            v_pd_lightsheet_ramp_end)
 
         self.inner_coil.ramp(t=t_magtrap_ramp,
-                             i_start=i_magtrap_init,
-                             i_end=i_magtrap_ramp_end)
+                            i_start=i_magtrap_init,
+                            i_end=i_magtrap_ramp_end)
 
         self.inner_coil.ramp(t=t_magtrap_rampdown,
-                             i_start=i_magtrap_ramp_end,
-                             i_end=0.)
-
+                            i_start=i_magtrap_ramp_end,
+                            i_end=0.)
         self.inner_coil.off()
+
+    @kernel
+    def magtrap(self,
+                t_magtrap_ramp=dv,
+                i_magtrap_init=dv,
+                i_magtrap_ramp_end=dv,
+                v_zshim_current=dv,
+                v_yshim_current=dv,
+                v_xshim_current=dv):
+        if t_magtrap_ramp == dv:
+            t_magtrap_ramp = self.params.t_magtrap_ramp
+        if i_magtrap_init == dv:
+            i_magtrap_init = self.params.i_mot
+        if i_magtrap_ramp_end == dv:
+            i_magtrap_ramp_end = self.params.i_magtrap_ramp_end
+        if v_zshim_current == dv:
+            v_zshim_current = self.params.v_zshim_current_magtrap
+        if v_yshim_current == dv:
+            v_yshim_current = self.params.v_yshim_current_magtrap
+        if v_xshim_current == dv:
+            v_xshim_current = self.params.v_xshim_current_magtrap
+
+        self.start_magtrap(v_zshim_current=v_zshim_current,
+                           v_yshim_current=v_yshim_current,
+                           v_xshim_current=v_xshim_current)
+
+        self.inner_coil.ramp(t=t_magtrap_ramp,
+                            i_start=i_magtrap_init,
+                            i_end=i_magtrap_ramp_end)
+
 
     @kernel
     def release(self):
