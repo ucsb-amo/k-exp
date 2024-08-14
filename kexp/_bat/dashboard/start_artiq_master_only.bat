c4d798cb428895eb32c7aec0d170de352a804b26
jpagett <pagett.jared@gmail.com>
2024-08-14 07:46:50 -0700

    repo to all expts

--- kexp/_bat/dashboard/start_artiq_master_only.bat
+++ kexp/_bat/dashboard/start_artiq_master_only.bat
@@ -1,4 +1,4 @@
 call %kpy%
 B:
 cd %data%
-artiq_master --device-db %db%  --repository %code%\\k-exp\\kexp --experiment-subdir experiments\test\\test_expt_repo
+artiq_master --device-db %db%  --repository %code%\\k-exp\\kexp --experiment-subdir experiments
