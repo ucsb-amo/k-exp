cad54d40a86d9705f0e533de4942e57d675d5e89
jpagett <pagett.jared@gmail.com>
2024-08-21 08:22:04 -0700

    updates

--- kexp/experiments/test/cmot_mloop_results_test.py
+++ kexp/experiments/test/cmot_mloop_results_test.py
@@ -3,25 +3,38 @@ from artiq.experiment import delay
 from kexp import Base
 import numpy as np
 
-class tof(EnvExperiment, Base):
+class cmot_mloop_results_test(EnvExperiment, Base):
 
     def build(self):
         Base.__init__(self,setup_camera=True,camera_select='xy_basler',save_data=True)
 
         self.p.imaging_state = 2.
-        self.p.t_tof = 800.e-6
+        self.p.t_tof = 1.e-3
         #self.xvar('t_tof',np.linspace(230.,700.,10)*1.e-6)
-        self.xvar('dummy',[0]*3)
+        self.xvar('use_mloop_params',[0,1])
+        
+        self.p.N_repeats = 50
 
-        self.p.N_repeats = 10
-
-
-        self.p.t_mot_load = .05
+        self.p.t_mot_load = .1
 
         self.finish_build(shuffle=True)
 
     @kernel
     def scan_kernel(self):
+
+        if self.p.use_mloop_params:
+            self.p.detune_push = -3.152
+            self.p.amp_push = 0.108
+            self.p.detune_d2_c_2dmot = -1.202
+            self.p.detune_d2_r_2dmot = -2.933
+            self.p.amp_d2_c_2dmot = 0.179
+            self.p.amp_d2_r_2dmot = 0.172
+            self.p.v_2d_mot_current = 1.968
+            self.p.detune_d2_c_mot = -1.599
+            self.p.detune_d2_r_mot = -3.918
+            self.p.amp_d2_c_mot = 0.2
+            self.p.amp_d2_r_mot = 0.099
+            self.p.i_mot = 17.403
 
         self.dds.init_cooling()
 
@@ -35,7 +48,7 @@ class tof(EnvExperiment, Base):
         delay(self.p.t_tof)
         self.flash_repump()
         self.abs_image()
-       
+    
     @kernel
     def run(self):
         self.init_kernel()
