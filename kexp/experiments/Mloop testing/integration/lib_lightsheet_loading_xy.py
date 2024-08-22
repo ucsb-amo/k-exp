1850c4e45e1494d19dbdd5440e6b8e3cd1af7551
hpbroida <weldlabucsb@gmail.com>
2024-08-22 15:57:43 -0700

    preppred nighttime m,loop sutff MORE

--- kexp/experiments/Mloop testing/integration/lib_lightsheet_loading_xy.py
+++ kexp/experiments/Mloop testing/integration/lib_lightsheet_loading_xy.py
@@ -25,12 +25,7 @@ def getAtomNumber():
         ad = load_atomdata(0,crop_type='lightsheet',average_repeats = True)
         # peakDensity = findPeakOD(ad.od[0])
         # print(peakDensity)
-        return np.max(ad.atom_number_density)*sum(ad.atom_number)/len(ad.atom_number) 
-
-#Cost function is just the negative of the atom number
-def getCost():
-    atomnumber = getAtomNumber()
-    return -1*atomnumber
+        return -np.max(ad.atom_number_density)*sum(ad.atom_number)/len(ad.atom_number) 
 
 
 class ExptBuilder():
@@ -91,22 +86,32 @@ class ExptBuilder():
                 def prepare(self):
                     Base.__init__(self,setup_camera=True,camera_select='xy_basler',save_data=True)
 
-                    self.p.imaging_state = 2.
-                    self.p.t_tof = 800.e-6
-                    self.xvar('dummy',[0]*1)
+                    self.camera_params.amp_imaging = 0.5
+                    
+                    self.p.t_mot_load = .5
+
+                    self.xvar("beans", [0,1]*20)
                                  
                     {assignment_lines} 
 
-                    self.p.N_repeats = 10
 
+                    #self.p.t_tof = 10.e-6
+                    self.p.t_lightsheet_hold = .1
+                    self.p.N_repeats = [1]
 
-                    self.p.t_mot_load = .05
+                    self.sh_dds = self.get_device("shuttler0_dds0")
+                    self.sh_dds: DDS
+                    self.sh_trigger = self.get_device("shuttler0_trigger")
+                    self.sh_trigger: Trigger
+                    self.sh_relay = self.get_device("shuttler0_relay")
+                    self.sh_relay: Relay
 
                     self.finish_prepare(shuffle=True)
 
                 @kernel
                 def scan_kernel(self):
                     self.set_high_field_imaging(i_outer = self.p.i_evap2_current)
+                    self.outer_coil.discharge()
                     self.switch_d2_2d(1)
                     self.mot(self.p.t_mot_load)
                     self.dds.push.off()
@@ -119,7 +124,7 @@ class ExptBuilder():
                     self.magtrap_and_load_lightsheet()
                     
                     delay(self.p.t_lightsheet_hold)
-                    # feshbach field on, ramp up to field 1  
+                    # # feshbach field on, ramp up to field 1  
                     self.outer_coil.on()
                     delay(1.e-3)
                     self.outer_coil.set_voltage()
@@ -127,24 +132,47 @@ class ExptBuilder():
                                         i_start=0.,
                                         i_end=self.p.i_evap1_current)
                     
-                    # lightsheet evap 1
+                    #lightsheet evap 1
                     self.lightsheet.ramp(t=self.p.t_lightsheet_rampdown,
                                         v_start=self.p.v_pd_lightsheet_rampup_end,
                                         v_end=self.p.v_pd_lightsheet_rampdown_end)
                     
-                    # feshbach field ramp to field 2
+                    # # feshbach field ramp to field 2
                     self.outer_coil.ramp(t=self.p.t_feshbach_field_ramp,
                                         i_start=self.p.i_evap1_current,
                                         i_end=self.p.i_evap2_current)
+
 
                     self.lightsheet.off()
                 
                     delay(self.p.t_tof)
                     self.flash_repump()
                     self.abs_image()
-                    self.outer_coil.off()
-
+                    self.outer_coil.off()   
                     self.outer_coil.discharge()
+
+                    # self.set_high_field_imaging(i_outer = self.p.i_evap2_current)
+                    # self.outer_coil.discharge()
+                    
+                    # self.switch_d2_2d(1)
+                    # self.mot(self.p.t_mot_load)
+                    # self.dds.push.off()
+                    # self.cmot_d1(self.p.t_d1cmot * s)
+                    
+                    # self.gm(self.p.t_gm * s)
+                    # self.gm_ramp(self.p.t_gmramp)
+
+
+                    # self.magtrap_and_load_lightsheet()
+                    
+                    
+                    # delay(self.p.t_lightsheet_hold)
+
+                    # self.lightsheet.off()
+                
+                    # delay(self.p.t_tof)
+                    # self.flash_repump()
+                    # self.abs_image()
 
 
                 @kernel
