fdbdb5a3e63bddc1fd25f274d90992d90a428bb2
hpbroida <weldlabucsb@gmail.com>
2024-09-03 11:40:06 -0700

    some small edits

--- kexp/experiments/_tools/tweezer balance/balance_tweezer_functions_atoms.py
+++ kexp/experiments/_tools/tweezer balance/balance_tweezer_functions_atoms.py
@@ -7,6 +7,13 @@ import matplotlib.pyplot as plt
 import spcm
 from spcm import units
 from Lightshift_Scan_Expt_Builder import ExptBuilder
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from kexp.analysis.fitting import GaussianFit
+
+from kexp.util.data.load_atomdata import load_atomdata
+from kexp.analysis.plotting_1d import *
 
 
 
@@ -28,3 +35,16 @@ class TweezerBalancer():
         eBuilder = ExptBuilder()
         eBuilder.write_experiment_to_file(eBuilder.lightshift_expt(f_list = self.F_LIST, a_list = self.A_LIST, f_img_detuning_list = self.IMG_DETUNING_LIST))
         eBuilder.run_expt()
+
+    def set_a_list(self, a_list):
+        self.A_LIST = a_list
+
+    def analyze(self):
+        ad = load_atomdata(0,crop_type='andor_tweezer_smol')
+        plt.set_cmap("viridis")
+        fit = GaussianFit(ad.xvars[0],ad.fit_amp_x)
+        fit.plot_fit()
+        plt.vlines(fit.x_center,ymin=plt.ylim()[0],ymax=plt.ylim()[1],colors='k',linestyles='--')
+        print(fit.x_center)
+        plt.plot(fit.xdata,fit.y_fitdata)
+        plt.show()
