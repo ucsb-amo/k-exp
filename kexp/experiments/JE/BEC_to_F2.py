6f1a7c3c90ff4dc7abdab5b74269e78522201f15
jpagett <pagett.jared@gmail.com>
2024-11-11 18:30:25 -0800

    set up expt to test PID

--- kexp/experiments/JE/BEC_to_F2.py
+++ kexp/experiments/JE/BEC_to_F2.py
@@ -19,13 +19,13 @@ class tweezer_load(EnvExperiment, Base):
         
         self.p.t_rf_state_xfer_sweep = 40.e-3
         self.p.n_rf_sweep_steps = 1000
-        self.p.frequency_rf_sweep_state_prep_fullwidth = 200.e3
+        self.p.frequency_rf_sweep_state_prep_fullwidth = 30.e3
 
         # self.xvar('frequency_rf_sweep_state_prep_fullwidth',np.linspace(10.,200.,20)*1.e3)
         # self.xvar('frequency_rf_sweep_state_prep_center', 145.5e6 + np.linspace(-5.,5.,60)*1.e6)
-        # self.xvar('frequency_rf_sweep_state_prep_center', np.linspace(143.,146.,10)*1.e6)
+        # self.xvar('frequency_rf_sweep_state_prep_center', np.linspace(146.,146.3,40)*1.e6)
         # self.p.frequency_rf_sweep_state_prep_center = 145.336e6
-        self.p.frequency_rf_sweep_state_prep_center = 145.336e6
+        self.p.frequency_rf_sweep_state_prep_center = 146.177e6
 
         # self.xvar('t_rf_state_xfer_sweep',np.linspace(15.,50.,20)*1.e-3)
         # self.xvar('t_fake_ramsey_delay',np.linspace(0.,100.,20)*1.e-3)
@@ -40,8 +40,8 @@ class tweezer_load(EnvExperiment, Base):
 
         # self.xvar('rf_drive_frequency', np.linspace(145.3,145.4,15)*1.e6)
         # self.p.rf_drive_frequency = 145.336e6
-        # self.xvar('t_rabi_drive', np.linspace(200.,1500.,40)*1.e-6)
-        # self.p.t_rabi_drive = 10.e-3
+        self.xvar('t_rabi_drive', np.linspace(20.,750.,40)*1.e-6)
+        self.p.t_rabi_drive = .5e-3
 
         # self.xvar('t_rabi_drive', np.linspace(200.,1500.,40)*1.e-6)
 
@@ -106,12 +106,12 @@ class tweezer_load(EnvExperiment, Base):
                              i_start=self.p.i_evap1_current,
                              i_end=self.p.i_evap2_current)
         
-        # self.ttl.pd_scope_trig.pulse(1.e-6)
-        # self.tweezer.on(paint=False)
-        # self.tweezer.ramp(t=self.p.t_tweezer_1064_ramp,
-        #                   v_start=0.,
-        #                   v_end=self.p.v_pd_tweezer_1064_ramp_end,
-        #                   paint=True,keep_trap_frequency_constant=False)
+        self.ttl.pd_scope_trig.pulse(1.e-6)
+        self.tweezer.on(paint=False)
+        self.tweezer.ramp(t=self.p.t_tweezer_1064_ramp,
+                          v_start=0.,
+                          v_end=self.p.v_pd_tweezer_1064_ramp_end,
+                          paint=True,keep_trap_frequency_constant=False)
         
         # # # lightsheet ramp down (to off)
         # self.lightsheet.ramp(t=self.p.t_lightsheet_rampdown2,
@@ -146,18 +146,23 @@ class tweezer_load(EnvExperiment, Base):
         #                      i_start=self.p.i_evap3_current,
         #                      i_end=self.p.i_end)
         delay(30.e-3)
+        self.ttl.pd_scope_trig.pulse(1.e-6)
         self.dac.outer_coil_pid.set(v=4.8075)
         self.ttl.outer_coil_pid_enable.on()
-        delay(1.)
+        delay(.5)
 
-        # if self.p.ifdosweep:
-        #     self.rf.sweep(t=self.p.t_rf_state_xfer_sweep, frequency_center=self.p.frequency_rf_sweep_state_prep_center)
-        #     # delay(self.p.t_fake_ramsey_delay)
-        #     # self.rf.sweep(t=self.p.t_rf_state_xfer_sweep, frequency_center=self.p.frequency_rf_sweep_state_prep_center)
-        # else:
-        #     delay(self.p.t_rf_state_xfer_sweep)
-        #     # delay(self.p.t_fake_ramsey_delay)
-        #     # delay(self.p.t_rf_state_xfer_sweep)
+        if self.p.ifdosweep:
+            self.rf.set_rf(frequency=self.p.frequency_rf_sweep_state_prep_center)
+            self.rf.on()
+            delay(self.p.t_rabi_drive)
+            # self.rf.sweep(t=self.p.t_rf_state_xfer_sweep, frequency_center=self.p.frequency_rf_sweep_state_prep_center)
+            # delay(self.p.t_fake_ramsey_delay)
+            # self.rf.sweep(t=self.p.t_rf_state_xfer_sweep, frequency_center=self.p.frequency_rf_sweep_state_prep_center)
+            self.rf.off()
+        else:
+            delay(self.p.t_rf_state_xfer_sweep)
+            # delay(self.p.t_fake_ramsey_delay)
+            # delay(self.p.t_rf_state_xfer_sweep)
         
         self.lightsheet.off()
         self.tweezer.off()
