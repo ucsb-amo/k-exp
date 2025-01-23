b6c05a965bbd284b3c3ef3cab8288ef51e482f77
jpagett <pagett.jared@gmail.com>
2025-01-23 08:07:17 -0800

    Create fluorescence_image_test.py

---
+++ kexp/experiments/_test/fluorescence_image_test.py
@@ -0,0 +1,45 @@
+from artiq.experiment import *
+from artiq.experiment import delay
+from kexp import Base, img_types
+import numpy as np
+
+class flimage_test(EnvExperiment, Base):
+
+    def prepare(self):
+        Base.__init__(self,
+                      imaging_type=img_types.FLUORESCENCE,
+                      setup_camera=True,
+                      camera_select='xy_basler',
+                      save_data=False)
+        
+        self.xvar('dummy',[1])
+
+        self.finish_prepare(shuffle=False)
+
+    @kernel
+    def scan_kernel(self):
+
+        self.mot(self.p.t_mot_load)
+        self.dds.push.off()
+        self.release()
+
+        delay(self.p.t_tof)
+        self.flimage_single()
+
+        delay(20.e-3)
+        self.flimage_single()
+
+        delay(20.e-3)
+        self.flimage_single(with_light=False)
+       
+    @kernel
+    def run(self):
+        self.init_kernel()
+        self.load_2D_mot(self.p.t_2D_mot_load_delay)
+        self.scan()
+        self.mot_observe()
+
+    def analyze(self):
+        import os
+        expt_filepath = os.path.abspath(__file__)
+        self.end(expt_filepath)
