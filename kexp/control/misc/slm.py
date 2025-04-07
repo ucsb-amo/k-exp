2c8b26de6cda930cb82a059038e3a6cedf8a49af
hpbroida <weldlabucsb@gmail.com>
2025-04-07 15:17:41 -0700

    Update slm.py

--- kexp/control/misc/slm.py
+++ kexp/control/misc/slm.py
@@ -8,10 +8,22 @@ class SLM():
     self.slm.write_phase_spot(diameter = 200, phase = np.pi / 2, x_center
         = 100, y_center = 300)
 
-    Break different elements into separate functions -- one to format and send a
-    message to the SLM server, one that gets called by the command-line
+    Break different elements into separate functions -- one to format a command,
+    one to send a message to the SLM server, one that gets called by the command-line
     interface ("input" style), one that the user will call in experiments (like
     the "slm.write_phase_spot" example above).
     """    
     def __init__(self):
         self.ip = "192.168.1.102"
+
+    def write_phase_spot(self,diameter,phase,x_center,y_center):
+        pass
+    
+    def send_command_to_slm(self,diameter,phase,x_center,y_center):
+        pass
+
+    def format_phase_spot_slm_commmand(self,diameter,phase,x_center,y_center):
+        pass
+
+    def send_command(self, command):
+        pass
