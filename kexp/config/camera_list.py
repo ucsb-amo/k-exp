c46ea679b0d7c996475edb85fa6596aeda700b42
jpagett <pagett.jared@gmail.com>
2025-04-14 12:04:11 -0700

    Create camera_list.py

---
+++ kexp/config/camera_list.py
@@ -0,0 +1,8 @@
+class CameraList():
+    def __init__(self):
+        self.xy_basler = "xy_basler"
+        self.x_basler = "x_basler"
+        self.z_basler = "z_basler"
+        self.andor = "andor"
+        self.basler_2dmot = "basler_2dmot"
+camera_list = CameraList()
