c0c5eeb55f9d2ac8dd18ca8a11f22fc1a502a101
jpagett <pagett.jared@gmail.com>
2025-04-28 15:56:20 -0700

    labeling update

--- kexp/base/sub/cooling.py
+++ kexp/base/sub/cooling.py
@@ -800,9 +800,9 @@ class Cooling():
         if amp_push == dv:
             amp_push = self.params.amp_push
         if frequency_ry_405 == dv:
-            frequency_ry_405 = self.params.frequency_ao_ry_405
+            frequency_ry_405 = self.params.frequency_ao_ry_405_switch
         if amp_ry_405 == dv:
-            amp_ry_405 = self.params.amp_ao_ry_405
+            amp_ry_405 = self.params.amp_ao_ry_405_switch
         if frequency_ry_980 == dv:
             frequency_ry_980 = self.params.frequency_ao_ry_980_switch
         if amp_ry_980 == dv:
@@ -825,7 +825,7 @@ class Cooling():
         self.dds.ry_980_switch.set_dds(frequency=frequency_ry_405,
                                 amplitude=amp_ry_405)
         delay(self.params.t_rtio)
-        self.dds.ry_405.set_dds(frequency=frequency_ry_980,
+        self.dds.ry_405_switch.set_dds(frequency=frequency_ry_980,
                                 amplitude=amp_ry_980)
         
         
@@ -844,7 +844,7 @@ class Cooling():
 
         self.dac.supply_current_2dmot.set(v=v_2d_mot_supply)
 
-        self.dds.ry_405.on()
+        self.dds.ry_405_switch.on()
         self.dds.ry_980_switch.on()
 
         self.dds.beatlock_ref.on()
