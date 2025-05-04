1a6fec6d7aa1e555aab318394b2e0a7cb68daff7
jpagett <pagett.jared@gmail.com>
2025-05-04 10:55:27 -0700

    expt moving

--- kexp/experiments/JE/trouble_shoot_cooling/lightsheet_evap.py
+++ kexp/experiments/JE/trouble_shoot_cooling/lightsheet_evap.py
@@ -12,14 +12,14 @@ T32 = 1<<32
 class mag_trap(EnvExperiment, Base):
 
     def prepare(self):
-        Base.__init__(self,setup_camera=True,save_data=True,
-                      camera_select=cameras.xy_basler,
+        Base.__init__(self,setup_camera=True,save_data=False,
+                      camera_select=cameras.andor,
                       imaging_type=img_types.ABSORPTION)
 
-        self.p.t_tof = 1000.e-6
-        # self.xvar('t_tof',np.linspace(20,1500.,10)*1.e-6)
+        self.p.t_tof = 20.e-6
+        # self.xvar('t_tof',np.linspace(400,1500.,10)*1.e-6)
         # self.xvar('t_tof',np.linspace(5.,20.,10)*1.e-3)
-        # self.xvar('dumy',[0]*5)
+        self.xvar('dumy',[0]*500)
 
         # self.xvar('t_pump_to_F1',np.linspace(0.05,10.,10)*1.e-6)
 
@@ -51,23 +51,23 @@ class mag_trap(EnvExperiment, Base):
         # self.p.t_lightsheet_rampup = 1.
         # self.p.v_pd_lightsheet_rampup_end = 9.
 
-        self.xvar('i_evap1_current',np.linspace(180.,200.,10))
-        self.p.i_evap1_current = 193.
+        # self.xvar('i_evap1_current',np.linspace(180.,200.,10))
+        self.p.i_evap1_current = 184.
 
-        # self.xvar('v_pd_lightsheet_rampdown_end',np.linspace(1.,7.,8))
-        self.p.v_pd_lightsheet_rampdown_end = 4.
+        # self.xvar('v_pd_lightsheet_rampdown_end',np.linspace(2.,7.,8))
+        self.p.v_pd_lightsheet_rampdown_end = 2.7
 
         # self.xvar('t_lightsheet_rampdown',np.linspace(100.,2000.,8)*1.e-3)
-        self.p.t_lightsheet_rampdown = .9
+        self.p.t_lightsheet_rampdown = 1.3
 
-        # self.xvar('v_pd_lightsheet_rampdown2_end',np.linspace(.5,3.,8))
-        self.p.v_pd_lightsheet_rampdown2_end = 1.1
+        # self.xvar('v_pd_lightsheet_rampdown2_end',np.linspace(.5,2.,15))
+        self.p.v_pd_lightsheet_rampdown2_end = 1.3
 
         # self.xvar('t_lightsheet_rampdown2',np.linspace(100.,2000.,8)*1.e-3)
         self.p.t_lightsheet_rampdown2 = 1.7
 
-        self.xvar('i_evap2_current',np.linspace(180.,205.,10))
-        self.p.i_evap2_current = 196.5
+        # self.xvar('i_evap2_current',np.linspace(180.,205.,10))
+        self.p.i_evap2_current = 194.
         
         self.p.t_lightsheet_hold = .2
 
@@ -75,13 +75,13 @@ class mag_trap(EnvExperiment, Base):
         # self.xvar('t_imaging_pulse',np.linspace(1.,20.,20)*1.e-6)
         # self.p.t_imaging_pulse = 2.e-5    
        
-        # self.camera_params.exposure_time = 50.e-6
+        # self.camera_params.exposure_time = 25.e-6
         # self.params.t_imaging_pulse = self.camera_params.exposure_time
         # self.camera_params.em_gain = 1.
 
         self.p.N_repeats = 1
-        self.p.t_mot_load = .5
-        # self.p.amp_imaging = .1
+        self.p.t_mot_load = 1.
+        self.p.amp_imaging = .54
         self.p.imaging_state = 2.
 
         self.finish_prepare(shuffle=True)
