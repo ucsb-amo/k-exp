6aff01149d781fdd2d03c9ab4afc8f5c567754c1
hpbroida <weldlabucsb@gmail.com>
2025-05-26 08:44:50 -0700

    add quadratic imaging detuning function

--- kexp/calibrations/imaging.py
+++ kexp/calibrations/imaging.py
@@ -25,14 +25,29 @@ def high_field_imaging_detuning(i_transducer) -> TFloat:
 
 # run 23512
 # all shims set to zero during feshbach field
-slope_imaging_frequency_per_i_transducer_lf = -8509090.922196094
-yintercept_imaging_frequency_per_i_transducer_lf = 449781818.3732016
+# slope_imaging_frequency_per_i_transducer_lf = -8509090.922196094
+# yintercept_imaging_frequency_per_i_transducer_lf = 449781818.3732016
+
+# @portable
+# def low_field_imaging_detuning(i_transducer) -> TFloat:
+#     detuning = slope_imaging_frequency_per_i_transducer_lf * i_transducer \
+#       + yintercept_imaging_frequency_per_i_transducer_lf
+#     return detuning
+
+# run 23527
+a2_imaging_frequency_per_i_transducer_lf = 53030.29454330852
+a1_imaging_frequency_per_i_transducer_lf = -10615151.158501664
+a0_imaging_frequency_per_i_transducer_lf = 468981814.71528655
 
 @portable
 def low_field_imaging_detuning(i_transducer) -> TFloat:
-    detuning = slope_imaging_frequency_per_i_transducer_lf * i_transducer \
-      + yintercept_imaging_frequency_per_i_transducer_lf
-    return detuning
+   a0 = a0_imaging_frequency_per_i_transducer_lf
+   a1 = a1_imaging_frequency_per_i_transducer_lf
+   a2 = a2_imaging_frequency_per_i_transducer_lf
+   i = i_transducer
+   detuning = a2 * i**2 + a1 * i + a0
+   return detuning
+
 
 # run 23397
 # all shims set to zero during feshbach field
