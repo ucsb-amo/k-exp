be9a007710f7ca8a9768420f1b787fe3c792761c
jpagett <pagett.jared@gmail.com>
2025-05-28 17:32:43 -0700

    Create align_by_killing_tweezer.py

---
+++ kexp/experiments/JP/align_by_killing_tweezer.py
@@ -0,0 +1,111 @@
+from artiq.experiment import *
+from artiq.experiment import delay
+from kexp import Base
+import numpy as np
+from kexp.util.artiq.async_print import aprint
+
+from kexp.calibrations.tweezer import tweezer_vpd1_to_vpd2
+from kexp.calibrations.imaging import high_field_imaging_detuning
+
+class tweezer_load(EnvExperiment, Base):
+
+    def prepare(self):
+        Base.__init__(self,setup_camera=True,camera_select='andor',save_data=True)
+
+        self.p.t_tof = 100.e-6
+        self.p.t_lightsheet_rampdown = .65
+        self.p.t_tweezer_1064_rampdown = 100.e-3
+
+        self.p.frequency_tweezer_list = [74.e6]
+        self.p.amp_tweezer_list = [.5]
+
+        self.xvar('amp_mot_killer',np.linspace(0.,0.188,5))
+        self.p.amp_mot_killer = 0.188
+
+        self.p.t_mot_load = 1.
+        self.p.N_repeats = 1
+
+        self.finish_prepare(shuffle=True)
+
+    @kernel
+    def scan_kernel(self):
+
+        self.switch_d2_2d(1)
+        self.mot(self.p.t_mot_load)
+        self.dds.push.off()
+        self.cmot_d1(self.p.t_d1cmot * s)
+        
+        self.gm(self.p.t_gm * s)
+        self.gm_ramp(self.p.t_gmramp)
+
+        self.magtrap_and_load_lightsheet()
+
+        # feshbach field on, ramp up to field 1  
+        self.outer_coil.on()
+        self.outer_coil.set_voltage()
+        self.outer_coil.ramp_supply(t=self.p.t_feshbach_field_rampup,
+                             i_start=0.,
+                             i_end=self.p.i_lf_lightsheet_evap1_current)
+        
+        self.set_shims(v_zshim_current=0.,
+                        v_yshim_current=0.,
+                        v_xshim_current=0.)
+
+        # lightsheet evap 1
+        self.lightsheet.ramp(t=self.p.t_lightsheet_rampdown,
+                             v_start=self.p.v_pd_lightsheet_rampup_end,
+                             v_end=self.p.v_pd_lightsheet_rampdown_end)
+        
+        # feshbach field ramp to field 2
+        self.outer_coil.ramp_supply(t=self.p.t_feshbach_field_ramp,
+                             i_start=self.p.i_lf_lightsheet_evap1_current,
+                             i_end=self.p.i_lf_tweezer_load_current)
+        
+        #
+        self.tweezer.on(paint=False)
+        self.tweezer.ramp(t=self.p.t_tweezer_1064_ramp,
+                          v_start=0.,
+                          v_end=self.p.v_pd_tweezer_1064_ramp_end,
+                          paint=True,keep_trap_frequency_constant=False)
+        
+        # lightsheet ramp down (to off)
+        self.lightsheet.ramp(t=self.p.t_lightsheet_rampdown2,
+                             v_start=self.p.v_pd_lightsheet_rampdown_end,
+                             v_end=self.p.v_pd_lightsheet_rampdown2_end)
+        
+        # delay(self.p.t_lightsheet_hold)
+        self.lightsheet.off()
+
+        delay(self.p.t_tweezer_hold)
+
+        self.ttl.pd_scope_trig.pulse(1.e-6)
+        self.outer_coil.snap_off()
+
+        self.dds.mot_killer.set_dds_gamma(delta=0., amplitude=self.p.amp_mot_killer)
+        self.dds.mot_killer.on()
+        
+        delay(10.e-3)
+
+        self.tweezer.off()
+
+        
+        delay(self.p.t_tof)
+        self.dds.mot_killer.off()
+
+        self.flash_repump()
+        self.abs_image()
+
+        self.outer_coil.off()
+        self.outer_coil.discharge()
+
+    @kernel
+    def run(self):
+        self.init_kernel()
+        self.load_2D_mot(self.p.t_2D_mot_load_delay)
+        self.scan()
+        # self.mot_observe()
+
+    def analyze(self):
+        import os
+        expt_filepath = os.path.abspath(__file__)
+        self.end(expt_filepath)
