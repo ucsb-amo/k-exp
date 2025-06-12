c4b18371cc219d9581e5e8f84ff73b9f97d3306f
jpagett <pagett.jared@gmail.com>
2025-06-11 17:17:17 -0700

    Create ramsey.py

---
+++ kexp/experiments/ramsey.py
@@ -0,0 +1,182 @@
+from artiq.experiment import *
+from artiq.experiment import delay
+from kexp import Base, cameras, img_types
+import numpy as np
+from kexp.util.artiq.async_print import aprint
+
+from kexp.calibrations.tweezer import tweezer_vpd1_to_vpd2
+from kexp.calibrations.imaging import high_field_imaging_detuning
+
+class tweezer_load(EnvExperiment, Base):
+
+    def prepare(self):
+        Base.__init__(self,setup_camera=True,
+                      camera_select=cameras.andor,
+                      save_data=True,
+                      imaging_type=img_types.ABSORPTION)
+        
+        # IMAGING FREQUENCIES IN FREE SPACE
+
+        self.p.frequency_detuned_imaging_m1 = 286.e6
+        self.p.frequency_detuned_imaging_0 = 318.e6
+        self.p.frequency_detuned_imaging_midpoint = 608.e6
+
+        self.p.i_spin_mixture = 19.48
+        self.p.frequency_raman_transition = 41.236e6
+        
+        ### 
+
+        self.p.t_hadamard = 1.62e-6
+        # self.xvar('t_ramsey',np.linspace(0.,20.e-6,10))
+
+        ###
+
+        self.p.frequency_tweezer_list = [74.e6]
+        self.p.amp_tweezer_list = [.99]
+        self.p.t_mot_load = 1.
+        self.p.t_tof = 300.e-6
+        self.p.N_repeats = 1
+
+        self.finish_prepare(shuffle=True)
+
+    @kernel
+    def scan_kernel(self):
+        # self.set_imaging_detuning(self.p.frequency_detuned_imaging_midpoint)
+        self.set_imaging_detuning(self.p.frequency_detuned_imaging_m1)
+
+        self.prepare_atoms()
+
+        # self.hadamard()
+        # delay(self.p.t_ramsey)
+        # self.hadamard()
+
+        self.tweezer.off()
+
+        delay(self.p.t_tof)
+        self.abs_image()
+
+        self.finish_shot()
+
+    @kernel
+    def init_raman(self):
+        self.dds.raman_minus.set_dds(amplitude=self.p.amp_raman)
+        self.dds.raman_plus.set_dds(amplitude=self.p.amp_raman)
+        self.raman.set_transition_frequency(self.p.frequency_raman_transition)
+
+    @kernel
+    def hadamard(self):
+        with parallel:
+            self.dds.raman_minus.on()
+            self.dds.raman_plus.on()
+        delay(self.p.t_hadamard)
+        with parallel:
+            self.dds.raman_minus.off()
+            self.dds.raman_plus.off()
+
+    @kernel
+    def finish_shot(self):
+        self.outer_coil.stop_pid()
+
+        self.outer_coil.off()
+        self.outer_coil.discharge()
+
+        self.dac.supply_current_2dmot.set(v=self.p.v_2d_mot_current)
+
+    @kernel
+    def prepare_atoms(self):
+        self.switch_d2_2d(1)
+        self.mot(self.p.t_mot_load)
+        self.dds.push.off()
+        self.cmot_d1(self.p.t_d1cmot * s)
+        
+        self.gm(self.p.t_gm * s)
+        self.gm_ramp(self.p.t_gmramp)
+
+        self.magtrap_and_load_lightsheet()
+
+        self.set_shims(v_zshim_current=0.,
+                        v_yshim_current=0.,
+                        v_xshim_current=0.)
+
+        # feshbach field on, ramp up to field 1
+        self.outer_coil.on()
+        self.outer_coil.set_voltage()
+        self.outer_coil.ramp_supply(t=self.p.t_feshbach_field_rampup,
+                             i_start=0.,
+                             i_end=self.p.i_lf_lightsheet_evap1_current)
+
+        # lightsheet evap 1
+        self.lightsheet.ramp(t=self.p.t_lightsheet_rampdown,
+                             v_start=self.p.v_pd_lightsheet_rampup_end,
+                             v_end=self.p.v_pd_lightsheet_rampdown_end)
+        
+        # feshbach field ramp to field 2
+        self.outer_coil.ramp_supply(t=self.p.t_feshbach_field_ramp,
+                             i_start=self.p.i_lf_lightsheet_evap1_current,
+                             i_end=self.p.i_lf_tweezer_load_current)
+        
+        #
+        self.tweezer.on(paint=False)
+        self.tweezer.ramp(t=self.p.t_tweezer_1064_ramp,
+                          v_start=0.,
+                          v_end=self.p.v_pd_tweezer_1064_ramp_end,
+                          paint=True,keep_trap_frequency_constant=False)
+        
+        # # lightsheet ramp down (to off)
+        self.lightsheet.ramp(t=self.p.t_lightsheet_rampdown2,
+                             v_start=self.p.v_pd_lightsheet_rampdown_end,
+                             v_end=self.p.v_pd_lightsheet_rampdown2_end)
+        
+        # delay(self.p.t_lightsheet_hold)
+        self.lightsheet.off()
+
+        self.outer_coil.ramp_supply(t=self.p.t_feshbach_field_ramp,
+                             i_start=self.p.i_lf_tweezer_load_current,
+                             i_end=self.p.i_lf_tweezer_evap1_current)
+
+        
+        # # tweezer evap 1 with constant trap frequency
+        self.tweezer.ramp(t=self.p.t_tweezer_1064_rampdown,
+                          v_start=self.p.v_pd_tweezer_1064_ramp_end,
+                          v_end=self.p.v_pd_tweezer_1064_rampdown_end,
+                          paint=True,keep_trap_frequency_constant=True)
+        
+        self.outer_coil.ramp_supply(t=self.p.t_feshbach_field_ramp,
+                             i_start=self.p.i_lf_tweezer_evap1_current,
+                             i_end=self.p.i_lf_tweezer_evap2_current)
+        
+        # # tweezer evap 2 with constant trap frequency
+        self.tweezer.ramp(t=self.p.t_tweezer_1064_rampdown2,
+                          v_start=self.p.v_pd_tweezer_1064_rampdown_end,
+                          v_end=self.p.v_pd_tweezer_1064_rampdown2_end,
+                          paint=True,keep_trap_frequency_constant=True)
+        delay(2.e-3)
+        # tweezer evap 3 with constant trap frequency
+        self.tweezer.ramp(t=self.p.t_tweezer_1064_rampdown3,
+                          v_start=tweezer_vpd1_to_vpd2(self.p.v_pd_tweezer_1064_rampdown2_end),
+                          v_end=self.p.v_pd_tweezer_1064_rampdown3_end,
+                          paint=True,keep_trap_frequency_constant=True,low_power=True)
+
+        self.dac.supply_current_2dmot.set(v=0.)
+
+        self.outer_coil.ramp_supply(t=20.e-3,
+                             i_start=self.p.i_lf_tweezer_evap2_current,
+                             i_end=self.p.i_spin_mixture)
+        
+        self.ttl.pd_scope_trig.pulse(1.e-6)
+        self.outer_coil.start_pid()
+
+        delay(40.e-3)
+
+
+    @kernel
+    def run(self):
+        self.init_kernel()
+        self.load_2D_mot(self.p.t_2D_mot_load_delay)
+        self.scan()
+        # self.mot_observe()
+
+    def analyze(self):
+        import os
+        expt_filepath = os.path.abspath(__file__)
+        self.end(expt_filepath)
