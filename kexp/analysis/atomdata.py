atomdata fixes for averaged

kexp/analysis/atomdata.py
@@ -233,7 +233,7 @@ class atomdata():
             atomdata: The sliced atomdata object.
         """
 
-        ad = atomdata(self.run_info.run_id)
+        ad = atomdata(self.run_info.run_id, avg_repeats=self._analysis_tags.averaged)
         which_shot_idx = ensure_ndarray(which_shot_idx, enforce_1d=True)
 
         # repeat handling is broken right now, in that if the repeats are on the
@@ -262,6 +262,7 @@ class atomdata():
                 subset = arr[which_shot_idx]
                 _, subset_counts = np.unique(subset, return_counts=True)
                 if np.any(subset_counts > 1):
+                    print(subset_counts)
                     if not np.all(subset_counts == subset_counts[0]):
                         raise ValueError("When slicing into the axis with repeats, you must slice the same number of repeats for each value.")
                     n_repeats = subset_counts[0]
@@ -308,13 +309,17 @@ class atomdata():
         # only remove the sort_N and sort_idx for this xvar if is the only one of
         # its length (otherwise another xvar also uses that sort_idx list)
         sliced_xvardim = ad.xvardims[which_xvar_idx]
-        sort_N_idx = np.where(ad.sort_N == sliced_xvardim)[0][0]
+
+        if not self._analysis_tags.averaged:
+            sort_N_idx = np.where(ad.sort_N == sliced_xvardim)[0][0]
+            
         if len(which_shot_idx) == 1 and ad.Nvars > 1:
             # if you are slicing out a single shot, remove the xvar to reduce
             # the dimensionality of the dataset
-            if np.sum(ad.xvardims == sliced_xvardim) == 1:
-                ad.sort_N = remove_element_by_index(ad.sort_N, sort_N_idx)
-                ad.sort_idx = remove_element_by_index(ad.sort_idx, sort_N_idx)
+            if not self._analysis_tags.averaged:
+                if np.sum(ad.xvardims == sliced_xvardim) == 1:
+                    ad.sort_N = remove_element_by_index(ad.sort_N, sort_N_idx)
+                    ad.sort_idx = remove_element_by_index(ad.sort_idx, sort_N_idx)
 
             for k in keys:
                 vars(ad)[k] = remove_element_by_index(vars(ad)[k],
@@ -324,11 +329,12 @@ class atomdata():
         else:
             # otherwise, just slice the xvar without reducing atomdata dimensionality
             if ad.sort_idx.size != 0:
-                ad.sort_N[sort_N_idx] = len(which_shot_idx)
-                ad.sort_idx[sort_N_idx] = grab_these_sort_idx(which_shot_idx,which_xvar_idx)
-                # you left out some sort_idx elements, so remap the sort_idx to
-                # sequential integers starting from 0.
-                ad.sort_idx[sort_N_idx] = remap_sort_idx_to_sequential(ad.sort_idx[sort_N_idx])
+                if not self._analysis_tags.averaged:
+                    ad.sort_N[sort_N_idx] = len(which_shot_idx)
+                    ad.sort_idx[sort_N_idx] = grab_these_sort_idx(which_shot_idx,which_xvar_idx)
+                    # you left out some sort_idx elements, so remap the sort_idx to
+                    # sequential integers starting from 0.
+                    ad.sort_idx[sort_N_idx] = remap_sort_idx_to_sequential(ad.sort_idx[sort_N_idx])
 
             ad.xvars[which_xvar_idx] = np.take(ad.xvars[which_xvar_idx],
                                                indices=which_shot_idx,

kexp/analysis/standard_experiments.py
@@ -92,6 +92,7 @@ def rabi_oscillation(ad:atomdata,
                      plot_bool=True,
                      plot_raw_data=True,
                      fit_params_on_plot=True,
+                     fit_params_on_left=True,
                      fit_guess_frequency=dv_fit_guess_rabi_frequency,
                      fit_guess_phase=np.pi/2,
                      fit_guess_amp=1.,
@@ -128,6 +129,8 @@ def rabi_oscillation(ad:atomdata,
         avg_repeats (bool, optional): If True, averages repeats and plots with error bars.
         plot_bool (bool, optional): If True, plots the data and fit. Defaults to True.
         fit_params_on_plot (bool, optional): If True, puts the fit parameter values on the plot.
+        fit_params_on_left (bool, optional): If True, puts the fit parameter box
+        on the left side of the plot.
         
 
     Returns:
@@ -289,8 +292,15 @@ def rabi_oscillation(ad:atomdata,
                 fit_params_str = f"$\Omega$ = $2\pi \\times {popt[0]/(2*np.pi)/1.e3:1.2f}$ kHz"\
                     +f"\n$A = {popt[3]:1.2f}$, $B = {popt[2]:1.2f}$"\
                     +f"\n$\\tau = {popt[4]*1.e6:1.2f}$ us"
-                ax.text(0.6, 0.75, fit_params_str, transform=ax.transAxes,
-                        bbox=dict(facecolor='white', alpha=0.5, edgecolor='black', boxstyle='round,pad=0.5'))
+                if fit_params_on_left:
+                    x_pos = 0.05
+                    ha = 'left'
+                else:
+                    x_pos = 0.6
+                    ha = 'left'
+                ax.text(x_pos, 0.75, fit_params_str, transform=ax.transAxes,
+                        bbox=dict(facecolor='white', alpha=0.5, edgecolor='black', boxstyle='round,pad=0.5'),
+                        ha=ha)
             except:
                 pass
 