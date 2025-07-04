e91f3d26e41e7e46c11b117e88c641be7591c359
Herbert-Broida <weldlabucsb@gmail.com>
2025-07-03 17:05:08 -0700

    no slm for non andor

--- kexp/experiments/default_experiments/mot_tof.py
+++ kexp/experiments/default_experiments/mot_tof.py
@@ -6,27 +6,28 @@ import numpy as np
 class gm_tof(EnvExperiment, Base):
 
     def prepare(self):
-        Base.__init__(self,setup_camera=True,camera_select='xy_basler',save_data=True)
+        Base.__init__(self,setup_camera=True,camera_select='xy_basler',save_data=False)
 
-        # self.xvar('frequency_detuned_imaging',np.arange(-150.,150.,8)*1.e6)
+        # self.xvar('frequency_detuned_imaging',np.arange(-50.,50.,5)*1.e6)
 
 
-        # self.xvar('dumdum',[0]*5)
+        self.xvar('dumdum',[0]*1000)
 
-        self.xvar('t_tof',np.linspace(.05,.5,10)*1.e-3)
+        # self.xvar('t_tof',np.linspace(.05,.5,10)*1.e-3)
+
         
-        self.p.amp_imaging = .35
+        # self.p.amp_imaging = .35
         self.p.imaging_state = 2.
-        self.p.t_tof = 1.e-3
-        self.p.t_mot_load = .3
+        self.p.t_tof = 20e-6
+        self.p.t_mot_load = 1.
         self.p.N_repeats = 1
 
         self.finish_prepare(shuffle=False)
 
     @kernel
     def scan_kernel(self):
-        self.dds.imaging.set_dds(amplitude=self.p.amp_imaging)
-        # self.set_imaging_detuning(self.p.frequency_detuned_imaging)
+        # self.dds.imaging.set_dds(amplitude=self.p.amp_imaging)
+        self.set_imaging_detuning(self.p.frequency_detuned_imaging)     
         
         self.mot(self.p.t_mot_load)
         self.dds.push.off()
