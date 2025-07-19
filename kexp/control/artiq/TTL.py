3d23b96ff32a36cd8d504b9ff3e7633435d402da
hpbroida <weldlabucsb@gmail.com>
2025-07-18 18:06:39 -0700

    setup inputs

--- kexp/control/artiq/TTL.py
+++ kexp/control/artiq/TTL.py
@@ -2,6 +2,11 @@ from artiq.experiment import delay, kernel
 from artiq.language.core import at_mu
 from artiq.coredevice.ttl import TTLOut, TTLInOut
 import artiq.experiment
+import numpy as np
+
+T_LINE_TRIGGER_SAMPLE_INTERVAL = 1/60
+T_LINE_TRIGGER_RTIO_DELAY = 100.e-6
+dv = np.int64(-1)
 
 class TTL():
     def __init__(self,ch):
@@ -31,29 +36,31 @@ class TTL_OUT(TTL):
         delay(t)
         self.ttl_device.off()
 
-T_LINE_TRIGGER_SAMPLE_INTERVAL = 30.e-3
-T_LINE_TRIGGER_RTIO_DELAY = 5.e-6
-
 class TTL_IN(TTL):
     def __init__(self,ch):
         super().__init__(ch)
         self.ttl_device = TTLInOut
+
+        self.t_input_gate_end = np.int64(0)
 
     @kernel
     def wait_for_line_trigger(self):
         while True:
             t_end = self.ttl_device.gate_rising(T_LINE_TRIGGER_SAMPLE_INTERVAL)
             t_edge = self.ttl_device.timestamp_mu(t_end)
-            
-            while True:
-                t_other_edge = self.ttl_device.timestamp_mu(t_end)
-                if t_other_edge == -1:
-                    break
-            
+            self.t_input_gate_end = t_end
             if t_edge > 0:
                 at_mu(t_edge)
                 delay(T_LINE_TRIGGER_RTIO_DELAY)
-                # read out the remaining input events to clear the input FIFO
+                break
+
+    @kernel
+    def clear_input_events(self,t_end=dv):
+        if t_end == dv:
+            t_end = self.t_input_gate_end
+        while True:
+            t_other_edge = self.ttl_device.timestamp_mu(t_end)
+            if t_other_edge == -1:
                 break
 
 class DummyTTL(TTL):
--- kexp/experiments/test/line_trigger_demo_class.py
+++ kexp/experiments/test/line_trigger_demo_class.py
@@ -4,27 +4,71 @@ from artiq.coredevice.ttl import TTLInOut, TTLOut
 from artiq.coredevice.core import Core, rtio_get_counter
 from artiq.language.core import at_mu, now_mu
 import numpy as np
+from kexp.control.artiq import TTL
+
+T_LINE_TRIGGER_SAMPLE_INTERVAL = 1/60
+T_LINE_TRIGGER_RTIO_DELAY = 100.e-6
+
+class TTL_IN(TTL):
+    def __init__(self,ch):
+        super().__init__(ch)
+        self.ttl_device = TTLInOut
+
+    @kernel
+    def wait_for_line_trigger(self,slack_fcn,edge_count_fcn) -> TInt64:
+        t_end = np.int64(-1)
+        while True:
+            t_end = self.ttl_device.gate_rising(T_LINE_TRIGGER_SAMPLE_INTERVAL)
+            t_edge = self.ttl_device.timestamp_mu(t_end)
+            if t_edge > 0:
+                at_mu(t_edge)
+                delay(T_LINE_TRIGGER_RTIO_DELAY)
+                break
+        return t_end
+
+    @kernel
+    def clear_input_events(self,t_end):
+        while True:
+            t_other_edge = self.ttl_device.timestamp_mu(t_end)
+            if t_other_edge == -1:
+                break
+
+class TTL_OUT(TTL):
+    def __init__(self,ch):
+        super().__init__(ch)
+        self.ttl_device = TTLOut
+    
+    @kernel
+    def on(self):
+        self.ttl_device.on()
+
+    @kernel
+    def off(self):
+        self.ttl_device.off()
+
+    @kernel
+    def pulse(self,t):
+        self.ttl_device.on()
+        delay(t)
+        self.ttl_device.off()
 
 class line_trigger(EnvExperiment):
 
     def prepare(self):
 
         self.core = self.get_device('core')
-        self.ttl_in = self.get_device('ttl0')
-        self.ttl_out = self.get_device('ttl4')
-        self.ttl_trig = self.get_device('ttl5')
 
-        # self.core = self.get_device('core')
-        # self.ttl_in = self.get_device('ttl40')
-        # self.ttl_out = self.get_device('ttl21')
-        # self.ttl_trig = self.get_device('ttl16')
+        self.ttl_out = TTL_OUT(4)
+        self.ttl_trig = TTL_OUT(5)
+        self.ttl_in = TTL_IN(0)
+
+        self.ttl_out.get_device(self)
+        self.ttl_trig.get_device(self)
+        self.ttl_in.get_device(self)
 
         self.core: Core
-        self.ttl_in: TTLInOut
-        self.ttl_out: TTLOut
-        self.ttl_trig: TTLOut
  
-        self.N = 5
+        self.N = 1
 
         self.t = np.zeros(10000,dtype=np.int64)
         self.t2 = np.zeros(100,dtype=np.int64)
@@ -57,18 +101,30 @@ class line_trigger(EnvExperiment):
         self.ttl_trig.pulse(1.e-6)  
         for _ in range(self.N):
 
-            self.wait_for_TTL()
+            # self.wait_for_TTL()
+            self.get_slack()
+            t_end = np.int64(1)
+            t_end = self.ttl_in.wait_for_line_trigger(self.get_slack,self.edge_count)
+            # self.wait_for_TTL()
+            self.get_slack()
+            self.ttl_out.pulse(1.e-3)
 
+            self.core.break_realtime()
+            self.ttl_in.clear_input_events(t_end)
             self.core.break_realtime()
             self.idx3 = self.idx3 + 1
             self.idx4 = self.idx4 + 1
         delay(1.e-3)
 
     @kernel
+    def edge_count(self):
+        self.edges[self.idx4] = self.edges[self.idx4] + 1
+
+    @kernel
     def wait_for_TTL(self):          
         edge = False
         while not edge:
-            t_end = self.ttl_in.gate_rising(1/60)       #opens gate for rising edges to be detected on TTL0
+            t_end = self.ttl_in.gate_rising(1/60 * 2)       #opens gate for rising edges to be detected on TTL0
                                                         #sets variable t_end as time(in MUs) at which detection stops
                                                 
             t_edge = self.ttl_in.timestamp_mu(t_end)    #sets variable t_edge as time(in MUs) at which first edge is detected
@@ -79,8 +135,8 @@ class line_trigger(EnvExperiment):
                 self.get_slack()
                 at_mu(t_edge)                       #set time cursor to position of edge
                 self.get_slack()
-                delay(10.e-6)
-                self.get_slack()
+                delay(5.e-6)
+                # delay(-1.e-6)
                 # self.ttl_trig.pulse(1.e-6)
                 self.ttl_out.pulse(5.e-3)
                 self.edges[self.idx4] = self.edges[self.idx4] + 1
