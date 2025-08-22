2a98d04633afe6acb4f70f9d98251eb9c0e79838
hpbroida <weldlabucsb@gmail.com>
2025-08-22 11:52:13 -0700

    Update ramsey.py

--- kexp/experiments/ramsey.py
+++ kexp/experiments/ramsey.py
@@ -12,30 +12,48 @@ class tweezer_load(EnvExperiment, Base):
     def prepare(self):
         Base.__init__(self,setup_camera=True,camera_select='andor',save_data=True)
 
-        f_range = 20.e3
-        df = 5.e3
-        self.p.frequency_raman_transition = 41.099e6
+        # f_range = 20.e3
+        # df = 5.e3
+        # self.p.frequency_raman_transition = 41.099e6
         # self.xvar('frequency_raman_transition',
         #           self.p.frequency_raman_transition + np.arange(-f_range, f_range +df, df))
 
-        # self.xvar('t_ramsey_delay', np.linspace(10.,90.,10)*1.e-6)
-        self.p.t_ramsey_delay = 10.e-6
+        # self.xvar('t_ramsey_delay', np.linspace(3.,90.,10)*1.e-6)
+        # self.p.t_ramsey_delay = 10.e-6
+        self.p.t_ramsey_delay = 5.e-6
 
         # self.xvar('line_trigger_phase_delay', np.linspace(0.00, 0.017, 10))
         self.p.line_trigger_phase_delay = 0.
-
-        self.xvar('global_phase_shift',np.linspace(0.,np.pi,5))
+        
+        self.xvar('global_phase_shift',np.linspace(0.,np.pi,10))
         self.p.global_phase_shift = 0.
 
-        self.p.do_pi_pulse = 0
+        self.p.do_pi_pulse = 1
 
         self.p.t_tof = 600.e-6
         self.p.t_tweezer_hold = .1e-3
 
         self.p.t_mot_load = 1.
-        self.p.N_repeats = 2
+        self.p.N_repeats = 3
 
         self.finish_prepare(shuffle=True)
+
+    @kernel
+    def scan_kernel(self):
+
+        self.set_imaging_detuning(self.p.frequency_detuned_imaging_m1)
+
+        self.prepare_lf_tweezers()
+
+        self.init_raman_beams(self.p.frequency_raman_transition,self.p.amp_raman)
+
+        self.line_trigger()
+
+        self.state_prep()
+        
+        self.ramsey()
+
+        self.release_and_image()
 
     @kernel
     def ramsey(self):
@@ -48,35 +66,27 @@ class tweezer_load(EnvExperiment, Base):
         self.raman.pulse(self.p.t_raman_pi_pulse/2)
 
     @kernel
-    def scan_kernel(self):
-
-        self.set_imaging_detuning(self.p.frequency_detuned_imaging_m1)
-
-        self.prepare_lf_tweezers()
-        self.ttl.d2_mot_shutter.off()
-
-        self.init_raman_beams(self.p.frequency_raman_transition,self.p.amp_raman)
-
+    def line_trigger(self):
         # line trigger
         self.ttl.line_trigger.wait_for_line_trigger()
         delay(1.e-3)
         delay(self.p.line_trigger_phase_delay)
 
+    @kernel
+    def state_prep(self):
         # state prep
         if self.p.do_pi_pulse:
             self.raman.pulse(self.p.t_raman_pi_pulse)
         else:
             delay(self.p.t_raman_pi_pulse)
         
-        self.ramsey()
-
+    @kernel
+    def release_and_image(self):
         delay(self.p.t_tweezer_hold)
         self.tweezer.off()
-
         delay(self.p.t_tof)
-
         self.abs_image()
-
+        
     @kernel
     def run(self):
         self.init_kernel()
