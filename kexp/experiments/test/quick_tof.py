f6f1a16568429d86a091dc55fa31eca41ef0c3bb
hpbroida <weldlabucsb@gmail.com>
2025-08-22 11:52:15 -0700

    Create quick_tof.py

---
+++ kexp/experiments/test/quick_tof.py
@@ -0,0 +1,46 @@
+from artiq.experiment import *
+from artiq.experiment import delay
+from kexp import Base, img_types
+import numpy as np
+from kexp.util.artiq.async_print import aprint
+from kexp.control.slm.slm import SLM
+from kexp.calibrations.tweezer import tweezer_vpd1_to_vpd2
+from kexp.calibrations.imaging import high_field_imaging_detuning
+
+class tweezer_load(EnvExperiment, Base):
+
+    def prepare(self):
+        Base.__init__(self,setup_camera=True,camera_select='andor',save_data=True)
+
+        self.p.t_tof = 600.e-6
+        self.xvar('t_tof', np.linspace(20.,1500.,10))
+        self.p.t_tweezer_hold = .1e-3
+
+        self.p.t_mot_load = 1.
+        self.p.N_repeats = 2
+
+        self.finish_prepare(shuffle=True)
+
+    @kernel
+    def scan_kernel(self):
+
+        self.set_imaging_detuning(self.p.frequency_detuned_imaging_m1)
+
+        self.prepare_lf_tweezers()
+
+        delay(self.p.t_tweezer_hold)
+        self.tweezer.off()
+        delay(self.p.t_tof)
+        self.abs_image()
+        
+    @kernel
+    def run(self):
+        self.init_kernel()
+        self.load_2D_mot(self.p.t_2D_mot_load_delay)
+        self.scan()
+        # self.mot_observe()
+
+    def analyze(self):
+        import os
+        expt_filepath = os.path.abspath(__file__)
+        self.end(expt_filepath)
