aa06bdcec01e29e63c6f19a343044377b260483c
hpbroida <weldlabucsb@gmail.com>
2025-09-03 11:46:07 -0700

    good to go

--- kexp/control/artiq/Sampler_CH.py
+++ kexp/control/artiq/Sampler_CH.py
@@ -2,9 +2,14 @@ from artiq.experiment import delay, kernel, TFloat
 from artiq.coredevice.sampler import Sampler
 import numpy as np
 
+di = -1
+
 class Sampler_CH():
-    def __init__(self,ch,sample_array:np.ndarray):
+    def __init__(self,ch,
+                 gain=0,
+                 sample_array=np.zeros(8,dtype=float)):
         self.ch = ch
+        self.gain = gain
         self.sampler_device = Sampler
         self.key = ""
         self.samples = sample_array
@@ -13,3 +18,8 @@ class Sampler_CH():
     def sample(self) -> TFloat:
         self.sampler_device.sample(self.samples)
         return self.samples[self.ch]
+    
+    @kernel
+    def set_gain(self,gain=di):
+        self.gain = gain if gain !=  di else self.gain
+        self.sampler_device.set_gain_mu(self.ch,gain=self.gain)
