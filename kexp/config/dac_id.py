d4cac8cba5d4d2066a74a562beb4f460154425ea
hpbroida <weldlabucsb@gmail.com>
2025-12-02 15:14:40 -0800

    add channel number to populate dac, ttl lists

--- kexp/config/dac_id.py
+++ kexp/config/dac_id.py
@@ -5,13 +5,15 @@ from artiq.coredevice.zotino import Zotino
 from waxx.config.dac_id import dac_frame as dac_frame_waxx
 from waxx.control.artiq.DAC_CH import DAC_CH
 from kexp.config.expt_params import ExptParams
+from kexp.util.db.device_db import device_db
 
 FORBIDDEN_CH = []
+N_CH = 32
 
 class dac_frame(dac_frame_waxx):
     def __init__(self, expt_params = ExptParams(), dac_device = Zotino):
 
-        self.setup(expt_params, dac_device)
+        self.setup(expt_params, dac_device, N_CH)
         self.dac_device: Zotino
         self.p: ExptParams
 
--- kexp/config/ttl_id.py
+++ kexp/config/ttl_id.py
@@ -5,10 +5,16 @@ from artiq.coredevice.ttl import TTLOut, TTLInOut
 from waxx.config.ttl_id import ttl_frame as ttl_frame_waxx
 from waxx.control.artiq.TTL import TTL, TTL_IN, TTL_OUT
 
+from kexp.util.db.device_db import device_db
+
+N_TTL = 88
+
 class ttl_frame(ttl_frame_waxx):
     def __init__(self):
 
-        self.setup()
+        self._db = device_db
+
+        self.setup(N_TTL)
         
         self.img_beam_sw = self.assign_ttl_out(0)
         self.tweezer_pid2_enable = self.assign_ttl_out(1)
