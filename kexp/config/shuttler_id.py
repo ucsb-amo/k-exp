3ebd9fbf75fc8ba8e7d7178ae9b2ca722d1a2a08
hpbroida <weldlabucsb@gmail.com>
2025-12-02 11:40:09 -0800

    kexp frames as child classes of waxx frames

--- kexp/config/shuttler_id.py
+++ kexp/config/shuttler_id.py
@@ -4,21 +4,15 @@ from artiq.experiment import kernel, portable
 from artiq.coredevice.zotino import Zotino
 from artiq.coredevice.shuttler import DCBias, DDS, Relay, Trigger, Config, shuttler_volt_to_mu
 
+from waxx.config.shuttler_id import shuttler_frame as shuttler_frame_waxx
 from waxx.control.artiq.Shuttler_CH import Shuttler_CH
 
-class shuttler_frame():
+class shuttler_frame(shuttler_frame_waxx):
     def __init__(self):
 
         ### Setup
 
-        self._STATE_BASE = np.array([1<<n for n in range(16)])
-
-        self.shuttler_list = []
-        self._relay_state = np.zeros(16,dtype=int)
-
-        self._config = Config
-        self._trigger = Trigger
-        self._relay = Relay
+        self.setup()
 
         ### Channel assignment
 
@@ -26,42 +20,4 @@ class shuttler_frame():
 
         ###
 
-        self._write_dds_keys()
-
-    def _write_dds_keys(self):
-        '''Adds the assigned keys to the DDS objects so that the user-defined
-        names (keys) are available with the DDS objects.'''
-        for key in self.__dict__.keys():
-            if isinstance(self.__dict__[key],Shuttler_CH):
-                self.__dict__[key].key = key
-
-    def _assign_ch(self,ch) -> Shuttler_CH:
-        shuttler_ch = Shuttler_CH(ch, relay_state=self._relay_state)
-        shuttler_ch._relay = self._relay
-        shuttler_ch._trigger = self._trigger
-        self.shuttler_list.append(shuttler_ch)
-        return shuttler_ch
-    
-    @portable
-    def _relay_state_to_int(self):
-        return self._STATE_BASE @ self._relay_state
-
-    @kernel
-    def update_relay(self,chs,states):
-        self._relay_state[chs] = states
-        self._relay.enable(self._relay_state_to_int())
-
-    @kernel
-    def trigger(self):
-        self._trigger(self._relay_state_to_int())
-
-    @kernel
-    def set_gain(self,ch,gain_mu):
-        self._config.set_gain(ch,gain_mu)
-
-    @kernel
-    def init(self):
-        for ch in range(16):
-            self.set_gain(ch,0)
-            delay(10.e-6)
-        self._relay.init()
+        self.cleanup()
