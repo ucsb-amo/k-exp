b5f45bebd273133c78dcbec81ece4c2513e98c36
hpbroida <weldlabucsb@gmail.com>
2025-12-03 10:46:00 -0800

    gamin

--- kexp/experiments/test/monitor_test.py
+++ kexp/experiments/test/monitor_test.py
@@ -16,13 +16,13 @@ class testcrate_base(EnvExperiment, Base):
 
     @kernel
     def run(self):
-        # self.init_kernel(run_id=False,init_lightsheet=False,setup_awg=False,setup_slm=False,dds_set=False,
-        #                  dds_off=False,beat_ref_on=True,init_shuttler=False)
-        self.init_kernel(run_id=False,
-                         init_dds=False,
-                         init_dac=False,
-                         dds_set=False,
-                         dds_off=False)
+        self.init_kernel(run_id=False,init_lightsheet=False,setup_awg=False,setup_slm=False,dds_set=False,
+                         dds_off=False,beat_ref_on=True,init_shuttler=False)
+        # self.init_kernel(run_id=False,
+        #                  init_dds=False,
+        #                  init_dac=False,
+        #                  dds_set=False,
+        #                  dds_off=False)
 
         while True:
             self.core.wait_until_mu(now_mu())
