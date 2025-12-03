6e4ef3a02d9bd58d30908d6819221e3d2f647642
hpbroida <weldlabucsb@gmail.com>
2025-12-03 11:09:43 -0800

    liveOD changes to implement monitor

--- kexp/util/live_od/__init__.py
+++ kexp/util/live_od/__init__.py
@@ -4,3 +4,4 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
 from .camera_mother import CameraMother, CameraBaby, CameraNanny, DataHandler
+from .monitor_manager import MonitorManager
