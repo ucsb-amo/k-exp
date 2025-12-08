ba1b8b16325806659fea8338d469d71cb8c4765e
jpagett <pagett.jared@gmail.com>
2025-12-08 07:54:26 -0800

    instance the generator object in kexp

--- kexp/_bat/regenerate_device_state_file.bat
+++ kexp/_bat/regenerate_device_state_file.bat
@@ -1,4 +1,4 @@
 call %kpy%
-cd %code%\wax\waxx-src\waxx
-python %code%\wax\waxx-src\waxx\util\device_state\generate_state_file.py
+cd %code%\k-exp
+python %code%\k-exp\kexp\util\guis\device_state_gui\generate_state_file.py
 pause
---
+++ kexp/util/guis/device_state_gui/generate_state_file.py
@@ -0,0 +1,11 @@
+from waxx.util.device_state.generate_state_file import Generator
+
+from kexp.config.dds_id import dds_frame
+from kexp.config.dac_id import dac_frame
+from kexp.config.ttl_id import ttl_frame
+
+gen = Generator(dds_frame=dds_frame(),
+          ttl_frame=ttl_frame(),
+          dac_frame=dac_frame())
+
+gen.generate()
