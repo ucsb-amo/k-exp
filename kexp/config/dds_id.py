fdb22fd7cc3e1c48bf926afdf84195454fca1ce1
hpbroida <weldlabucsb@gmail.com>
2025-12-10 14:05:54 -0800

    ry classes, objects

--- kexp/config/dds_id.py
+++ kexp/config/dds_id.py
@@ -49,64 +49,9 @@ class dds_frame(dds_frame_waxx):
         self.dds_vva_calibration = DDS_VVA_Calibration()
 
         # self.aom_name = self.dds_assign(urukul_idx,ch_idx,ao_order,transition,dac_ch_vpd)
-        self.antenna_rf = self.dds_assign(0,0,default_freq=200.e6,default_amp=self.p.amp_rf_source)
-        self.push = self.dds_assign(2,0, ao_order = 1, transition = 'D2',
-                                    default_detuning = self.p.detune_push,
-                                    default_amp = self.p.amp_push)
-        # old 2d MOT AOs
-        self.d2_2dv_r = self.dds_assign(2,1, ao_order = 1, transition = 'D2',
-                                    default_detuning = self.p.detune_d2v_r_2dmot,
-                                    default_amp = self.p.amp_d2v_r_2dmot)
-        self.d2_2dv_c = self.dds_assign(2,2, ao_order = -1, transition = 'D2',
-                                    default_detuning = self.p.detune_d2v_c_2dmot,
-                                    default_amp = self.p.amp_d2v_c_2dmot)
-        # old 3d MOT AOs
-        self.d2_2dh_r = self.dds_assign(2,3, ao_order = 1, transition = 'D2',
-                                    default_detuning = self.p.detune_d2h_r_2dmot,
-                                    default_amp = self.p.amp_d2h_r_2dmot)
-        self.d2_2dh_c = self.dds_assign(3,0, ao_order = -1, transition = 'D2',
-                                    default_detuning = self.p.detune_d2h_c_2dmot,
-                                    default_amp = self.p.amp_d2h_c_2dmot)
-        self.mot_killer = self.dds_assign(3,1, ao_order = -1, transition = 'D2',
-                                    default_detuning = 0.,
-                                    default_amp = 0.188)
-        self.beatlock_ref = self.dds_assign(3,2,
-                                            default_freq=42.26e6,
-                                            default_amp=0.1)
-        self.d1_3d_c = self.dds_assign(3,3, ao_order = -1, transition = 'D1',
-                                    dac_ch_vpd = self._dac_frame.vva_d1_3d_c.ch,
-                                    default_detuning = self.p.detune_d1_c_gm,
-                                    default_amp = self.p.amp_d1_3d_c)
-        self.d1_3d_r = self.dds_assign(4,0, ao_order = 1, transition = 'D1',
-                                    dac_ch_vpd = self._dac_frame.vva_d1_3d_r.ch,
-                                    default_detuning = self.p.detune_d1_r_gm,
-                                    default_amp = self.p.amp_d1_3d_r)
-        self.imaging = self.dds_assign(4,1, ao_order = 1,
-                                    default_freq = 350.e6,
-                                    default_amp = 0.5,
-                                    dac_ch_vpd=self._dac_frame.imaging_pid.ch)
-        self.imaging_x_switch = self.dds_assign(5,2,
-                                        ao_order=1,
-                                        default_freq=100.e6,
-                                        default_amp=0.3)
-        # self.op_r = self.dds_assign(4,2, ao_order = 1, transition = 'D1',
-        #                             default_detuning = self.p.detune_optical_pumping_op,
-        #                             default_amp = self.p.amp_optical_pumping_op)
-
-        self.raman_80_plus = self.dds_assign(4,2, ao_order = 1,
-                                    default_freq = 80.e6,
-                                    default_amp = 0.277)
-        self.raman_150_plus = self.dds_assign(5,3, ao_order = 1,
-                                    default_freq = 150.e6,
-                                    default_amp = 0.324)
-        self.raman_150_minus = self.dds_assign(5,0,
-                                        ao_order=-1,
-                                        default_freq=150.e6,
-                                        default_amp=0.324)
-        
-        self.optical_pumping = self.dds_assign(4,3, ao_order = -1, transition = 'D1',
-                                    default_detuning = self.p.detune_optical_pumping_r_op,
-                                    default_amp = self.p.amp_optical_pumping_r_op)
+        self.antenna_rf = self.dds_assign(0,0,
+                                    default_freq=200.e6,
+                                    default_amp=self.p.amp_rf_source)
         self.tweezer_pid_1 = self.dds_assign(0,3, ao_order = 1,
                                     default_freq = 80.e6,
                                     dac_ch_vpd = self._dac_frame.v_pd_tweezer_pid1.ch,
@@ -115,19 +60,72 @@ class dds_frame(dds_frame_waxx):
                                     default_freq = 200.e6,
                                     dac_ch_vpd = self._dac_frame.v_pd_tweezer_pid2.ch,
                                     default_amp = self.p.amp_tweezer_pid2)
-        self.imaging_eo = self.dds_assign(1,1,
-                                    default_freq = 2.e6,
-                                    default_amp = 1.)
-        self.d1_beatlock_ref = self.dds_assign(5,1,
-                                    default_freq=42.26e6,
-                                    default_amp=0.1)
-        
-        
+        self.ry_405 = self.dds_assign(1,1,
+                                    default_freq = 80.e6,
+                                    default_amp = 0.188,
+                                    ao_order = 1)
         self.d2_3d_c = self.dds_assign(1,2, ao_order = -1, transition = 'D2',
                                     default_detuning = self.p.detune_d2_c_mot,
                                     default_amp = self.p.amp_d2_c_mot)
         self.d2_3d_r = self.dds_assign(1,3, ao_order = 1, transition = 'D2',
                                     default_detuning = self.p.detune_d2_r_mot,
                                     default_amp = self.p.amp_d2_r_mot)
+
+        self.push = self.dds_assign(2,0, ao_order = 1, transition = 'D2',
+                                    default_detuning = self.p.detune_push,
+                                    default_amp = self.p.amp_push)
+        self.d2_2dv_r = self.dds_assign(2,1, ao_order = 1, transition = 'D2',
+                                    default_detuning = self.p.detune_d2v_r_2dmot,
+                                    default_amp = self.p.amp_d2v_r_2dmot)
+        self.d2_2dv_c = self.dds_assign(2,2, ao_order = -1, transition = 'D2',
+                                    default_detuning = self.p.detune_d2v_c_2dmot,
+                                    default_amp = self.p.amp_d2v_c_2dmot)
+        self.d2_2dh_r = self.dds_assign(2,3, ao_order = 1, transition = 'D2',
+                                    default_detuning = self.p.detune_d2h_r_2dmot,
+                                    default_amp = self.p.amp_d2h_r_2dmot)
+        
+        self.d2_2dh_c = self.dds_assign(3,0, ao_order = -1, transition = 'D2',
+                                    default_detuning = self.p.detune_d2h_c_2dmot,
+                                    default_amp = self.p.amp_d2h_c_2dmot)
+        self.mot_killer = self.dds_assign(3,1, ao_order = -1, transition = 'D2',
+                                    default_detuning = 0.,
+                                    default_amp = 0.188)
+        self.beatlock_ref = self.dds_assign(3,2,
+                                    default_freq=42.26e6,
+                                    default_amp=0.1)
+        self.d1_3d_c = self.dds_assign(3,3, ao_order = -1, transition = 'D1',
+                                    dac_ch_vpd = self._dac_frame.vva_d1_3d_c.ch,
+                                    default_detuning = self.p.detune_d1_c_gm,
+                                    default_amp = self.p.amp_d1_3d_c)
+        
+        self.d1_3d_r = self.dds_assign(4,0, ao_order = 1, transition = 'D1',
+                                    dac_ch_vpd = self._dac_frame.vva_d1_3d_r.ch,
+                                    default_detuning = self.p.detune_d1_r_gm,
+                                    default_amp = self.p.amp_d1_3d_r)
+        self.imaging = self.dds_assign(4,1, ao_order = 1,
+                                    default_freq = 350.e6,
+                                    default_amp = 0.5,
+                                    dac_ch_vpd=self._dac_frame.imaging_pid.ch)
+        self.raman_80_plus = self.dds_assign(4,2, ao_order = 1,
+                                    default_freq = 80.e6,
+                                    default_amp = 0.277)
+        self.optical_pumping = self.dds_assign(4,3, ao_order = -1, transition = 'D1',
+                                    default_detuning = self.p.detune_optical_pumping_r_op,
+                                    default_amp = self.p.amp_optical_pumping_r_op)
+        
+        self.raman_150_minus = self.dds_assign(5,0,
+                                ao_order=-1,
+                                default_freq=150.e6,
+                                default_amp=0.324)
+        self.d1_beatlock_ref = self.dds_assign(5,1,
+                                    default_freq=42.26e6,
+                                    default_amp=0.1)
+        self.imaging_x_switch = self.dds_assign(5,2,
+                                        ao_order=1,
+                                        default_freq=100.e6,
+                                        default_amp=0.3)
+        self.raman_150_plus = self.dds_assign(5,3, ao_order = 1,
+                                    default_freq = 150.e6,
+                                    default_amp = 0.324)
         
         self.cleanup()
--- kexp/control/rydberg_lasers.py
+++ kexp/control/rydberg_lasers.py
@@ -1,21 +1,27 @@
-from waxx.control.misc.sdg6000x import SDG6000X_CH, dv
+from artiq.language import TFloat
+
+from waxx.control.misc.sdg6000x import SDG6000X_CH, dv, SDG6000X_Params
 from waxx.control.artiq.TTL import TTL_OUT
 from waxx.control.artiq.DAC_CH import DAC_CH
 from waxx.control.artiq.DDS import DDS
 from artiq.language import now_mu, kernel, delay, portable
 
-class SiglentPIDBeam():
-    def __init__(self,
-                 siglent_ch:SDG6000X_CH,
-                 dac_pid_setpoint:DAC_CH):
+def remap_class(base_obj, target_class):
+    return target_class(**base_obj.__dict__)
+
+class SiglentPID_Params(SDG6000X_Params):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.v_pd = 0.
+
+class SiglentBeamBase():
+    def __init__(self,siglent_ch:SDG6000X_CH):
         self.siglent = siglent_ch
-        self.dac_pid = dac_pid_setpoint
-        self._sync_params()
-        
+        self._p_siglent = siglent_ch._p
+
     @kernel
     def init(self):
-        self.set_siglent(init=True)
-        self.siglent.set_output(1)
+        self.siglent.init(init=True)
 
     @kernel
     def set_siglent(self,
@@ -25,34 +31,12 @@ class SiglentPIDBeam():
         self.siglent.set(frequency=frequency,
                          amplitude=amplitude,
                          init=init)
-        self._sync_params()
-
-    @kernel
-    def set_power(self, v_pd=dv, load_dac=True):
-        self.dac_pid.set(v_pd, load_dac)
-        self._sync_params()
         
-    @portable
-    def _sync_params(self):
-        self.frequency = self.siglent.frequency
-        self.amplitude_vpp = self.siglent.amplitude_vpp
-        self.siglent_output = self.siglent.state
-        self.v_pd = self.dac_pid.v
-
-    @portable
-    def _read_params(self):
-        self.siglent.fetch_state()
-        self.frequency = self.siglent.frequency
-        self.amplitude_vpp = self.siglent.amplitude_vpp
-        self.state = self.siglent.state
-
-class SiglentDDSBeam(SiglentPIDBeam):
+class SiglentDDSBeam(SiglentBeamBase):
     def __init__(self,
                   siglent_ch:SDG6000X_CH,
-                  dac_pid_setpoint:DAC_CH,
                   dds_sw:DDS):
-        super().__init__(siglent_ch=siglent_ch,
-                         dac_pid_setpoint=dac_pid_setpoint)
+        super().__init__(siglent_ch=siglent_ch)
         self.dds_sw = dds_sw
     
     @kernel
@@ -66,13 +50,14 @@ class SiglentDDSBeam(SiglentPIDBeam):
     def off(self):
         self.dds_sw.off()
 
-class SiglentTTLBeam(SiglentPIDBeam):
+    def set_power(self,amplitude):
+        self.dds_sw.set_dds(amplitude=amplitude)
+
+class SiglentTTLBeam(SiglentBeamBase):
     def __init__(self,
                   siglent_ch:SDG6000X_CH,
-                  dac_pid_setpoint:DAC_CH,
                   ttl_sw:TTL_OUT):
-        super().__init__(siglent_ch=siglent_ch,
-                         dac_pid_setpoint=dac_pid_setpoint)
+        super().__init__(siglent_ch=siglent_ch)
         self.ttl_sw = ttl_sw
 
     def on(self):
@@ -80,6 +65,49 @@ class SiglentTTLBeam(SiglentPIDBeam):
 
     def off(self):
         self.ttl_sw.off()
+
+    def set_power(self):
+        raise ValueError('No power control configured!')
+
+class SiglentBeamPID(SiglentBeamBase):
+    def __init__(self,
+                 siglent_ch:SDG6000X_CH,
+                 dac_pid_setpoint:DAC_CH):
+        super().__init__(self,siglent_ch)
+
+        self.dac_pid = dac_pid_setpoint
+
+        self._p = remap_class(self._p_siglent, SiglentPID_Params)
+        self._p.v_pd = self.dac_pid.v
+
+    @kernel
+    def set_power(self, v_pd=dv, load_dac=True):
+        self.dac_pid.set(v_pd, load_dac)
+
+class SiglentDDSBeamPID(SiglentDDSBeam,SiglentBeamPID):
+    def __init__(self,
+                  siglent_ch:SDG6000X_CH,
+                  dds_sw:DDS,
+                  dac_pid_setpoint:DAC_CH):
+        super().__init__(siglent_ch=siglent_ch,
+                         dds_sw=dds_sw,
+                         dac_pid_setpoint=dac_pid_setpoint)
+
+class SiglentTTLBeamPID(SiglentTTLBeam,SiglentBeamPID):
+    def __init__(self,
+                  siglent_ch:SDG6000X_CH,
+                  dac_pid_setpoint:DAC_CH,
+                  ttl_sw:TTL_OUT):
+        super().__init__(siglent_ch=siglent_ch,
+                         ttl_sw=ttl_sw,
+                         dac_pid_setpoint=dac_pid_setpoint)
+
+class RydbergLaser_Params():
+    def __init__(self,
+                ry_405_params: SiglentPID_Params,
+                ry_980_params: SiglentPID_Params):
+        self._p_405 = ry_405_params
+        self._p_980 = ry_980_params
 
 class RydbergLasers():
     def __init__(self,
@@ -96,3 +124,38 @@ class RydbergLasers():
         self.beam_980 = SiglentTTLBeam(siglent_ch=siglent_980_cavity,
                                     dac_pid_setpoint=dac_pid_setpoint_980,
                                     ttl_sw=ttl_sw_980)
+        
+        self._p = RydbergLaser_Params(ry_405_params=self.beam_405._p,
+                                      ry_980_params=self.beam_980._p)
+        
+class CavityAOControlledRyDDSBeam(SiglentDDSBeam):
+    def __init__(self,
+                siglent_ch:SDG6000X_CH,
+                dds_sw:DDS,
+                ao_order_cavity=-1,
+                ao_order_pid=1,
+                frequency_pid_ao=80.e6):
+        super().__init__(siglent_ch,dds_sw=dds_sw)
+
+        self._ao_order_cavity = ao_order_cavity
+        self._ao_order_pid = ao_order_pid
+        self._frequency_pid_ao = frequency_pid_ao
+        self._f_siglent_detuning_reference = self.siglent._p.frequency
+
+    @kernel
+    def set_detuning(self,frequency_detuned):
+        f_ao = self.compute_detuning(frequency_detuned)
+        self.siglent.set(frequency=f_ao)
+
+    @portable(flags={"fast-math"})
+    def compute_detuning(self,frequency_detuned) -> TFloat:
+        delta = frequency_detuned
+        a_c = self._ao_order_cavity
+        f_0 = self._f_siglent_detuning_reference
+        a_pid = self._ao_order_pid
+        f_pid = self._frequency_pid_ao
+        a_sw = self.dds_sw.aom_order
+        f_sw = self.dds_sw.frequency
+        # print(delta,a_c,f_0,a_pid,f_pid,a_sw,f_sw)
+        f_ao = f_0 + (delta-2*a_pid*f_pid-2*a_sw*f_sw)/(-2*a_c)
+        return f_ao
