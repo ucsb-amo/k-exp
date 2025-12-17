6ea07a5383b7c08002e74dc90312fb2749edd80c
hpbroida <weldlabucsb@gmail.com>
2025-12-17 15:30:22 -0800

    Update siglent_id.py

--- kexp/config/siglent_id.py
+++ kexp/config/siglent_id.py
@@ -1,10 +1,11 @@
 from waxx.control.misc.sdg6000x import SDG6000X_CH
+from waxx.config.siglent_id import siglent_frame as siglent_frame_waxx
 from artiq.coredevice.core import Core
 
-class siglent_frame():
+class siglent_frame(siglent_frame_waxx):
     def __init__(self, core=Core):
 
-        self.core = core
+        self.setup(core)
 
         IP_RY_CAVITY_SIGLENT = "192.168.1.101"
         self.siglent_980 = self.assign_sdg6000x_ch(1,IP_RY_CAVITY_SIGLENT,
@@ -15,16 +16,5 @@ class siglent_frame():
                                                    frequency=80.0e6,
                                                    amplitude_vpp=0.362,
                                                    max_amplitude_vpp=1.0)
-
-    def assign_sdg6000x_ch(self, ch, ip,
-                           frequency,
-                           amplitude_vpp,
-                           max_amplitude_vpp,
-                           default_state=1) -> SDG6000X_CH:
-        siglent_ch = SDG6000X_CH(ch=ch,ip=ip,
-                           frequency=frequency,
-                           amplitude_vpp=amplitude_vpp,
-                           max_amplitude_vpp=max_amplitude_vpp,
-                           default_state = default_state,
-                           core=self.core)
-        return siglent_ch
+        
+        self.cleanup()
