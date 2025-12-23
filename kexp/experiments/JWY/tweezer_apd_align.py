2c7799d73a708919ceca95002fa8ec902e4d00c9
jpagett <pagett.jared@gmail.com>
2025-12-23 14:26:28 -0800

    x

--- kexp/experiments/JWY/tweezer_apd_align.py
+++ kexp/experiments/JWY/tweezer_apd_align.py
@@ -13,7 +13,7 @@ class tweezer_load(EnvExperiment, Base):
         Base.__init__(self,setup_camera=True,
                       camera_select=cameras.andor,
                       save_data=False,
-                      imaging_type=img_types.DISPERSIVE)
+                      imaging_type=img_types.ABSORPTION)
 
         # self.xvar('beans',[0,1]*50)
         # self.xvar('t_tof',np.linspace(100.,1500.,10)*1.e-6) 
@@ -32,10 +32,10 @@ class tweezer_load(EnvExperiment, Base):
         
         # self.xvar('amp_imaging',np.linspace(0.15,.4,10))
         # self.p.amp_imaging = .28
-        self.p.amp_imaging = .2
+        self.p.amp_imaging = .5
 
         # self.xvar('hf_imaging_detuning',np.linspace(-580.5e6,-300.5e6,50))
-        self.p.hf_imaging_detuning = -635.e6 # -572.e6 #-565.e6 # 182. -1
+        self.p.hf_imaging_detuning = -565.e6 #-635.e6 # -572.e6 #-565.e6 # 182. -1
         
         # self.xvar('dimension_slm_mask',np.linspace(10.e-6, 200.e-6, 8))
         # self.p.dimension_slm_mask = 50.e-6
@@ -48,45 +48,46 @@ class tweezer_load(EnvExperiment, Base):
         self.p.t_tof = 133.e-6
 
         self.p.t_mot_load = 1.
-        
+        self.scope = self.scope_data.add_siglent_scope("192.168.1.108", label='PD', arm=False)
+
         self.p.N_repeats = 1
 
         self.finish_prepare(shuffle=False)
 
     @kernel
     def scan_kernel(self):
+        self.set_imaging_detuning(frequency_detuned = self.p.frequency_detuned_imaging)
+        # self.set_imaging_detuning(frequency_detuned = self.p.frequency_detuned_imaging_m1)
+        # self.slm.write_phase_mask_kernel(phase=self.p.phase_slm_mask)
+        self.dds.imaging.set_dds(amplitude=self.p.amp_imaging)
 
-        self.set_imaging_detuning(frequency_detuned = self.p.hf_imaging_detuning)
-        # self.dds.imaging.set_dds(amplitude=self.p.amp_imaging)
-        self.slm.write_phase_mask_kernel(phase=self.p.phase_slm_mask)
-        # self.dds.imaging.set_dds(amplitude=self.p.amp_imaging)
+        self.prepare_lf_tweezers()
 
-        self.prepare_hf_tweezers()
-
-
-        self.init_raman_beams(frequency_transition = self.p.frequency_raman_transition, 
-                              fraction_power = self.params.fraction_power_raman)
-
+        self.init_raman_beams(self.p.frequency_raman_transition,self.p.fraction_power_raman)
         self.ttl.line_trigger.wait_for_line_trigger()
         delay(5.7e-3)
 
-        # self.raman.sweep(t=self.p.t_raman_sweep,
-        #                  frequency_center=self.p.frequency_raman_sweep_center,
-        #                  frequency_sweep_fullwidth=self.p.frequency_raman_sweep_width,
-        #                  n_steps=100)
+        # self.raman.pulse(t=self.p.t_raman_stateprep_pulse)
 
-        self.raman.pulse(self.p.t_raman_pulse)
+        self.dds.imaging.on()
+        self.raman.on()
+        self.ttl.pd_scope_trig.pulse(1.e-6)
+        delay(self.p.t_raman_pulse)
+        self.dds.imaging.off()
+        self.raman.off()
+
+        # self.imaging.init(frequency_polmod=10.e3,t_phase_origin_mu=now_mu())
 
         delay(self.p.t_tweezer_hold)
         self.tweezer.off()
-
-        delay(self.p.t_tof)
+        delay(self.p.t_tof)     
 
         self.abs_image()
 
-        self.outer_coil.stop_pid()
-
-        self.outer_coil.off()
+        self.core.wait_until_mu(now_mu())
+        self.scope.read_sweep(0)
+        self.core.break_realtime()
+        delay(20.e-3)
 
     @kernel
     def run(self):
