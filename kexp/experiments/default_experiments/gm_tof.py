d8fcef960ec48c7dca977f784f9c8bd01dfa2e11
jpagett <pagett.jared@gmail.com>
2026-01-05 15:45:01 -0800

    mot killer gm

--- kexp/experiments/default_experiments/gm_tof.py
+++ kexp/experiments/default_experiments/gm_tof.py
@@ -6,20 +6,20 @@ import numpy as np
 class gm_tof(EnvExperiment, Base):
 
     def prepare(self):
-        Base.__init__(self,setup_camera=True,save_data=True,
+        Base.__init__(self,setup_camera=True,save_data=False,
                       camera_select=cameras.xy_basler,
                       imaging_type=img_types.ABSORPTION)
 
         # self.xvar('frequency_detuned_imaging',np.arange(350.,450.,3)*1.e6)]
 
         # self.xvar('dumdum',[0]*10)
-        self.xvar('t_tof',np.linspace(10.,20.,10)*1.e-3)
+        # self.xvar('t_tof',np.linspace(10.,20.,10)*1.e-3)
         
         self.p.amp_imaging = .18
         self.p.imaging_state = 2.
-        self.p.t_tof = 19.e-3
+        self.p.t_tof = 15.e-3
         self.p.t_mot_load = .2
-        self.p.N_repeats = 1
+        self.p.N_repeats = 100
 
         self.finish_prepare(shuffle=True)
 
@@ -27,6 +27,8 @@ class gm_tof(EnvExperiment, Base):
     def scan_kernel(self):
         self.dds.imaging.set_dds(amplitude=self.p.amp_imaging)
         # self.set_imaging_detuning(self.p.frequency_detuned_imaging)
+
+        self.dds.mot_killer.set_dds_gamma(0.,.188)
         
         self.mot(self.p.t_mot_load)
         self.dds.push.off()
@@ -39,10 +41,14 @@ class gm_tof(EnvExperiment, Base):
 
         self.release()
 
+        self.dds.mot_killer.on()
+
         delay(self.p.t_tof)
 
         self.flash_repump()
         self.abs_image()
+
+        self.dds.mot_killer.off()
        
     @kernel
     def run(self):
