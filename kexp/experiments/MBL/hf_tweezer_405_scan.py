c2b376c70a4360f6f6cb5392cbca416b3072389d
jpagett <pagett.jared@gmail.com>
2026-01-13 18:56:48 -0800

    Update hf_tweezer_405_scan.py

--- kexp/experiments/MBL/hf_tweezer_405_scan.py
+++ kexp/experiments/MBL/hf_tweezer_405_scan.py
@@ -8,7 +8,7 @@ from kexp import Base, img_types, cameras
 class mag_trap(EnvExperiment, Base):
 
     def prepare(self):
-        Base.__init__(self,setup_camera=True,save_data=True,
+        Base.__init__(self,setup_camera=True,save_data=False,
                       camera_select=cameras.andor,
                       imaging_type=img_types.ABSORPTION)
         
@@ -16,12 +16,13 @@ class mag_trap(EnvExperiment, Base):
         # self.xvar('t_tof',np.linspace(1000.,4500.,10)*1.e-6)
 
         # self.xvar('t_pulse',np.linspace(0.,10.,5)*1.e-3)
-        self.p.t_pulse = 15.e-3
+        self.p.t_pulse = 30.e-3
         # self.p.t_pulse = 
 
-        self.xvar('woo',[0,1]*1)
+        # self.xvar('woo',[0,1]*1)
+        self.p.woo = 0
 
-        # self.xvar('frequency_405_cavity_ao',np.linspace(78.,84.,7)*1.e6)
+        self.xvar('frequency_405_cavity_ao',np.linspace(78.,82.,7)*1.e6)
 
         # self.p.frequency_405_cavity_ao = 80.0*1.e6
 
@@ -33,7 +34,7 @@ class mag_trap(EnvExperiment, Base):
         # self.p.hf_imaging_detuning = -617.e6 # 193.2
         self.p.imaging_state = 2.
 
-        self.p.N_repeats = 100
+        self.p.N_repeats = 10
         self.p.t_mot_load = 1.
 
         self.finish_prepare(shuffle=False)
