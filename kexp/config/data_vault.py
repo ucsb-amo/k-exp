6768d9877ec2390fffa0169763b0206965d3e501
jpagett <pagett.jared@gmail.com>
2026-01-16 17:04:16 -0800

    Update data_vault.py

--- kexp/config/data_vault.py
+++ kexp/config/data_vault.py
@@ -3,6 +3,6 @@ from waxa.data import DataVault as DataVaultWax
 
 class DataVault(DataVaultWax):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         
         self.apd = self.add_data_container('apd', 1, np.float64)
