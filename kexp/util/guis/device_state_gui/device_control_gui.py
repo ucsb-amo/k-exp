3e5e44cc014f269cba75019df68084311b1db30b
jpagett <pagett.jared@gmail.com>
2026-02-27 17:40:07 -0800

    Update device_control_gui.py

--- kexp/util/guis/device_state_gui/device_control_gui.py
+++ kexp/util/guis/device_state_gui/device_control_gui.py
@@ -5,6 +5,7 @@ from waxx.util.guis.device_control_gui import DeviceStateGUI
 
 from kexp.config.ip import MONITOR_SERVER_IP, MONITOR_SERVER_PORT, MONITOR_STATE_FILEPATH
 from kexp import dds_frame
+from kexp.config.dac_id import dac_frame
 
 def main():
     """Main application entry point"""
@@ -13,11 +14,14 @@ def main():
     # Set application style
     app.setStyle('Fusion')
     
+    dac = dac_frame()
+    dds = dds_frame(dac_frame_obj=dac)
     # Create and show main window
     window = DeviceStateGUI(monitor_server_ip=MONITOR_SERVER_IP,
                             monitor_server_port=MONITOR_SERVER_PORT,
                             device_state_json_path=MONITOR_STATE_FILEPATH,
-                            dds_frame=dds_frame())
+                            dds_frame=dds,
+                            dac_frame=dac)
     window.show()
     
     # Run application
