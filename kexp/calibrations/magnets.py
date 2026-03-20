811d0570b59c461fe4da7a745a9371c209f1259e
jpagett <pagett.jared@gmail.com>
2026-03-19 17:06:16 -0700

    Update magnets.py

--- kexp/calibrations/magnets.py
+++ kexp/calibrations/magnets.py
@@ -20,7 +20,7 @@ offset_i_transducer_per_v_setpoint_pid_outer  = -0.3520810805908351
 # offset_overhead_per_i_transducer = -1.25112
 
 # 2025-11-11 fudged to make it work only near I=20A (LF), I=182A (LF)
-slope_overhead_per_i_transducer = 0.35
+slope_overhead_per_i_transducer = 0.4
 offset_overhead_per_i_transducer = 0.
 
 @portable
