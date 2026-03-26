1ea16d4ac77985d95ec7e63af03a599ee88b6174
jpagett <pagett.jared@gmail.com>
2026-03-26 10:32:30 -0700

    rm old liveod

--- kexp/util/live_od/camera_nanny.py
+++
@@ -1,6 +0,0 @@
-"""Re-export from waxx — see :mod:`waxx.util.live_od.camera_nanny`."""
-from waxx.util.live_od.camera_nanny import *  # noqa: F401,F403
-from waxx.util.live_od.camera_nanny import CameraNanny
-
-
-    
