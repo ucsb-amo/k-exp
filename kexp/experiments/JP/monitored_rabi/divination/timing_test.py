a9a7fadb9e511020f34c8c886c21c915d019e679
hpbroida <weldlabucsb@gmail.com>
2026-04-06 16:38:47 -0700

    gamin

--- kexp/experiments/JP/monitored_rabi/divination/timing_test.py
+++ kexp/experiments/JP/monitored_rabi/divination/timing_test.py
@@ -1,59 +1,130 @@
 from artiq.experiment import *
-from artiq.language import now_mu, at_mu
+from artiq.language import now_mu, delay
 import numpy as np
 
 class timing_test(EnvExperiment):
     kernel_invariants = {
                         "m",
-                        "Omega",
                         "dt",
                         "N_photons_per_shot",
                         "omega_guess_list",
                         "omega_sq_list",
+                        "state_x",
+                        "state_y",
+                        "state_z",
                         "cos_z_list",
                         "sin_z_list",
                         "cos_H_list",
                         "sin_H_list",
                         "Omega_over_H_list",
                         "u_z_list",
-                        "omega_step"}
+                        "sin_lut",
+                        "lut_size",
+                        "lut_scale",
+                        "two_pi",
+                        "pi_half"}
 
     def prepare(self):
         self.core = self.get_device('core')
-        self.ttl = self.get_device('ttl4')
 
         self.m = 21
         self.P0 = np.ones(self.m)
         self.P0 = self.P0 / np.sum(self.P0)
-        self.state_list = np.zeros((self.m,3))
-        self.state_list[:,2] = 1.
+        self.state_x = np.zeros(self.m)
+        self.state_y = np.zeros(self.m)
+        self.state_z = np.ones(self.m)
 
         self.Omega = 2*np.pi*80.e3 # rabi frequency guess
+        self.two_pi = 2*np.pi
+        self.pi_half = 0.5*np.pi
 
         omega_guess = 2*np.pi*147.e6 # state splitting guess
         offset = 5 # how many rabi frequencies away from the guess to "search"
         self.omega_guess_list = omega_guess + 2*offset*self.Omega*np.linspace(-1,1,self.m)
-        self.omega_step = self.omega_guess_list[1] - self.omega_guess_list[0]
         self.omega_sq_list = self.omega_guess_list * self.omega_guess_list
 
         self.omega_raman = omega_guess # omega_ctrl
         
         self.dt = 2.e-6 # drive pulse length per step
 
-        delta_omega_list = self.omega_raman - self.omega_guess_list
-        alpha_z_list = 2.0 * self.dt * delta_omega_list
-        self.cos_z_list = np.cos(alpha_z_list)
-        self.sin_z_list = np.sin(alpha_z_list)
-
-        norm_H_list = np.sqrt(self.Omega*self.Omega + delta_omega_list*delta_omega_list)
-        self.Omega_over_H_list = self.Omega / norm_H_list
-        self.u_z_list = delta_omega_list / norm_H_list
-
-        alpha_H_list = 2.0 * norm_H_list * self.dt
-        self.cos_H_list = np.cos(alpha_H_list)
-        self.sin_H_list = np.sin(alpha_H_list)
+        # These tables depend on (omega_raman, Omega) and are refreshed in kernel each call.
+        self.cos_z_list = np.zeros(self.m)
+        self.sin_z_list = np.zeros(self.m)
+        self.cos_H_list = np.ones(self.m)
+        self.sin_H_list = np.zeros(self.m)
+        self.Omega_over_H_list = np.zeros(self.m)
+        self.u_z_list = np.zeros(self.m)
 
         self.N_photons_per_shot = 10
+
+        # Sine lookup table used by kernel-side trig interpolation.
+        self.lut_size = 4096
+        self.lut_scale = self.lut_size / self.two_pi
+        self.sin_lut = np.sin(self.two_pi * np.arange(self.lut_size) / self.lut_size)
+
+    @kernel(flags={"fast-math"})
+    def sin_lut_interp(self, x):
+        two_pi = self.two_pi
+        # Fast phase wrap to [0, 2pi) without expensive transcendental operations.
+        turns = int(x / two_pi)
+        x -= two_pi * turns
+        if x < 0.0:
+            x += two_pi
+
+        y = x * self.lut_scale
+        i0 = int(y)
+        frac = y - i0
+        i1 = i0 + 1
+        if i1 == self.lut_size:
+            i1 = 0
+
+        s0 = self.sin_lut[i0]
+        s1 = self.sin_lut[i1]
+        return s0 + frac * (s1 - s0)
+
+    @kernel(flags={"fast-math"})
+    def cos_lut_interp(self, x):
+        return self.sin_lut_interp(x + self.pi_half)
+
+    @kernel(flags={"fast-math"})
+    def update_dynamic_tables(self):
+        omega_guess_list = self.omega_guess_list
+        cos_z_list = self.cos_z_list
+        sin_z_list = self.sin_z_list
+        cos_H_list = self.cos_H_list
+        sin_H_list = self.sin_H_list
+        Omega_over_H_list = self.Omega_over_H_list
+        u_z_list = self.u_z_list
+        m = self.m
+
+        omega_raman = self.omega_raman
+        Omega = self.Omega
+        dt = self.dt
+
+        j = 0
+        while j < m:
+            delta_omega = omega_raman - omega_guess_list[j]
+
+            alpha_z = 2.0 * dt * delta_omega
+            cos_z_list[j] = self.cos_lut_interp(alpha_z)
+            sin_z_list[j] = self.sin_lut_interp(alpha_z)
+
+            norm_H = np.sqrt(Omega * Omega + delta_omega * delta_omega)
+            if norm_H > 0.0:
+                inv_norm_H = 1.0 / norm_H
+                Omega_over_H_list[j] = Omega * inv_norm_H
+                u_z_list[j] = delta_omega * inv_norm_H
+
+                alpha_H = 2.0 * norm_H * dt
+                cos_H_list[j] = self.cos_lut_interp(alpha_H)
+                sin_H_list[j] = self.sin_lut_interp(alpha_H)
+            else:
+                # Degenerate axis/angle case maps to identity evolution for this term.
+                Omega_over_H_list[j] = 0.0
+                u_z_list[j] = 0.0
+                cos_H_list[j] = 1.0
+                sin_H_list[j] = 0.0
+            j += 1
         
     @kernel
     def run(self):
@@ -66,25 +137,27 @@ class timing_test(EnvExperiment):
 
         self.core.reset()
         
-        self.ttl.on()
         t0 = now_mu()
         self.core.wait_until_mu(t0)
         # slack0 = t0 - self.core.get_rtio_counter_mu()
-        (mn, std) = self.generate_posterior(v, 100.e-6)
+        (self.omega_raman, self.Omega) = self.generate_posterior(v, 100.e-6)
         slack1 = t0 - self.core.get_rtio_counter_mu()
-        # self.ttl.off()
         
         delay(10.e-3)
         print(abs(slack1))
 
     @kernel(flags={"fast-math"})
     def generate_posterior(self, k, t):
+        self.update_dynamic_tables()
+
         P0_total = 0.
         moment_2 = 0.
         mn = 0.
         omega_guess_list = self.omega_guess_list
         omega_sq_list = self.omega_sq_list
-        state_list = self.state_list
+        state_x = self.state_x
+        state_y = self.state_y
+        state_z = self.state_z
         P0 = self.P0
         cos_z_list = self.cos_z_list
         sin_z_list = self.sin_z_list
@@ -96,26 +169,20 @@ class timing_test(EnvExperiment):
         n_photons = self.N_photons_per_shot
 
         k_int = int(k)
-        # int_k_mode = (k == k_int)
         nk_int = n_photons - k_int
-        # int_nk_mode = (nk_int >= 0)
-
-        wt = omega_guess_list[0] * t
-        cos_wt = np.cos(wt)
-        sin_wt = np.sin(wt)
-
-        dwt = self.omega_step * t
-        cos_dwt = np.cos(dwt)
-        sin_dwt = np.sin(dwt)
 
         for j in range(m):
             omega = omega_guess_list[j]
             cos_z = cos_z_list[j]
             sin_z = sin_z_list[j]
 
-            sx = state_list[j][0]
-            sy = state_list[j][1]
-            sz = state_list[j][2]
+            wt = omega * t
+            cos_wt = self.cos_lut_interp(wt)
+            sin_wt = self.sin_lut_interp(wt)
+
+            sx = state_x[j]
+            sy = state_y[j]
+            sz = state_z[j]
 
             cos_H = cos_H_list[j]
             sin_H = sin_H_list[j]
@@ -149,9 +216,9 @@ class timing_test(EnvExperiment):
             nx = cos_z * hx + sin_z * hy
             ny = -sin_z * hx + cos_z * hy
 
-            state_list[j][0] = nx
-            state_list[j][1] = ny
-            state_list[j][2] = hz
+            state_x[j] = nx
+            state_y[j] = ny
+            state_z[j] = hz
 
             p1 = hz
             # if int_k_mode and int_nk_mode:
@@ -175,10 +242,6 @@ class timing_test(EnvExperiment):
             moment_2 += pj * omega_sq_list[j]
 
             P0[j] = pj
-
-            next_cos_wt = cos_wt * cos_dwt - sin_wt * sin_dwt
-            sin_wt = sin_wt * cos_dwt + cos_wt * sin_dwt
-            cos_wt = next_cos_wt
 
         mn = mn / P0_total
         moment_2 = moment_2 / P0_total
