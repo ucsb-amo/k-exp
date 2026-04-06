07f911b3f1471ddf5a4fafb75707f0647f17c8bd
hpbroida <weldlabucsb@gmail.com>
2026-04-06 16:01:07 -0700

    winning

---
+++ kexp/experiments/JP/monitored_rabi/divination/timing_test_2.py
@@ -0,0 +1,159 @@
+from artiq.experiment import *
+from artiq.language import now_mu, at_mu
+import numpy as np
+
+class timing_test(EnvExperiment):
+    kernel_invariants = {
+                        "m",
+                        "Omega",
+                        "dt",
+                        "N_photons_per_shot"}
+
+    def prepare(self):
+        self.core = self.get_device('core')
+        self.ttl = self.get_device('ttl4')
+
+        self.m = 21
+        self.P0 = np.ones(self.m)
+        self.P0 = self.P0 / np.sum(self.P0)
+        self.state_list = np.zeros((self.m,3))
+        self.state_list[:,2] = 1.
+
+        self.Omega = 2*np.pi*80.e3 # rabi frequency guess
+
+        omega_guess = 2*np.pi*147.e6 # state splitting guess
+        offset = 5 # how many rabi frequencies away from the guess to "search"
+        self.omega_guess_list = omega_guess + 2*offset*self.Omega*np.linspace(-1,1,self.m)
+
+        self.omega_raman = omega_guess # omega_ctrl
+        
+        self.dt = 2.e-6 # drive pulse length per step
+
+        self.N_photons_per_shot = 10
+
+        self.I = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
+        self.R_z = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
+        self.K = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
+        self.R_H = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
+        self.R = np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
+        
+    @kernel
+    def run(self):
+        v = 0.1
+        v = v * self.N_photons_per_shot
+        t = 100.e-6
+
+        slack0 = 0
+        slack1 = 1
+
+        self.core.reset()
+        
+        self.ttl.on()
+        t0 = now_mu()
+        self.core.wait_until_mu(t0)
+        # slack0 = t0 - self.core.get_rtio_counter_mu()
+        (mn, std) = self.generate_posterior(v, 100.e-6)
+        slack1 = t0 - self.core.get_rtio_counter_mu()
+        # self.ttl.off()
+        
+        delay(500.e-3)
+        # print(slack0, slack1)
+        print(slack1)
+
+    @kernel(flags={"fast-math"})
+    def generate_posterior(self, k, t):
+        P0_total = 0.
+        p1 = 0.
+        moment_2 = 0.
+        mn = 0.
+        std = 0.
+        sin_H = 0.
+        cos_H = 0.
+
+        for j in range(self.m):
+            omega = self.omega_guess_list[j]
+            delta_omega = self.omega_raman - omega
+
+            phase = self.dt * delta_omega
+            alpha_Z = 2.0 * phase
+            cos_z = np.sin(np.pi/2 - alpha_Z)
+            sin_z = np.sin(alpha_Z)
+
+            self.R_z[0][0] = cos_z
+            self.R_z[0][1] = sin_z
+            self.R_z[0][2] = 0.0
+
+            self.R_z[1][0] = -sin_z
+            self.R_z[1][1] = cos_z
+            self.R_z[1][2] = 0.0
+
+            self.R_z[2][0] = 0.0
+            self.R_z[2][1] = 0.0
+            self.R_z[2][2] = 1.0
+
+            norm_H = np.sqrt(self.Omega**2 + delta_omega**2)
+            if norm_H != 0.:
+                alpha_H = 2.0 * norm_H * self.dt
+                cos_H = np.sin(np.pi/2 - alpha_H)
+                sin_H = np.sin(alpha_H)
+
+                Omega_over_H = self.Omega / norm_H
+
+                u_x = Omega_over_H * np.sin(np.pi/2 - omega * t) 
+                u_y = Omega_over_H * np.sin(omega * t)
+                u_z = delta_omega / norm_H
+
+                self.K[0][0] = 0.0
+                self.K[0][1] = -u_z
+                self.K[0][2] = u_y
+
+                self.K[1][0] = u_z
+                self.K[1][1] = 0.0
+                self.K[1][2] = -u_x
+
+                self.K[2][0] = -u_y
+                self.K[2][1] = u_x
+                self.K[2][2] = 0.0
+
+                # for r in range(3):
+                #     for c in range(3):
+                #         self.R[r][c] = 0.0
+
+                for q in range(3):
+                    for c in range(3):
+                        k2_qc = 0.0
+                        for s in range(3):
+                            k2_qc += self.K[q][s] * self.K[s][c]
+
+                        rh_qc = (
+                            self.I[q][c]
+                            + sin_H * self.K[q][c]
+                            + (1.0 - cos_H) * k2_qc
+                        )
+                        self.R_H[q][c] = rh_qc
+
+                        for r in range(3):
+                            self.R[r][c] += self.R_z[r][q] * rh_qc
+
+                for i in range(3):
+                    self.state_list[j][i] = self.R[i] @ self.state_list[j]
+            else:
+                self.R = self.R_z
+                for i in range(3):
+                    self.state_list[j][i] = self.R[i] @ self.state_list[j]
+            
+            p1 = self.state_list[j][2]
+            pj = self.P0[j] * (p1**k) * ((1.0 - p1)**(self.N_photons_per_shot-k))
+
+            P0_total += pj
+            mn += pj * omega
+            moment_2 += pj * omega**2
+
+            self.P0[j] = pj
+
+        mn = mn / P0_total
+        moment_2 = moment_2 / P0_total
+
+        std = np.sqrt(moment_2 - mn**2)
+
+        return mn, std
