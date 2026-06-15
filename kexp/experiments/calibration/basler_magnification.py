6c8039a3e735a06d5510a6a693cb32cafae6cba8
jpagett <pagett.jared@gmail.com>
2026-06-15 16:07:49 -0700

    Create basler_magnification.py

---
+++ kexp/experiments/calibration/basler_magnification.py
@@ -0,0 +1,60 @@
+from artiq.experiment import *
+from artiq.experiment import delay
+from kexp import Base, img_types, cameras
+import numpy as np
+
+class gm_tof(EnvExperiment, Base):
+
+    def prepare(self):
+        Base.__init__(self,setup_camera=True,save_data=True,
+                      camera_select=cameras.xy_basler,
+                      imaging_type=img_types.ABSORPTION)
+
+        # self.xvar('frequency_detuned_imaging',np.arange(350.,450.,3)*1.e6)]
+
+        # self.xvar('dumdum',[0]*10)
+        self.xvar('t_tof',np.linspace(10.,20.,8)*1.e-3)
+
+        self.p.imaging_state = 2.
+        self.p.t_tof = 7.e-6
+        self.p.t_mot_load = 0.5
+        self.p.N_repeats = 1
+
+        # self.xvar('amp_imaging',np.linspace(0.25,1.,8))
+
+        self.finish_prepare(shuffle=True)
+
+    @kernel
+    def scan_kernel(self):
+
+        # self.imaging.set_power(self.p.amp_imaging)
+
+        self.hybrid_mot(self.p.t_mot_load)
+        self.dds.push.off()
+
+        self.cmot_d1(self.p.t_d1cmot)
+
+        self.ttl.pd_scope_trig.pulse(1.e-8)
+        self.gm(self.p.t_gm)
+        self.gm_ramp(self.p.t_gmramp)
+
+        self.release()
+        
+        delay(self.p.t_tof)
+
+        self.flash_repump()
+        self.abs_image()
+       
+    @kernel
+    def run(self):
+        self.init_kernel(setup_awg=False)
+        self.load_2D_mot(self.p.t_2D_mot_load_delay)
+        self.scan()
+        self.mot_observe()
+
+    def analyze(self):
+        import os
+        expt_filepath = os.path.abspath(__file__)
+        self.end(expt_filepath)
+        
+        
