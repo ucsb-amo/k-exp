4a0ed90ca4032f43c00b1bf7e164c3d2e7f07601
jpagett <pagett.jared@gmail.com>
2026-08-06 15:55:53 -0700

    m

--- kexp/experiments/HF_experiments/Rydberg/hf_bec_rydberg_scan_apd_time_resolved.py
+++ kexp/experiments/HF_experiments/Rydberg/hf_bec_rydberg_scan_apd_time_resolved.py
@@ -16,7 +16,7 @@ class hf_bec(EnvExperiment, Base):
 
         # self.xvar('do_405_pulse',[0,1])
         self.p.do_405_pulse = 1
-        # self.xvar('do_980_pulse',[0,1])
+        self.xvar('do_980_pulse',[0,1])
         self.p.do_980_pulse = 1
         self.p.amp_dds_405 = .06
 #   
@@ -26,7 +26,7 @@ class hf_bec(EnvExperiment, Base):
 
 
         # self.xvar('frequency_eo_980', 366.4e6 + 1.e6 * np.linspace(-5,5,9))
-        self.xvar('frequency_eo_980', np.arange(422.,434.,0.05)*1.e6)
+        # self.xvar('frequency_eo_980', np.arange(422.,434.,0.05)*1.e6)
         # self.xvar('frequency_eo_980', np.linspace(430.,460.,40)*1.e6)
         # self.p.frequency_eo_980 = self.siglent.siglent_980._frequency_default
         # self.p.frequency_eo_980 = 352.1e6
@@ -34,26 +34,26 @@ class hf_bec(EnvExperiment, Base):
 
         # self.xvar('frequency_eo_980', np.arange(422.,424.,0.1)*1.e6)
         self.p.frequency_eo_980 = 422.695e6
-        # self.xvar('frequency_eo_980', 418.1e6 + 1e6*np.linspace(-1,1,3))
+        # self.xvar('frequency_eo_980', self.p.frequency_eo_980 + np.arange(-1.e6,1.e6,0.05e6))
 
         # self.xvar('t_tweezer_hold', np.linspace(0.0, 700.0, 4) * 1.e-3)
-        self.p.t_tweezer_hold = 1200.e-3
+        self.p.t_tweezer_hold = 800.e-3
 
         # self.p.v_pd_hf_tweezer_1064_rampdown3_end=3.5
 
         # self.p.hf_imaging_detuning = -568.e6
 
-        self.p.amp_imaging = 0.15
+        self.p.amp_imaging = 0.25
         self.p.t_imaging_pulse = 10.e-6
 
         # self.xvar('v_pd_ry_980',np.linspace(0.,1.,5))
         self.p.v_pd_ry_405 = 0.4
         self.p.v_pd_ry_980 = 2.8
 
-        self.p.N_pulses = 7
+        self.p.N_pulses = 6
 
         # self.xvar('beans',np.linspace(0,30,10))
-        self.p.N_repeats = 1
+        self.p.N_repeats = 10
 
 
         self.data.apd = self.data.add_data_container(self.p.N_pulses, float)
